a2ef8a5da0fb3b81a88b713a1ab0d8f1c670c395
Nate Jones <pair+nate@casecommons.org>
2016-06-28 12:50:15 -0400

    Add source maps to SCSS transform
    [#122713461]

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default', ['compile-scss']);
 
 gulp.task('compile-scss', function() {
-  gulp.src('app/assets/stylesheets/**/*.scss')
+  gulp.src('app/assets/stylesheets/application.scss')
+  .pipe(sourcemaps.init())
   .pipe(sass({ indentedSyntax: false, errLogToConsole: true }))
+  .pipe(sourcemaps.write())
   .pipe(gulp.dest('public/assets'));
 });
