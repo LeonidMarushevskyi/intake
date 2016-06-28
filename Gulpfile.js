cc8e6eab7fd1de0b12b1d06706087073204260b4
Nate Jones <pair+nate@casecommons.org>
2016-06-28 16:10:13 -0400

    Transpile application.js and move it to public assets
    This allows us to tranform our es6 code to es5
    
    An important thing to note about browserify is that by default
    browserify takes an array of file paths and returns a text stream.
    However Gulp requires each transformation to provide a virtual file as
    input.
    
    We can solve this problem by using the vinyl-source-stream package.
    vinyl-source-stream is a stream transformation which takes text stream
    as an input and returns a virtual file as an output.

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,7 +2,24 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('default', ['compile-scss']);
+var vinylSource = require('vinyl-source-stream')
+var vinylBuffer = require('vinyl-buffer')
+function bundle(browserifyPack, name) {
+  var p = browserifyPack.bundle().pipe(vinylSource(name)).pipe(vinylBuffer())
+  return p.pipe(sourcemaps.init({loadMaps: true}))
+  .pipe(sourcemaps.write('.'))
+  .pipe(gulp.dest('public/assets/'))
+}
+
+var browserify = require('browserify')
+var babelify = require('babelify')
+var appPack = browserify({
+  entries: ['app/assets/javascripts/application.js'],
+  paths: ['./app/assets/javascripts/'],
+  debug: true,
+}).transform(babelify)
+
+gulp.task('default', ['js-app', 'compile-scss']);
 
 gulp.task('compile-scss', function() {
   gulp.src('app/assets/stylesheets/application.scss')
@@ -11,3 +28,7 @@ gulp.task('compile-scss', function() {
   .pipe(sourcemaps.write())
   .pipe(gulp.dest('public/assets'));
 });
+
+gulp.task('js-app', function() {
+  return bundle(appPack, 'application.js')
+})
---
+++ app/assets/javascripts/samples/hello.js
@@ -0,0 +1,7 @@
+export function sayHello(name) {
+  console.log(`Hello, ${name}!`);
+}
+
+export function sayGoodbye(name) {
+  console.log(`Goodbye, ${name}!`);
+}
