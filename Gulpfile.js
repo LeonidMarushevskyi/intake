e9f50f76e3feb5b73f00957770266351e6e36f21
Nate Jones <pair+nate@casecommons.org>
2016-06-28 12:45:36 -0400

    Add gulp SCSS transform task
    Added sample.application.scss to test transformed
    output
    
    [#122713461]

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,10 @@
-var gulp;
+var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-gulp = require('gulp');
+gulp.task('default', ['compile-scss']);
 
-gulp.task('default', function() {
-    //Yep. Nothing.
+gulp.task('compile-scss', function() {
+  gulp.src('app/assets/stylesheets/**/*.scss')
+  .pipe(sass({ indentedSyntax: false, errLogToConsole: true }))
+  .pipe(gulp.dest('public/assets'));
 });
