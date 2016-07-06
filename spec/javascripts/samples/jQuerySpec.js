58a1c7942450915b2bdaec87d92696d7f6f0eb72
Lucian Kahn and Nate Jones <pair+lucian+nate@casecommons.org>
2016-07-06 17:36:52 -0400

    Add jquery and jquery-ujs
    [#123023061]

---
+++ spec/javascripts/samples/jQuerySpec.js
@@ -0,0 +1,8 @@
+import 'jquery-helpers'
+
+describe('jquery-helpers', ()=> {
+  it('adds jQuery to the window', () => {
+    window.jQuery('body').data('example', 'kitty cats')
+    expect(window.jQuery('body').data('example')).toEqual('kitty cats')
+  })
+})
