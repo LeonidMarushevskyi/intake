e43a2e2e5c26802d84e9680787956305221f87e7
Suman Gurung <pair+suman@casecommons.org>
2016-08-02 16:47:25 -0400

    Require 'faker' in spec_helper
    [#127497321]

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ require 'spec_helper'
 require 'rspec/rails'
 require 'factory_girl_rails'
 require 'support/assets_gulp'
+require 'faker'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
