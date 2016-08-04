afc9e57eee79fb88c660cf7cf478c8eeac28f261
Aneesh Pakala and Suman Gurung <pair+suman@casecommons.org>
2016-08-04 15:55:24 -0400

    Add /lib to autoload_paths
    [#127199843]

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ Bundler.require(*Rails.groups)
 
 module CaIntake
   class Application < Rails::Application
+    config.autoload_paths << Rails.root.join('lib')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
