23c6181f0bd7082ef35b19bbbc8253ace22f9a59
Suman Gurung <suman@casecommons.org>
2016-08-09 17:28:27 -0400

    Fix controller testing, make rspec work when using assigns and render_template
    See: https://github.com/rails/rails-controller-testing#rspec
    
    [#128025831]

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,6 +10,7 @@ require 'rspec/rails'
 require 'factory_girl_rails'
 require 'support/assets_gulp'
 require 'faker'
+require 'rails-controller-testing'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -36,4 +37,10 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  [:controller, :view, :request].each do |type|
+    config.include ::Rails::Controller::Testing::TestProcess, :type => type
+    config.include ::Rails::Controller::Testing::TemplateAssertions, :type => type
+    config.include ::Rails::Controller::Testing::Integration, :type => type
+  end
 end
