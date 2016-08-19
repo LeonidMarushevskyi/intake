df3aefa5a23c078019e15bf4e5e5c550e828138f
Suman Gurung <suman@casecommons.org>
2016-08-19 12:39:41 -0400

    Use constant instead of magic strings
    [#127207495]

--- app/services/referral_creator.rb
+++ app/services/referral_creator.rb
@@ -4,6 +4,9 @@ require 'luid'
 # ReferralCreator is a service class which is responsible for creating
 # a referral using the API endpoint
 class ReferralCreator
+  REFERRAL_PATH = '/api/v1/referrals'
+  CONTENT_TYPE = 'application/json'
+
   def self.create
     reference = LUID.generate.first
     response = make_api_request_with_params(reference: reference)
@@ -13,8 +16,8 @@ class ReferralCreator
 
   def self.make_api_request_with_params(referral_params)
     ::API.connection.post do |req|
-      req.url '/api/v1/referrals'
-      req.headers['Content-Type'] = 'application/json'
+      req.url REFERRAL_PATH
+      req.headers['Content-Type'] = CONTENT_TYPE
       req.body = { referral: referral_params }.to_json
     end
   end
