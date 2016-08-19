6f7a6cd7dd52a239a44dadf30ca2769aafbe452c
Pema Geyleg <ccUser@suffolk.nyc.casecommons.org>
2016-08-19 15:29:19 -0400

    Refactor build.sh
    [#128228297]

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,4 +6,5 @@ revision=$(git rev-parse HEAD)
 output_dir="/ca_intake_build"
 input_dir="/ca_intake"
 
-bundle exec fpm -s dir -t deb --name ca_intake --version "$revision" --package "$output_dir" "$input_dir" 
+bundle exec fpm --deb-no-default-config-files -s dir -t deb --name ca_intake \
+--version "$revision" --package "$output_dir" "$input_dir"
