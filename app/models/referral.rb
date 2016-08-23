cb2ca79c4e3581b82c4ecdc06e6a53eb7b28c778
Nate Jones <pair+suman@casecommons.org>
2016-08-23 13:20:32 -0400

    Add Referrals Model
    [#127201331]

---
+++ app/models/referral.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# Model for storing Intake referral information.
+class Referral # :nodoc:
+  include Her::Model
+  use_api API_V1
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -43,10 +43,11 @@ RSpec.configure do |config|
 
   config.include(Module.new do
     def stub_api_for(klass)
-      klass.use_api (api = Her::API.new)
+      api = Her::API.new
+      klass.use_api(api)
 
       # Here, you would customize this for your own API (URL, middleware, etc)
-      # like you have done in your application’s initializer
+      # like you have done in your application's initializer
       api.setup url: 'http://api.example.com' do |c|
         c.use Her::Middleware::FirstLevelParseJSON
         c.adapter(:test) { |s| yield(s) }
