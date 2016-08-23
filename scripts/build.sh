88045c54ac9c0f033d522fcf3dfd9a16079ca207
Pema Geyleg <ccUser@suffolk.nyc.casecommons.org>
2016-08-23 11:19:29 -0400

    Run bin/gulp before building artefacts using fpm
    [#128228297]

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,5 +6,7 @@ revision=$(git rev-parse HEAD)
 output_dir="/ca_intake_build"
 input_dir="/ca_intake"
 
+rm -rf public/assets && RAILS_ENV=production bin/gulp
+
 bundle exec fpm --deb-no-default-config-files -s dir -t deb --name ca_intake \
 --version "$revision" --package "$output_dir" "$input_dir"
