082e54ec6aa60fc44d025a8bc5305bb3589aa7b7
Pema Geyleg <ccUser@suffolk.nyc.casecommons.org>
2016-08-23 16:00:37 -0400

    fpm --version option have to start with digit
    [#128228297]

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,11 +2,11 @@
 
 cd "$APP_HOME"
 
-revision=$(git rev-parse HEAD)
+revision=$(git rev-parse --short HEAD)
 output_dir="/ca_intake_build"
 input_dir="/ca_intake"
 
 rm -rf public/assets && RAILS_ENV=production bin/gulp
 
-bundle exec fpm --deb-no-default-config-files -s dir -t deb --name ca_intake \
---version "$revision" --package "$output_dir" "$input_dir"
+bundle exec fpm --deb-no-default-config-files -s dir -t deb --name "intake_accelerator" \
+--version "1.$revision" --package "$output_dir" "$input_dir"
