026ade3d3ac50c4b656e202c35aad6f456d4f833
Nate Jones <pair+suman@casecommons.org>
2016-09-12 10:13:49 -0400

    Capybara-accessible to use non deprecated driver settings
    [#130045903]

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require 'capybara/rspec'
+require 'capybara/accessible'
 
-Capybara.default_driver = :accessible
+Capybara.default_driver = :accessible_selenium
 Capybara.javascript_driver = Capybara.default_driver
