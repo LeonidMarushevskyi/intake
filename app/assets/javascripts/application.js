0361183312efb02e5c610b80c754cfd2592ca471
Suman Gurung <suman@casecommons.org>
2016-09-21 12:09:48 -0400

    Make js-linter happy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,8 +4,6 @@ import 'jquery-ujs'
 import 'babel-polyfill'
 import 'bootstrap'
 import * as Hello from 'samples/hello'
-import React from 'react'
-import ReactDOM from 'react-dom'
 
 Hello.sayHello('Visitor')
 Hello.sayGoodbye('Visitor')
