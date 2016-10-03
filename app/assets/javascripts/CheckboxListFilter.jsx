2982fdca32f7771b3cc253f1e6371ea0adcbbd12
Nate Jones <pair+Nate.Jones+Nora.Lin@OSI.ca.gov>
2016-10-03 13:37:26 -0700

    Create checkbox list filter component
    Response time filter and screening decision filter
    are almost the same.
    
    [#131583711]

---
+++ app/assets/javascripts/CheckboxListFilter.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+
+export default class CheckboxListFilter extends React.Component {
+  constructor() {
+    super(...arguments)
+    this.onChange = this.onChange.bind(this)
+  }
+
+  onChange(event, checkedItems, item) {
+    let newCheckedItems
+    if (event.target.checked) {
+      newCheckedItems = checkedItems.concat(item)
+    } else {
+      newCheckedItems = checkedItems.filter((ele) => ele !== item)
+    }
+    this.props.onChange(newCheckedItems)
+  }
+
+  render() {
+    var {
+      collection,
+      name,
+      selected,
+    } = this.props
+
+    return (
+      <fieldset className='fieldset-inputs sans'>
+        <legend>{this.props.legend}</legend>
+        <ul className='unstyled-list'>
+          {
+            Object.keys(collection).map((item) => (
+              <li key={item}>
+                <input
+                  id={`${name}-${item}`}
+                  type='checkbox'
+                  checked={(selected || []).includes(item)}
+                  onChange={(event) => this.onChange(event, selected, item)}
+                />
+                <label htmlFor={`${name}-${item}`}>{collection[item]}</label>
+              </li>
+              ))
+          }
+        </ul>
+      </fieldset>
+    )
+  }
+}
+
+CheckboxListFilter.defaultProps = {
+  legend: '',
+  name: 'checkbox-filter',
+}
+
+CheckboxListFilter.propTypes = {
+  collection: React.PropTypes.object,
+  legend: React.PropTypes.string,
+  name: React.PropTypes.string,
+  onChange: React.PropTypes.func,
+  selected: React.PropTypes.array,
+}
