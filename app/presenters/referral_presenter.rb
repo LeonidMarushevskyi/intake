f22163ae9819f3447e6cff54bf1d9bc85ba62a5e
Nate Jones <pair+Nate.Jones+Nora.Lin@OSI.ca.gov>
2016-10-04 14:27:14 -0700

    Remove unused HAML and presenter method
    [#128068239]

--- app/presenters/referral_presenter.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Referral presenter used to minimise presentation logic in views
-class ReferralPresenter
-  def self.name_and_reference(referral)
-    "#{referral.name} - #{referral.reference}"
-  end
-end
