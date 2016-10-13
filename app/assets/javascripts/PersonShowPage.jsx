4947d474e8594b418e7bddead589f02b3eea5487
Nate Jones and Siva Mukunda <pair+Nate.Jones+Siva.Mukunda@OSI.ca.gov>
2016-10-13 14:25:47 -0700

    Add state attributes to person show page
    [#131770037]

--- app/assets/javascripts/PersonShowPage.jsx
+++ app/assets/javascripts/PersonShowPage.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import Immutable from 'immutable'
 
 export default class PersonShowPage extends React.Component {
   constructor() {
     super(...arguments)
+    this.state = {
+      person: Immutable.Map(),
+    }
   }
 
   render() {
+    const {person} = this.state
     return (
       <div className='card double-gap-top'>
         <div className='card-header'>
@@ -15,47 +20,47 @@ export default class PersonShowPage extends React.Component {
           <div className='row'>
             <div className='col-md-6'>
               <label className='no-gap'>First Name</label>
-              <div className='c-gray'></div>
+              <div className='c-gray'>{person.get('first_name')}</div>
             </div>
             <div className='col-md-6'>
               <label className='no-gap-top-desktop'>Last Name</label>
-              <div className='c-gray'></div>
+              <div className='c-gray'>{person.get('last_name')}</div>
             </div>
           </div>
           <div className='row gap-top'>
             <div className='col-md-6'>
               <label>Gender</label>
-              <div className='c-gray'></div>
+              <div className='c-gray'>{person.get('gender')}</div>
             </div>
           </div>
           <div className='row gap-top'>
             <div className='col-md-6'>
               <label>Date of birth</label>
-              <div className='c-gray'></div>
+              <div className='c-gray'>{person.get('date_of_birth')}</div>
             </div>
             <div className='col-md-6'>
               <label>Social security number</label>
-              <div className='c-gray'></div>
+              <div className='c-gray'>{person.get('ssn')}</div>
             </div>
           </div>
           <div className='row gap-top'>
             <div className='col-md-6'>
               <label>Address</label>
-              <div className='c-gray'></div>
+              <div className='c-gray'>{person.getIn(['address', 'street_address'])}</div>
             </div>
             <div className='col-md-6'>
               <label>City</label>
-              <div className='c-gray'></div>
+              <div className='c-gray'>{person.getIn(['address', 'city'])}</div>
             </div>
           </div>
           <div className='row gap-top'>
             <div className='col-md-6'>
               <label>State</label>
-              <div className='c-gray'></div>
+              <div className='c-gray'>{person.getIn(['address', 'state'])}</div>
             </div>
             <div className='col-md-6'>
               <label>Zip</label>
-              <div className='c-gray'></div>
+              <div className='c-gray'>{person.getIn(['address', 'zip'])}</div>
             </div>
           </div>
         </div>
