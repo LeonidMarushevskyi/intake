d493700c0e347f7e9d6029e49c78e7878f9bb379
Nora Lin and Sri Harsha Poosa <pair+Nora.Lin+sri@OSI.ca.gov>
2016-10-13 11:09:57 -0700

    Move fields to the Screening Information card
    [#131856305]

--- spec/features/show_screening_spec.rb
+++ spec/features/show_screening_spec.rb
@@ -42,15 +42,18 @@ feature 'Show Screening' do
 
     expect(page).to have_content 'Screening #My Bad!'
 
+    within '#screening-information-card' do
+      expect(page).to have_content 'Mail'
+      expect(page).to have_content 'The Rocky Horror Picture Show'
+      expect(page).to have_content '8/13/2016 10:00 AM'
+      expect(page).to have_content '8/22/2016 11:00 AM'
+    end
+
     within '#participants-card' do
       expect(page).to have_content 'Bart Simpson'
     end
 
     within '#referral-information-card' do
-      expect(page).to have_content 'Mail'
-      expect(page).to have_content 'The Rocky Horror Picture Show'
-      expect(page).to have_content '8/13/2016 10:00 AM'
-      expect(page).to have_content '8/22/2016 11:00 AM'
       expect(page).to have_content '8/11/2016'
       expect(page).to have_content 'Sacramento'
       expect(page).to have_content '123 fake st'
