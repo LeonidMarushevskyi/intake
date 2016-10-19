e44972dd6b5413b85581bb213296dc7939ca8d51
Nate Jones and Siva Mukunda <pair+Nate.Jones+Siva.Mukunda@OSI.ca.gov>
2016-10-19 11:18:56 -0700

    Person repository uses single method for API call
    [#refactor]

--- app/repositories/person_repository.rb
+++ app/repositories/person_repository.rb
@@ -7,33 +7,26 @@ class PersonRepository
   CONTENT_TYPE = 'application/json'
 
   def self.create(person)
-    response = make_api_post(PEOPLE_PATH, person)
+    response = make_api_call(PEOPLE_PATH, :post, person)
     raise 'Error creating person' if response.status != 201
     Rails.logger.info response.body.inspect
     Person.new(response.body)
   end
 
   def self.find(id)
-    response = make_api_get("#{PEOPLE_PATH}/#{id}")
+    response = make_api_call("#{PEOPLE_PATH}/#{id}", :get)
     raise 'Error finding person' if response.status != 200
     Rails.logger.info response.body.inspect
     Person.new(response.body)
   end
 
-  def self.make_api_post(url, attributes = {})
-    ::API.connection.post do |req|
-      req.url url
-      req.headers['Content-Type'] = CONTENT_TYPE
-      req.body = attributes.to_json
-    end
-  end
-  private_class_method :make_api_post
 
-  def self.make_api_get(url)
-    ::API.connection.get do |req|
+  def self.make_api_call(url, method, attributes = nil)
+    ::API.connection.send(method) do |req|
       req.url url
       req.headers['Content-Type'] = CONTENT_TYPE
+      req.body = attributes.to_json unless attributes.nil?
     end
   end
-  private_class_method :make_api_get
+  private_class_method :make_api_call
 end
