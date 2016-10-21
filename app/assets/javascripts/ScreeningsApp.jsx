2f17d1070cddfc89eeaa30ff64db8b6e4b1ba353
Nora Lin and Sri Harsha Poosa <pair+Nora.Lin+sri@OSI.ca.gov>
2016-10-21 11:29:58 -0700

    Convert screening "Narrative Card" (show) to using react
    [#132359327]

--- app/assets/javascripts/ScreeningsApp.jsx
+++ app/assets/javascripts/ScreeningsApp.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Router, Route, browserHistory} from 'react-router'
 import ScreeningEditPage from 'ScreeningEditPage'
+import ScreeningShowPage from 'ScreeningShowPage'
 import ScreeningsIndexPage from 'ScreeningsIndexPage'
 
 export default class App extends React.Component {
@@ -8,6 +9,7 @@ export default class App extends React.Component {
     return (
       <Router history={browserHistory}>
         <Route path='/screenings' component={ScreeningsIndexPage} />
+        <Route path='/screenings/:id' component={ScreeningShowPage} />
         <Route path='/screenings/:id/edit' component={ScreeningEditPage} />
       </Router>
     )
