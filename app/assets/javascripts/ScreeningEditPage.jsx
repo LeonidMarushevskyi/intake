b5cc5318a1413bfc8283b37f81f948f335089c4d
Nate Jones and Siva Mukunda <pair+Nate.Jones+Siva.Mukunda@OSI.ca.gov>
2016-10-21 16:42:09 -0700

    Add Autocompleter to edit screening page
    [#131856349]

--- app/assets/javascripts/ScreeningEditPage.jsx
+++ app/assets/javascripts/ScreeningEditPage.jsx
@@ -1,6 +1,7 @@
 import * as Utils from 'utils/http'
 import Immutable from 'immutable'
 import React from 'react'
+import Autocompleter from 'autocompleter'
 
 export default class ScreeningEditPage extends React.Component {
   constructor() {
@@ -8,10 +9,11 @@ export default class ScreeningEditPage extends React.Component {
     this.state = {
       screening: Immutable.fromJS({
         report_narrative: '',
+        participants: [],
       }),
     }
-
     this.fetch = this.fetch.bind(this)
+    this.setField = this.setField.bind(this)
   }
 
   componentDidMount() {
@@ -32,21 +34,41 @@ export default class ScreeningEditPage extends React.Component {
   }
 
   render() {
+    const {screening} = this.state
     return (
-      <div className='card edit double-gap-top' id='narrative-card'>
-        <div className='card-header'>
-          <span>Narrative</span>
+      <div>
+        <div className='card edit double-gap-top' id='participants-card'>
+          <div className='card-header'>
+            <span>Participants</span>
+          </div>
+          <div className='card-body'>
+            <div className='row'>
+              <div className='col-md-6'>
+                <label className='no-gap' htmlFor='screening_participants' >Participants</label>
+                <Autocompleter
+                  id='screening_participants'
+                  participants={screening.get('participants').toJS()}
+                  setField={this.setField}
+                />
+              </div>
+            </div>
+          </div>
         </div>
-        <div className='card-body'>
-          <div className='row'>
-            <div className='col-md-12'>
-              <label className='no-gap' htmlFor='screening[report_narrative]'>Report Narrative</label>
-              <textarea
-                name='screening[report_narrative]'
-                id='screening[report_narrative]'
-                value={this.state.screening.get('report_narrative') || ''}
-                onChange={(event) => this.setField(['report_narrative'], event.target.value)}
-              />
+        <div className='card edit double-gap-top' id='narrative-card'>
+          <div className='card-header'>
+            <span>Narrative</span>
+          </div>
+          <div className='card-body'>
+            <div className='row'>
+              <div className='col-md-12'>
+                <label className='no-gap' htmlFor='screening[report_narrative]'>Report Narrative</label>
+                <textarea
+                  name='screening[report_narrative]'
+                  id='screening[report_narrative]'
+                  value={screening.get('report_narrative') || ''}
+                  onChange={(event) => this.setField(['report_narrative'], event.target.value)}
+                />
+              </div>
             </div>
           </div>
         </div>
--- spec/javascripts/ScreeningEditPageSpec.jsx
+++ spec/javascripts/ScreeningEditPageSpec.jsx
@@ -12,33 +12,59 @@ describe('ScreeningEditPage', () => {
   })
 
   describe('render', () => {
-    it('renders the narrative card header', () => {
-      const props = { params: { id: 1 } }
-      const wrapper = mount(<ScreeningEditPage {...props} />)
-      expect(wrapper.find('#narrative-card .card-header').text()).toEqual('Narrative')
-    })
-
-    it('renders the report narrative textarea', () => {
-      const props = { params: { id: 1 } }
-      const wrapper = mount(<ScreeningEditPage {...props} />)
-      wrapper.setState({
-        screening: Immutable.fromJS({
-          report_narrative: 'some narrative',
-        }),
-      })
-
-      expect(wrapper.find('textarea').text()).toEqual('some narrative')
-    })
-
-    describe('fetch', () => {
-      it('GETs the screening data from the server', () => {
+    describe('participants card', () => {
+      it('renders the card header', () => {
         const props = { params: { id: 1 } }
         const wrapper = mount(<ScreeningEditPage {...props} />)
-        wrapper.instance().fetch()
-        expect(Utils.request).toHaveBeenCalled()
-        expect(Utils.request.calls.argsFor(0)[0]).toEqual('GET')
-        expect(Utils.request.calls.argsFor(0)[1]).toEqual('/screenings/1.json')
+        expect(wrapper.find('#participants-card .card-header').text()).toContain('Participants')
       })
+
+      it('renders the participant label', () => {
+        const props = { params: {} }
+        const wrapper = mount(<ScreeningEditPage {...props} />)
+        expect(wrapper.find('#participants-card label').text()).toEqual('Participants')
+      })
+
+      it('renders the autocompleter', () => {
+        const props = { params: {} }
+        const wrapper = mount(<ScreeningEditPage {...props} />)
+        const participants =  [{id: 1, first_name: 'Rob', last_name: 'Mee'}]
+        const screening = Immutable.fromJS({participants: participants})
+        wrapper.setState({screening: screening})
+        expect(wrapper.find('Autocompleter').props().id).toEqual('screening_participants')
+        expect(wrapper.find('Autocompleter').props().participants).toEqual(participants)
+      })
+    })
+
+    describe('narrative card', () => {
+      it('renders the narrative card header', () => {
+        const props = { params: { id: 1 } }
+        const wrapper = mount(<ScreeningEditPage {...props} />)
+        expect(wrapper.find('#narrative-card .card-header').text()).toEqual('Narrative')
+      })
+
+      it('renders the report narrative textarea', () => {
+        const props = { params: { id: 1 } }
+        const wrapper = mount(<ScreeningEditPage {...props} />)
+        wrapper.setState({
+          screening: Immutable.fromJS({
+            report_narrative: 'some narrative',
+            participants: [],
+          }),
+        })
+        expect(wrapper.find('textarea').text()).toEqual('some narrative')
+      })
+    })
+  })
+
+  describe('fetch', () => {
+    it('GETs the screening data from the server', () => {
+      const props = { params: { id: 1 } }
+      const wrapper = mount(<ScreeningEditPage {...props} />)
+      wrapper.instance().fetch()
+      expect(Utils.request).toHaveBeenCalled()
+      expect(Utils.request.calls.argsFor(0)[0]).toEqual('GET')
+      expect(Utils.request.calls.argsFor(0)[1]).toEqual('/screenings/1.json')
     })
   })
 })
