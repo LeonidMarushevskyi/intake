657d4ff9a65d7f0668f820b70e0740471fc16b77
Nate Jones and Siva Mukunda <pair+Nate.Jones+Siva.Mukunda@OSI.ca.gov>
2016-10-24 10:16:01 -0700

    Cleanup screening show page spec
    [#refactor]

--- spec/javascripts/ScreeningShowPageSpec.jsx
+++ spec/javascripts/ScreeningShowPageSpec.jsx
@@ -11,33 +11,28 @@ describe('ScreeningShowPage', () => {
   })
 
   describe('render', () => {
-    it('renders the card header', () => {
-      const props = { params: { id: 1 } }
-      const wrapper = mount(<ScreeningShowPage {...props} />)
-
-      expect(wrapper.find('.card-header').text()).toContain('Narrative')
-    })
-
-    it('renders the screening narrative label fields', () => {
-      const props = { params: {} }
-      const wrapper = mount(<ScreeningShowPage {...props} />)
-
-      expect(wrapper.find('label').length).toEqual(1)
-      expect(wrapper.find('label').nodes.map((element) => element.textContent)).toEqual([
-        'Report Narrative'
-      ])
-    })
-
-    it('renders the screening value fields', () => {
-      const props = { params: {} }
-      const wrapper = mount(<ScreeningShowPage {...props} />)
-      wrapper.setState({
-        screening: Immutable.fromJS({
-          report_narrative: 'some narrative',
-        }),
+      it('renders the card header', () => {
+        const props = { params: { id: 1 } }
+        const wrapper = mount(<ScreeningShowPage {...props} />)
+        expect(wrapper.find('.card-header').text()).toContain('Narrative')
       })
 
-      expect(wrapper.text()).toContain('some narrative')
+      it('renders the screening narrative label fields', () => {
+        const props = { params: {} }
+        const wrapper = mount(<ScreeningShowPage {...props} />)
+        expect(wrapper.find('label').length).toEqual(1)
+        expect(wrapper.find('label').text()).toEqual('Report Narrative')
+      })
+
+      it('renders the screening value fields', () => {
+        const props = { params: {} }
+        const wrapper = mount(<ScreeningShowPage {...props} />)
+        wrapper.setState({
+          screening: Immutable.fromJS({
+            report_narrative: 'some narrative',
+          }),
+        })
+        expect(wrapper.text()).toContain('some narrative')
     })
   })
 
