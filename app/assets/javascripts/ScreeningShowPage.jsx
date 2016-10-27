86fbeb9d3ee6ced55a11c94b7e4e26a380fa2ed5
Nora Lin and Sri Harsha Poosa <pair+Nora.Lin+sri@OSI.ca.gov>
2016-10-27 11:07:34 -0700

    Convert "Screening Information Card" (show) to react
    [#132947033]

--- app/assets/javascripts/ScreeningShowPage.jsx
+++ app/assets/javascripts/ScreeningShowPage.jsx
@@ -2,14 +2,20 @@ import * as Utils from 'utils/http'
 import Immutable from 'immutable'
 import React from 'react'
 import ParticipantShowView from 'ParticipantShowView'
+import CommunicationMethod from 'CommunicationMethod'
+import moment from 'moment'
 
 export default class ScreeningShowPage extends React.Component {
   constructor() {
     super(...arguments)
     this.state = {
       screening: Immutable.fromJS({
-        report_narrative: '',
+        name: '',
+        started_at: '',
+        ended_at: '',
+        communication_method: '',
         participants: [],
+        report_narrative: '',
       }),
     }
     this.fetch = this.fetch.bind(this)
@@ -34,8 +40,48 @@ export default class ScreeningShowPage extends React.Component {
         {
           screening.get('participants').map((participant) =>
             <ParticipantShowView key={participant.get('id')} participant= {participant} />
-            )
+          )
         }
+      </div>
+    )
+  }
+
+  parseDateTime(dateTime) {
+    return (dateTime === null ? '' : moment.utc(dateTime).format('MM/DD/YYYY hh:mm A'))
+  }
+
+  renderScreeningInformationCard() {
+    const {screening} = this.state
+
+    return (
+      <div className='card double-gap-top' id='screening-information-card'>
+        <div className='card-header'>
+          <span>Screening Information</span>
+        </div>
+        <div className='card-body'>
+          <div className='row'>
+            <div className='col-md-6'>
+              <label className='no-gap'>Title/Name of Screening</label>
+              <div className='c-gray'>{screening.get('name')}</div>
+            </div>
+          </div>
+          <div className='row double-gap-top'>
+            <div className='col-md-6'>
+              <label className='no-gap'>Screening Start Date/Time</label>
+              <div className='c-gray'>{this.parseDateTime(screening.get('started_at'))}</div>
+            </div>
+            <div className='col-md-6'>
+              <label className='no-gap'>Screening End Date/Time</label>
+              <div className='c-gray'>{this.parseDateTime(screening.get('ended_at'))}</div>
+            </div>
+          </div>
+          <div className='row double-gap-top'>
+            <div className='col-md-6'>
+              <label className='no-gap'>Communication Method</label>
+              <div className='c-gray'>{CommunicationMethod[screening.get('communication_method')]}</div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
@@ -62,6 +108,7 @@ export default class ScreeningShowPage extends React.Component {
   render() {
     return (
       <div>
+        {this.renderScreeningInformationCard()}
         {this.renderParticipantsCard()}
         {this.renderNarrativeCard()}
       </div>
