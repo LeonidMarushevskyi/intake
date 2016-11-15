0571df24b56027a5b83ec41c85ced5b2f61e1fd0
Nora Lin and Siva Mukunda <pair+Nora.Lin+Siva.Mukunda@OSI.ca.gov>
2016-11-15 15:39:35 -0800

    Add integration test for store to ensure person gets updated when createPersonSuccess is dispatched
    [#133044843]

--- spec/javascripts/store/storeSpec.js
+++ spec/javascripts/store/storeSpec.js
@@ -33,4 +33,27 @@ describe('Store', () => {
 
     expect(store.getState().person).toEqual(person)
   })
+
+  it('handles create person', () => {
+    expect(store.getState()).toEqual(initialState)
+    const person = Immutable.fromJS({
+      id: 1,
+      first_name: 'Kevin',
+      last_name: 'McCallister',
+      gender: 'male',
+      date_of_birth: '11/16/1990',
+      ssn: '111223333',
+      address: {
+        id: 3,
+        street_address: '671 Lincoln Avenue',
+        city: 'Winnetka',
+        state: 'IL',
+        zip: 60093,
+      },
+    })
+    const action = personActions.createPersonSuccess(person)
+    store.dispatch(action)
+
+    expect(store.getState().person).toEqual(person)
+  })
 })
