5d0370028a3d72c52b51be21dd61cf6c27998fd4
Nate Jones and Sri Harsha Poosa <pair+Nate.Jones+Sri.Poosa@OSI.ca.gov>
2016-11-28 11:22:01 -0800

    Use input/select/date field components in information edit view
    [#133272507]

--- app/assets/javascripts/components/screenings/InformationEditView.jsx
+++ app/assets/javascripts/components/screenings/InformationEditView.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import COMMUNICATION_METHOD from 'CommunicationMethod'
+import DateField from 'components/common/DateField'
+import InputField from 'components/common/InputField'
+import SelectField from 'components/common/SelectField'
 
 const InformationEditView = ({screening, onChange}) => (
   <div className='card edit double-gap-top' id='screening-information-card'>
@@ -8,48 +11,43 @@ const InformationEditView = ({screening, onChange}) => (
     </div>
     <div className='card-body'>
       <div className='row'>
-        <div className='col-md-6'>
-          <label className='no-gap' htmlFor='name'>Title/Name of Screening</label>
-          <input
-            id='name'
-            placeholder='Enter name of the screening'
-            value={screening.get('name') || ''}
-            onChange={(event) => onChange(['name'], event.target.value)}
-          />
-        </div>
+        <InputField
+          wrapperClassName='col-md-6'
+          labelClassName='no-gap'
+          id='name'
+          label='Title/Name of Screening'
+          placeholder='Enter name of the screening'
+          value={screening.get('name') || ''}
+          onChange={(event) => onChange(['name'], event.target.value)}
+        />
       </div>
       <div className='row'>
-        <div className='col-md-6'>
-          <label htmlFor='started_at'>Screening Start Date/Time</label>
-          <input
-            type='datetime'
-            id='started_at'
-            value={screening.get('started_at') || ''}
-            onChange={(event) => onChange(['started_at'], event.target.value)}
-          />
+        <DateField
+          wrapperClassName='col-md-6'
+          id='started_at'
+          label='Screening Start Date/Time'
+          value={screening.get('started_at') || ''}
+          onChange={(event) => onChange(['started_at'], event.target.value)}
+        />
+        <DateField
+          wrapperClassName='col-md-6'
+          id='ended_at'
+          label='Screening End Date/Time'
+          value={screening.get('ended_at') || ''}
+          onChange={(event) => onChange(['ended_at'], event.target.value)}
+        />
         </div>
-        <div className='col-md-6'>
-          <label htmlFor='ended_at'>Screening End Date/Time</label>
-          <input
-            type='datetime'
-            id='ended_at'
-            value={screening.get('ended_at') || ''}
-            onChange={(event) => onChange(['ended_at'], event.target.value)}
-          />
-        </div>
-      </div>
-      <div className='row'>
-        <div className='col-md-6'>
-          <label htmlFor='communication_method'>Communication Method</label>
-          <select
+        <div className='row'>
+          <SelectField
+            wrapperClassName='col-md-6'
             id='communication_method'
+            label='Communication Method'
             value={screening.get('communication_method') || ''}
             onChange={(event) => onChange(['communication_method'], event.target.value)}
           >
             <option key='' value=''></option>
             {Object.keys(COMMUNICATION_METHOD).map((item) => <option key={item} value={item}>{COMMUNICATION_METHOD[item]}</option>)}
-          </select>
-        </div>
+          </SelectField>
       </div>
     </div>
   </div>
