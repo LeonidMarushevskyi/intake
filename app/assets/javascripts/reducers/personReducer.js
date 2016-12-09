2cf24057571e615f3078adc1cbcac1c2982d7bc1
Nate Jones and Thomas Ramirez <pair+Nate.Jones+Thomas.Ramirez@OSI.ca.gov>
2016-12-09 15:28:20 -0800

    Cleanup person reducer
    [#135177441]

--- app/assets/javascripts/reducers/personReducer.js
+++ app/assets/javascripts/reducers/personReducer.js
@@ -4,9 +4,7 @@ import Immutable from 'immutable'
 export default function personReducer(state = Immutable.Map(), action) {
   switch (action.type) {
     case types.FETCH_PERSON_SUCCESS:
-      return action.person
     case types.CREATE_PERSON_SUCCESS:
-      return action.person
     case types.UPDATE_PERSON_SUCCESS:
       return action.person
     default:
