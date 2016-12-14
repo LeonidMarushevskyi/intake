3c038884d6a0082214d0244a969a9ddff8d9574a
Nate Jones <Nate.Jones@OSI.ca.gov>
2016-12-14 15:50:22 -0800

    Add create screening action to screening reducer
    [#135177441]

--- app/assets/javascripts/reducers/screeningReducer.js
+++ app/assets/javascripts/reducers/screeningReducer.js
@@ -3,6 +3,7 @@ import Immutable from 'immutable'
 
 export default function screeningReducer(state = Immutable.Map(), action) {
   switch (action.type) {
+    case types.CREATE_SCREENING_SUCCESS:
     case types.FETCH_SCREENING_SUCCESS:
     case types.UPDATE_SCREENING_SUCCESS:
       return action.screening
--- spec/javascripts/reducers/screeningReducerSpec.js
+++ spec/javascripts/reducers/screeningReducerSpec.js
@@ -3,6 +3,14 @@ import screeningReducer from 'reducers/screeningReducer'
 import Immutable from 'immutable'
 
 describe('screeningReducer', () => {
+  describe('on CREATE_SCREENING_SUCCESS', () => {
+    it('returns the screening from the action', () => {
+      const screening = Immutable.Map({id: 1, name: 'mock_screening'})
+      const action = screeningActions.createScreeningSuccess(screening)
+      expect(screeningReducer(Immutable.Map(), action)).toEqual(screening)
+    })
+  })
+
   describe('on FETCH_SCREENING_SUCCESS', () => {
     it('returns the screening from the action', () => {
       const screening = Immutable.Map({id: 1, name: 'mock_screening'})
