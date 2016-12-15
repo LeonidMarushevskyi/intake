796e2e5ae36bed5568798effc4575d4d55c4a42a
Nate Jones <Nate.Jones@OSI.ca.gov>
2016-12-14 16:12:52 -0800

    Remove non redux screening actions
    [#135177441]

--- app/assets/javascripts/actions/screeningActions.js
+++ app/assets/javascripts/actions/screeningActions.js
@@ -2,19 +2,6 @@ import * as Utils from 'utils/http'
 import * as types from 'actions/actionTypes'
 import Immutable from 'immutable'
 
-export function save(id, screening) {
-  const url = `/screenings/${id}.json`
-  return Utils.request('PUT', url, JSON.stringify({screening: screening}), {contentType: 'application/json'})
-}
-
-export function fetch(id) {
-  return Utils.request('GET', `/screenings/${id}.json`, null, {contentType: 'application/json'})
-}
-
-export function create() {
-  return Utils.request('POST', '/screenings.json')
-}
-
 export function createScreeningSuccess(screening) {
   return {type: types.CREATE_SCREENING_SUCCESS, screening: Immutable.fromJS(screening)}
 }
