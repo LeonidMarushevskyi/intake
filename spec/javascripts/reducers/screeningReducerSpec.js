0f2267e749b097ff801be825c8bb4dbafafb359a
Nate Jones <Nate.Jones@OSI.ca.gov>
2016-12-15 09:16:14 -0800

    Fix screening reducer spec setup
    screening is not Immutable object until after it's
    pass through success function
    
    [#135177441]

--- spec/javascripts/reducers/screeningReducerSpec.js
+++ spec/javascripts/reducers/screeningReducerSpec.js
@@ -5,25 +5,25 @@ import Immutable from 'immutable'
 describe('screeningReducer', () => {
   describe('on CREATE_SCREENING_SUCCESS', () => {
     it('returns the screening from the action', () => {
-      const screening = Immutable.Map({id: 1, name: 'mock_screening'})
+      const screening = {id: 1, name: 'mock_screening'}
       const action = screeningActions.createScreeningSuccess(screening)
-      expect(screeningReducer(Immutable.Map(), action)).toEqual(screening)
+      expect(screeningReducer(Immutable.Map(), action).toJS()).toEqual(screening)
     })
   })
 
   describe('on FETCH_SCREENING_SUCCESS', () => {
     it('returns the screening from the action', () => {
-      const screening = Immutable.Map({id: 1, name: 'mock_screening'})
+      const screening = {id: 1, name: 'mock_screening'}
       const action = screeningActions.fetchScreeningSuccess(screening)
-      expect(screeningReducer(Immutable.Map(), action)).toEqual(screening)
+      expect(screeningReducer(Immutable.Map(), action).toJS()).toEqual(screening)
     })
   })
 
   describe('on UPDATE_SCREENING_SUCCESS', () => {
     it('returns the screening from the action', () => {
-      const screening = Immutable.Map({id: 1, name: 'mock_screening'})
+      const screening = {id: 1, name: 'mock_screening'}
       const action = screeningActions.updateScreeningSuccess(screening)
-      expect(screeningReducer(Immutable.Map(), action)).toEqual(screening)
+      expect(screeningReducer(Immutable.Map(), action).toJS()).toEqual(screening)
     })
   })
 })
