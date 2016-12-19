323bf80df0818362bc3dfc5c1c787bdaca9126f8
Nate Jones and Nora Lin <pair+Nate.Jones+Nora.Lin@OSI.ca.gov>
2016-12-19 11:51:25 -0800

    Refactor
    - Extract person suggestion to its own view
    - Change to using Utils request for consistency
    
    [#136082227]

--- app/assets/javascripts/components/common/Autocompleter.jsx
+++ app/assets/javascripts/components/common/Autocompleter.jsx
@@ -1,8 +1,7 @@
-import $ from 'jquery'
+import * as Utils from 'utils/http'
+import PersonSuggestion from 'components/common/PersonSuggestion'
 import React from 'react'
 import ReactAutosuggest from 'react-autosuggest'
-import moment from 'moment'
-import Gender from 'Gender'
 
 export default class Autocompleter extends React.Component {
   constructor(props) {
@@ -16,18 +15,14 @@ export default class Autocompleter extends React.Component {
   }
 
   loadSuggestions(value) {
-    this.setState({
-      isLoading: true,
-    })
-
-    var self = this
-    $.get('/people/search', {query: value})
-      .done((result) => {
-        self.setState({
+    this.setState({isLoading: true})
+    Utils.request('GET', '/people/search', {query: value})
+      .then((result) =>
+        this.setState({
           isLoading: false,
           suggestions: result,
         })
-      })
+      )
   }
 
   onChange(event, {newValue}) {
@@ -58,50 +53,24 @@ export default class Autocompleter extends React.Component {
     return `${suggestion.first_name} ${suggestion.last_name}`
   }
 
-  renderSSN(ssn) {
-    return (
-      ssn ? <div><strong className='c-gray half-pad-right'>SSN</strong><span>{ssn}</span></div> : null
-    )
-  }
-
-  renderAddress(address) {
-    let addressIcon
-    let addressInfo
-    let addressType
-
-    if (address) {
-      const {type, street_address, city, state, zip} = address
-      const state_zip = [state, zip].filter(Boolean).join(' ')
-      addressIcon = <i className='fa fa-map-marker c-gray half-pad-right' />
-      addressType = type ? <strong className='c-gray half-pad-right'>{type}</strong> : null
-      addressInfo = <span>{[street_address, city, state_zip].filter(Boolean).join(', ')}</span>
-    }
-    return <div>{addressIcon}{addressType}{addressInfo}</div>
-  }
-
   renderSuggestion(suggestion) {
-    const {first_name, last_name, date_of_birth, gender, ssn, address} = suggestion
-    let ageInfo
-    const dob = moment(date_of_birth, 'YYYY-MM-DD')
-    if (dob.isValid()) {
-      const ageInYears = moment().diff(dob, 'years')
-      ageInfo = `${ageInYears} yrs old (${dob.format('M/D/YYYY')})`
+    const {first_name, last_name, gender, date_of_birth, ssn, address} = suggestion
+    const addressInfo = {
+      city: address.city,
+      state: address.state,
+      streetAddress: address.street_address,
+      type: address.type,
+      zip: address.zip,
     }
     return (
-      <div className='row'>
-        <div className='col-md-2'>
-          <img src='/assets/default-profile.svg' />
-        </div>
-        <div className='col-md-4'>
-          <strong>{[first_name, last_name].filter(Boolean).join(' ')}</strong>
-          <div>{Gender[gender]}</div>
-          <div>{ageInfo}</div>
-          {this.renderSSN(ssn)}
-        </div>
-        <div className='col-md-6'>
-          {this.renderAddress(address)}
-        </div>
-      </div>
+      <PersonSuggestion
+        firstName={first_name}
+        lastName={last_name}
+        gender={gender}
+        dateOfBirth={date_of_birth}
+        ssn={ssn}
+        address={addressInfo}
+      />
     )
   }
 
@@ -123,18 +92,16 @@ export default class Autocompleter extends React.Component {
       onChange: this.onChange.bind(this),
     }
     return (
-      <div>
-        <ReactAutosuggest
-          suggestions={suggestions}
-          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested.bind(this)}
-          onSuggestionsClearRequested={this.onSuggestionsClearRequested.bind(this)}
-          onSuggestionSelected={this.onSuggestionSelected.bind(this)}
-          getSuggestionValue={this.getSuggestionValue}
-          renderSuggestion={this.renderSuggestion.bind(this)}
-          inputProps={inputProps}
-          renderSuggestionsContainer={this.renderSuggestionsContainer}
-        />
-      </div>
+      <ReactAutosuggest
+        suggestions={suggestions}
+        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested.bind(this)}
+        onSuggestionsClearRequested={this.onSuggestionsClearRequested.bind(this)}
+        onSuggestionSelected={this.onSuggestionSelected.bind(this)}
+        getSuggestionValue={this.getSuggestionValue}
+        renderSuggestion={this.renderSuggestion.bind(this)}
+        inputProps={inputProps}
+        renderSuggestionsContainer={this.renderSuggestionsContainer}
+      />
     )
   }
 }
---
+++ app/assets/javascripts/components/common/PersonSuggestion.jsx
@@ -0,0 +1,56 @@
+import Gender from 'Gender'
+import React from 'react'
+import moment from 'moment'
+
+const AgeInfo = ({dateOfBirth}) => {
+  const dob = moment(dateOfBirth, 'YYYY-MM-DD')
+  const ageInYears = dob.isValid() && moment().diff(dob, 'years')
+  return (
+    dob.isValid() && <div>{`${ageInYears} yrs old (${dob.format('M/D/YYYY')})`}</div>
+  )
+}
+
+const AddressInfo = (address) => {
+  const {type, streetAddress, city, state, zip} = address
+  const stateZip = [state, zip].filter(Boolean).join(' ')
+  return (
+    <div>
+      <i className='fa fa-map-marker c-gray half-pad-right' />
+      {type && <strong className='c-gray half-pad-right'>{type}</strong>}
+      <span>{[streetAddress, city, stateZip].filter(Boolean).join(', ')}</span>
+    </div>
+  )
+}
+
+const PersonSuggestion = ({firstName, lastName, dateOfBirth, gender, ssn, address}) => (
+  <div className='row'>
+    <div className='col-md-2'>
+      <img src='/assets/default-profile.svg' />
+    </div>
+    <div className='col-md-4'>
+      <strong>{[firstName, lastName].filter(Boolean).join(' ')}</strong>
+      <AgeInfo dateOfBirth={dateOfBirth} />
+      {gender && <div>{Gender[gender]}</div>}
+      {ssn && <div><strong className='c-gray half-pad-right'>SSN</strong><span>{ssn}</span></div>}
+    </div>
+    <div className='col-md-6'>
+      <AddressInfo {...address} />
+    </div>
+  </div>
+)
+
+PersonSuggestion.propTypes = {
+  address: React.PropTypes.object({
+    city: React.PropTypes.string,
+    state: React.PropTypes.string,
+    streetAddress: React.PropTypes.string,
+    type: React.PropTypes.string,
+    zip: React.PropTypes.number,
+  }),
+  dateOfBirth: React.PropTypes.string,
+  firstName: React.PropTypes.string,
+  gender: React.PropTypes.string,
+  lastName: React.PropTypes.string,
+  ssn: React.PropTypes.string,
+}
+export default PersonSuggestion
