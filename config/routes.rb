c35f6c940aa3bb6c3cea814912236d97a203ca7d
Nate Jones <Nate.Jones@OSI.ca.gov>
2016-12-19 16:02:36 -0800

    Add release one constraint to routes
    [#135440229]

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,25 @@
 # frozen_string_literal: true
+require File.join(File.dirname(__FILE__), 'routes/inactive_release_one_constraint')
 
 Rails.application.routes.draw do
   root 'welcome#index'
-  resources :style_guide, only: [:index, :show]
-  resources :screenings, only: [:create, :edit, :show, :update, :index] do
+  resources :style_guide,
+    only: [:index, :show],
+    constraints: Routes::InactiveReleaseOneConstraint
+
+  resources :screenings,
+    only: [:create, :edit, :show, :update, :index],
+    constraints: Routes::InactiveReleaseOneConstraint do
     resources :participants, only: [:create]
   end
-  resources :people, only: [:new, :edit, :create, :show, :update] do
+
+  resource :people, only: [:search] do
     collection do
       get 'search'
     end
   end
+
+  resources :people,
+    only: [:new, :edit, :create, :show, :update],
+    constraints: Routes::InactiveReleaseOneConstraint
 end
