44c942a61b0f9e9400ea07c45e9e30a2f093fc25
Aaron Smith and Nate Jones <pair+Aaron.Smith+Nate.Jones@OSI.ca.gov>
2017-02-16 15:43:22 -0800

    Change screening create feature specs to match swagger docs
    [#139834803]

--- spec/features/screening/create_screening_spec.rb
+++ spec/features/screening/create_screening_spec.rb
@@ -7,19 +7,18 @@ feature 'Create Screening' do
     new_screening = FactoryGirl.build(
       :screening,
       created_at: nil,
-      id: nil,
       reference: 'DQJIYK',
       updated_at: nil,
       address: nil
     )
     stub_request(:post, api_screenings_path)
-      .with(body: new_screening.to_json)
-      .and_return(body: new_screening.as_json.merge(id: '1', address: {}).to_json,
+      .with(body: new_screening.to_json(except: :id))
+      .and_return(body: new_screening.to_json,
                   status: 201,
                   headers: { 'Content-Type' => 'application/json' })
 
     stub_request(:get, api_screening_path(1))
-      .and_return(body: new_screening.as_json.merge(id: '1', address: {}).to_json,
+      .and_return(body: new_screening.to_json,
                   status: 200,
                   headers: { 'Content-Type' => 'application/json' })
 
@@ -27,7 +26,7 @@ feature 'Create Screening' do
     click_link 'Start Screening'
 
     expect(
-      a_request(:post, api_screenings_path).with(body: new_screening.to_json)
+      a_request(:post, api_screenings_path).with(body: new_screening.to_json(except: :id))
     ).to have_been_made
     expect(page).to have_content('Edit Screening #DQJIYK')
   end
