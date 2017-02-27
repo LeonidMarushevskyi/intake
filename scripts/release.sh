ccf5d6d21a9a19ee5922c6d318731eeb09391109
Pema Geyleg <Pema.Geyleg@OSI.ca.gov>
2017-02-27 13:41:44 -0800

    Default value for SECRET_KEY_BASE and export it if needed
    [#140683719]

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-export SECRET_KEY_BASE=$(openssl rand -base64 32)
+: ${SECRET_KEY_BASE=$(openssl rand -base64 32)}
+env | grep -q ^SECRET_KEY_BASE= && export SECRET_KEY_BASE
 bundle exec puma -e production -C config/puma.rb
