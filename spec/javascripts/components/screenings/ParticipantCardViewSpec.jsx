39990a734ac85ac6fbe632c4fd91157003caba06
Sara Bocciardi and Siva Mukunda <pair+Sara.Bocciardi+Siva.Mukunda@OSI.ca.gov>
2017-03-15 15:25:11 -0700

    Participant can edit and save itself
    [#131918563]

--- spec/javascripts/components/screenings/ParticipantCardViewSpec.jsx
+++ spec/javascripts/components/screenings/ParticipantCardViewSpec.jsx
@@ -15,6 +15,11 @@ describe('ParticipantCardView', () => {
       expect(component.find('ParticipantShowView').length).toEqual(1)
     })
 
+    it('changes the mode when onEdit is called', () => {
+      component.instance().onEdit()
+      expect(component.instance().state.mode).toEqual('edit')
+    })
+
     describe("when a user clicks 'Edit participant'", () => {
       beforeEach(() => {
         const editLink = component.find('a[aria-label="Edit participant"]')
@@ -29,9 +34,23 @@ describe('ParticipantCardView', () => {
 
   describe('when mode is set to edit', () => {
     let component
+    const participantIndex = 2
+    const onCancel = jasmine.createSpy('onCancel')
+    const onChange = jasmine.createSpy('onCancel')
+    const onSave = jasmine.createSpy('onCancel')
+    const participant = Immutable.fromJS({id: '5', first_name: 'Tony', last_name: 'Hawk', ssn: 'ssn-1'})
+
     beforeEach(() => {
-      const participant = Immutable.fromJS({id: '5', first_name: 'Tony', last_name: 'Hawk'})
-      component = mount(<ParticipantCardView participant={participant} mode='edit'/>)
+      component = mount(
+        <ParticipantCardView
+          participant={participant}
+          mode='edit'
+          onCancel={onCancel}
+          onChange={onChange}
+          onSave={onSave}
+          index={participantIndex}
+        />
+      )
     })
 
     it('renders the participants edit view for each participant', () => {
@@ -41,7 +60,26 @@ describe('ParticipantCardView', () => {
     it("on edit when user hits 'Cancel' renders show view ", () => {
       const cancelButton = component.find('button[children="Cancel"]')
       cancelButton.simulate('click')
+      expect(onCancel).toHaveBeenCalledWith(participantIndex)
       expect(component.find('ParticipantShowView').length).toEqual(1)
+    })
+
+    it('calls onCancel from props and changes mode to show when onCancel is called', () => {
+      component.instance().onCancel()
+      expect(onCancel).toHaveBeenCalled()
+      expect(component.instance().state.mode).toEqual('show')
+    })
+
+    it('calls onSave from props and changes the mode to show when onSave is called', () => {
+      component.instance().onSave()
+      expect(onSave).toHaveBeenCalled()
+      expect(component.instance().state.mode).toEqual('show')
+    })
+
+    it('calls onChange from props with the appropriately changed values when onChange is called', () => {
+      const updatedParticipant = participant.setIn(['first_name'], 'Bart')
+      component.instance().onChange(['first_name'], 'Bart')
+      expect(onChange).toHaveBeenCalledWith(participantIndex, updatedParticipant)
     })
   })
 })
