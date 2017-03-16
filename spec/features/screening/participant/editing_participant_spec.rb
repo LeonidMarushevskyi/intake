122be2d5a3925f1b80df061cb6de7b9795071c20
Nate Jones <Nate.Jones@OSI.ca.gov>
2017-03-16 08:37:49 -0700

    Cleanup participant edit spec
    remove unused person let
    
    [#refactor]

--- spec/features/screening/participant/editing_participant_spec.rb
+++ spec/features/screening/participant/editing_participant_spec.rb
@@ -17,21 +17,16 @@ feature 'Edit Screening' do
       type: 'Home'
     )
   end
-  let(:person1_attributes) do
-    {
+  let(:participant1) do
+    FactoryGirl.create(
+      :participant,
+      person_id: 1,
       date_of_birth: 15.years.ago.to_date.to_s(:db),
       first_name: 'Marge',
       gender: 'female',
       last_name: 'Simpson',
       ssn: old_ssn,
       addresses: [address1]
-    }
-  end
-  let(:person1) { Person.new(person1_attributes) }
-  let(:participant1) do
-    FactoryGirl.build(
-      :participant,
-      person1_attributes.merge(person_id: person1.id)
     )
   end
 
@@ -45,21 +40,16 @@ feature 'Edit Screening' do
       type: 'Work'
     )
   end
-  let(:person2_attributes) do
-    {
+  let(:participant2) do
+    FactoryGirl.create(
+      :participant,
+      person_id: 2,
       date_of_birth: 25.years.ago.to_date.to_s(:db),
       first_name: 'Homer',
       gender: 'male',
       last_name: 'Simpson',
-      ssn: '999-99-9999',
+      ssn: old_ssn,
       addresses: [address2]
-    }
-  end
-  let(:person2) { Person.new(person1_attributes) }
-  let(:participant2) do
-    FactoryGirl.build(
-      :participant,
-      person2_attributes.merge(person_id: person2.id)
     )
   end
 
@@ -73,10 +63,10 @@ feature 'Edit Screening' do
   before do
     stub_request(:get, api_screening_path(screening.id))
       .and_return(
-        body: screening.to_json,
-        status: 200,
-        headers: { 'Content-Type' => 'application/json' }
-      )
+    body: screening.to_json,
+    status: 200,
+    headers: { 'Content-Type' => 'application/json' }
+    )
   end
 
   scenario 'editing and saving a participant for a screening saves only the relevant participant' do
@@ -90,16 +80,16 @@ feature 'Edit Screening' do
 
       within '.card-body' do
         expect(page).to have_selector("#address-#{address1.id}")
-        expect(page).to have_field('First Name', with: person1.first_name)
-        expect(page).to have_field('Last Name', with: person1.last_name)
-        expect(page).to have_field('Gender', with: person1.gender)
-        expect(page).to have_field('Date of birth', with: person1.date_of_birth)
-        expect(page).to have_field('Social security number', with: person1.ssn)
-        expect(page).to have_field('Address', with: person1.addresses.first.street_address)
-        expect(page).to have_field('City', with: person1.addresses.first.city)
-        expect(page).to have_field('State', with: person1.addresses.first.state)
-        expect(page).to have_field('Zip', with: person1.addresses.first.zip)
-        expect(page).to have_field('Address Type', with: person1.addresses.first.type)
+        expect(page).to have_field('First Name', with: participant1.first_name)
+        expect(page).to have_field('Last Name', with: participant1.last_name)
+        expect(page).to have_field('Gender', with: participant1.gender)
+        expect(page).to have_field('Date of birth', with: participant1.date_of_birth)
+        expect(page).to have_field('Social security number', with: participant1.ssn)
+        expect(page).to have_field('Address', with: participant1.addresses.first.street_address)
+        expect(page).to have_field('City', with: participant1.addresses.first.city)
+        expect(page).to have_field('State', with: participant1.addresses.first.state)
+        expect(page).to have_field('Zip', with: participant1.addresses.first.zip)
+        expect(page).to have_field('Address Type', with: participant1.addresses.first.type)
         expect(page).to have_button 'Cancel'
         expect(page).to have_button 'Save'
         fill_in 'Social security number', with: new_ssn
@@ -112,8 +102,8 @@ feature 'Edit Screening' do
       stub_request(:put, api_participant_path(participant1.id))
         .with(body: participant1.to_h.tap { |h| h.delete(:id) }.as_json)
         .and_return(status: 200,
-                    body: participant1.to_json,
-                    headers: { 'Content-Type' => 'application/json' })
+      body: participant1.to_json,
+      headers: { 'Content-Type' => 'application/json' })
     end
 
     within edit_participant_card_selector(participant2.id) do
@@ -155,11 +145,11 @@ feature 'Edit Screening' do
     within edit_participant_card_selector(participant1.id) do
       within '.card-body' do
         expect(page).to have_selector("#address-#{address1.id}")
-        expect(page).to have_field('Address', with: person1.addresses.first.street_address)
-        expect(page).to have_field('City', with: person1.addresses.first.city)
-        expect(page).to have_field('State', with: person1.addresses.first.state)
-        expect(page).to have_field('Zip', with: person1.addresses.first.zip)
-        expect(page).to have_field('Address Type', with: person1.addresses.first.type)
+        expect(page).to have_field('Address', with: participant1.addresses.first.street_address)
+        expect(page).to have_field('City', with: participant1.addresses.first.city)
+        expect(page).to have_field('State', with: participant1.addresses.first.state)
+        expect(page).to have_field('Zip', with: participant1.addresses.first.zip)
+        expect(page).to have_field('Address Type', with: participant1.addresses.first.type)
         expect(page).to have_button 'Cancel'
         expect(page).to have_button 'Save'
         fill_in 'City', with: 'New City'
@@ -170,8 +160,8 @@ feature 'Edit Screening' do
       stub_request(:put, api_participant_path(participant1.id))
         .with(body: participant1.to_h.tap { |h| h.delete(:id) }.as_json)
         .and_return(status: 200,
-                    body: participant1.to_json,
-                    headers: { 'Content-Type' => 'application/json' })
+      body: participant1.to_json,
+      headers: { 'Content-Type' => 'application/json' })
     end
 
     within edit_participant_card_selector(participant1.id) do
@@ -211,8 +201,8 @@ feature 'Edit Screening' do
     stub_request(:put, api_participant_path(participant1.id))
       .with(body: participant1.to_h.tap { |h| h.delete(:id) }.as_json)
       .and_return(status: 200,
-                  body: participant1.to_json,
-                  headers: { 'Content-Type' => 'application/json' })
+    body: participant1.to_json,
+    headers: { 'Content-Type' => 'application/json' })
 
     within edit_participant_card_selector(participant1.id) do
       within '.card-body' do
@@ -259,3 +249,5 @@ feature 'Edit Screening' do
     expect(page).to have_content old_ssn
   end
 end
+
+
