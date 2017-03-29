a7d0ef11f3e986b1d4853f167c27c59b558ee35e
Nate Jones and Sara Bocciardi <pair+Nate.Jones+Sara.Bocciardi@OSI.ca.gov>
2017-03-29 11:14:17 -0700

    Add feature spec for removing a participant from screening show
    Any allegations related to the remove participant show be removed from
    the allegations view.
    
    [#139255433]

--- spec/features/screening/allegations/show_allegations_spec.rb
+++ spec/features/screening/allegations/show_allegations_spec.rb
@@ -66,6 +66,56 @@ feature 'show allegations' do
     ).to have_been_made
   end
 
+  scenario 'deleting a participant removes related allegations' do
+    marge = FactoryGirl.create(:participant, first_name: 'Marge', roles: ['Perpetrator'])
+    lisa = FactoryGirl.create(:participant, first_name: 'Lisa', roles: ['Victim'])
+    homer = FactoryGirl.create(:participant, first_name: 'Homer', roles: ['Perpetrator'])
+    screening = FactoryGirl.create(
+      :screening,
+      participants: [marge, homer, lisa]
+    )
+    allegation = FactoryGirl.create(
+      :allegation,
+      victim_id: lisa.id,
+      perpetrator_id: marge.id,
+      screening_id: screening.id
+    )
+    screening.allegations << allegation
+
+    stub_request(:get, api_screening_path(screening.id))
+      .and_return(json_body(screening.to_json, status: 200))
+
+    visit screening_path(id: screening.id)
+
+    within '#allegations-card.card.show' do
+      within 'tbody tr' do
+        expect(page).to have_content('Marge')
+        expect(page).to have_content('Lisa')
+      end
+    end
+
+    within show_participant_card_selector(marge.id) do
+      click_button 'Delete participant'
+    end
+
+    within '#allegations-card.card.show' do
+      within 'tbody', visible: false do
+        expect(page).to_not have_content('Marge')
+        expect(page).to_not have_content('Lisa')
+      end
+
+      click_link 'Edit allegations'
+    end
+
+    within '#allegations-card.card.edit' do
+      within 'tbody' do
+        expect(page).to have_content('Lisa')
+        expect(page).to have_content('Homer')
+        expect(page).to_not have_content('Marge')
+      end
+    end
+  end
+
   scenario 'saving another card will not persists changes to allegations' do
     marge = FactoryGirl.create(:participant, first_name: 'Marge', roles: ['Perpetrator'])
     lisa = FactoryGirl.create(:participant, first_name: 'Lisa', roles: ['Victim'])
