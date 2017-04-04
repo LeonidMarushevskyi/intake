8fded45e775cb630621aa3097985eb7035259259
Sara Bocciardi and Thomas Ramirez <pair+Sara.Bocciardi+Thomas.Ramirez@OSI.ca.gov>
2017-04-04 10:44:14 -0700

    Add react select component to allegation row
    [#139255933]

--- app/assets/javascripts/components/screenings/AllegationRow.jsx
+++ app/assets/javascripts/components/screenings/AllegationRow.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 import nameFormatter from 'utils/nameFormatter'
+import Select from 'react-select'
 
 const AllegationRow = ({victim, perpetrator, displayVictim}) => (
   <tr>
     <td><strong>{displayVictim ? nameFormatter(victim) : ''}</strong></td>
     <td>{nameFormatter(perpetrator)}</td>
-    <td />
+    <td>
+      <Select
+        aria-label={`allegations ${nameFormatter(victim)} ${nameFormatter(perpetrator)}`}
+        multi
+        inputProps={{id: `allegations_${victim.get('id')}_${perpetrator.get('id')}`}}
+        value={''}
+        onChange={() => null}
+        options={[
+          {label: 'General neglect', value: 'General neglect'},
+          {label: 'Severe neglect', value: 'Severe neglect'},
+          {label: 'Physical abuse', value: 'Physical abuse'},
+          {label: 'Sexual abuse', value: 'Sexual abuse'},
+          {label: 'Emotional abuse', value: 'Emotional abuse'},
+          {label: 'Caretaker absent/incapacity', value: 'Caretaker absent/incapacity'},
+          {label: 'Exploitation', value: 'Exploitation'},
+          {label: 'Sibling at risk', value: 'Sibling at risk'},
+        ]}
+        clearable={false}
+        placeholder=''
+      />
+    </td>
   </tr>
 )
 
--- spec/javascripts/components/screenings/AllegationRowSpec.jsx
+++ spec/javascripts/components/screenings/AllegationRowSpec.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import AllegationRow from 'components/screenings/AllegationRow'
 import Immutable from 'immutable'
-import {shallow} from 'enzyme'
+import {mount, shallow} from 'enzyme'
 
 describe('AllegationRow', () => {
-  const bart = Immutable.fromJS({first_name: 'Bart', last_name: 'Simpson'})
-  const homer = Immutable.fromJS({first_name: 'Homer', last_name: 'Simpson'})
+  const bart = Immutable.fromJS({id: '123', first_name: 'Bart', last_name: 'Simpson'})
+  const homer = Immutable.fromJS({id: '456', first_name: 'Homer', last_name: 'Simpson'})
 
   const requiredProps = {
     victim: bart,
@@ -22,5 +22,21 @@ describe('AllegationRow', () => {
     const component = shallow(<AllegationRow {...requiredProps} displayVictim={false} />)
     expect(component.childAt(0).text()).toEqual('')
     expect(component.childAt(1).text()).toEqual('Homer Simpson')
+  })
+
+  it('displays allegation types', () => {
+    const component = mount(<AllegationRow {...requiredProps} displayVictim={true} />)
+    expect(component.find('Select').length).toEqual(1)
+    expect(component.find('Select').props()['aria-label']).toEqual('allegations Bart Simpson Homer Simpson')
+    expect(component.find('Select').props().options).toEqual([
+      {label: 'General neglect', value: 'General neglect'},
+      {label: 'Severe neglect', value: 'Severe neglect'},
+      {label: 'Physical abuse', value: 'Physical abuse'},
+      {label: 'Sexual abuse', value: 'Sexual abuse'},
+      {label: 'Emotional abuse', value: 'Emotional abuse'},
+      {label: 'Caretaker absent/incapacity', value: 'Caretaker absent/incapacity'},
+      {label: 'Exploitation', value: 'Exploitation'},
+      {label: 'Sibling at risk', value: 'Sibling at risk'},
+    ])
   })
 })
