2c01af8495545e342d0529666c9ffcae92ced149
Aaron Smith and Sara Bocciardi <pair+Aaron.Smith+Sara.Bocciardi@OSI.ca.gov>
2017-04-05 16:41:42 -0700

    Add endpoint to display current Git sha of codebase
    [#143213615]

---
+++ spec/controllers/version_controller_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+describe VersionController do
+  describe '#index' do
+    let(:sha) { SecureRandom.hex(36) }
+
+    it 'gets the sha of the latest git commit' do
+      allow(subject).to receive(:`).and_return sha
+      process :index, method: :get
+      assert_response :success
+      expect(response.body).to eq(sha)
+    end
+
+    it 'does not break if git is not installed on system' do
+      allow(subject).to receive(:`).and_return `(exit 127)`
+      process :index, method: :get
+      assert_response :success
+      expect(response.body).to eq('Git not found')
+    end
+  end
+end
