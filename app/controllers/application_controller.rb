24bd9d8d153861a60e04fa9b298fe0a553510f7d
Bruno Fagundez <Bruno.Fagundez@OSI.ca.gov>
2017-04-11 17:30:57 -0700

    Refactor auth logic to ApplicationController
    [#143527039]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,28 @@ class ApplicationController < ActionController::Base # :nodoc:
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :gulp_asset_path
+
+  private
+
+  def authenticate_user
+    return if session[:security_token] && set_api_header
+    if SecurityRepository.token_valid?(security_token)
+      session[:security_token] = security_token
+      set_api_header
+    else
+      redirect_to SecurityRepository.login_url(request.original_url)
+    end
+  end
+
+  def set_api_header
+    ::API.intake_api_connection.headers['Authorization'] = session[:security_token]
+  end
+
+  def security_token
+    params[:token]
+  end
+
+  def authentication_enabled?
+    Feature.active?(:authentication)
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,30 +4,5 @@
 # navigating the landing page.
 class HomeController < ApplicationController # :nodoc:
   before_action :authenticate_user, if: :authentication_enabled?
-
   def index; end
-
-  private
-
-  def authenticate_user
-    return if session[:security_token] && set_api_header
-    if SecurityRepository.token_valid?(security_token)
-      session[:security_token] = security_token
-      set_api_header
-    else
-      redirect_to SecurityRepository.login_url(request.original_url)
-    end
-  end
-
-  def set_api_header
-    ::API.intake_api_connection.headers['Authorization'] = session[:security_token]
-  end
-
-  def security_token
-    params[:token]
-  end
-
-  def authentication_enabled?
-    Feature.active?(:authentication)
-  end
 end
