1a607a512ca58311a33a8f513cc415b3498f1dcf
Nate Jones <Nate.Jones@OSI.ca.gov>
2017-04-13 09:01:50 -0700

    Remove release_one from authentication spec
    [#refactor]

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -3,20 +3,22 @@ require 'rails_helper'
 require 'feature/testing'
 
 feature 'login' do
-  before do
-    allow(ENV).to receive(:fetch).with('AUTHENTICATION_URL')
-      .and_return('http://www.foo.com')
+  around do |example|
+    authentication_url = ENV['AUTHENTICATION_URL']
+    ENV['AUTHENTICATION_URL'] = 'http://www.foo.com'
+    example.run
+    ENV['AUTHENTICATION_URL'] = authentication_url
   end
 
   scenario 'user has not logged in' do
-    Feature.run_with_activated(:release_one, :authentication) do
+    Feature.run_with_activated(:authentication) do
       visit root_path
       expect(page.current_url).to have_content 'http://www.foo.com/authn/login'
     end
   end
 
   scenario 'user provides valid security token' do
-    Feature.run_with_activated(:release_one, :authentication) do
+    Feature.run_with_activated(:authentication) do
       stub_request(:get, 'http://www.foo.com/authn/validate?token=123').and_return(status: 200)
       visit root_path(token: 123)
       expect(a_request(:get, 'http://www.foo.com/authn/validate?token=123')).to have_been_made
@@ -26,7 +28,7 @@ feature 'login' do
   end
 
   scenario 'user provides invalid security token' do
-    Feature.run_with_activated(:release_one, :authentication) do
+    Feature.run_with_activated(:authentication) do
       stub_request(:get, 'http://www.foo.com/authn/validate?token=123').and_return(status: 401)
       visit root_path(token: 123)
       expect(a_request(:get, 'http://www.foo.com/authn/validate?token=123')).to have_been_made
@@ -35,7 +37,7 @@ feature 'login' do
   end
 
   scenario 'user has already logged in' do
-    Feature.run_with_activated(:release_one, :authentication) do
+    Feature.run_with_activated(:authentication) do
       login
       visit root_path
       expect(a_request(:get, %r{http://www.foo.com})).to_not have_been_made
