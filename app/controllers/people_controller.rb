8f46a02f0bb9cc6824b25acf8430e2a11d42015a
Nate Jones <Nate.Jones@OSI.ca.gov>
2017-04-13 13:16:24 -0700

    Add authentication to people controller
    [#143069965]

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,6 +3,7 @@
 # People Controller handles all service request for
 # the creation and modification of person objects.
 class PeopleController < ApplicationController
+  before_action :authenticate_user, if: :authentication_enabled?
   def new
     render :show
   end
