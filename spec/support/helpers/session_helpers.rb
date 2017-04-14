af87fec5eaba66f1fb5131dd6c96c1c81cfc68c6
Nate Jones and Sara Bocciardi <pair+Nate.Jones+Sara.Bocciardi@OSI.ca.gov>
2017-04-13 17:30:38 -0700

    Stub rails config in login features
    [#139870345]

--- spec/support/helpers/session_helpers.rb
+++ spec/support/helpers/session_helpers.rb
@@ -1,22 +1,11 @@
 # frozen_string_literal: true
 
 module SessionHelpers
-  def login
-    stub_request(:get, token_url).and_return(status: 200)
-    visit root_path(token: 123)
-    expect(a_request(:get, token_url)).to have_been_made.once
-    WebMock.reset!
-  end
-
   def in_browser(name)
     old_session = Capybara.session_name
     Capybara.session_name = name
     yield
     Capybara.session_name = old_session
-  end
-
-  def token_url
-    "#{Rails.configuration.intake[:authentication_base_url]}/authn/validate?token=123"
   end
 end
 
