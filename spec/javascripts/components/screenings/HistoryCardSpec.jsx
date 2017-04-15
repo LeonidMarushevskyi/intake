6658a230e2531ab9de4ed7841b4c11d325744209
Nate Jones <Nate.Jones@OSI.ca.gov>
2017-04-15 10:12:21 -0700

    Use enzyme instead of react test-utils

--- spec/javascripts/components/screenings/HistoryCardSpec.jsx
+++ spec/javascripts/components/screenings/HistoryCardSpec.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import HistoryCard from 'components/screenings/HistoryCard'
-import TestUtils from 'react-addons-test-utils'
+import {shallow} from 'enzyme'
 
 describe('HistoryCard', () => {
   it('renders history card headings', () => {
-    const view = TestUtils.renderIntoDocument(<HistoryCard />)
-    const tr = TestUtils.findRenderedDOMComponentWithTag(view, 'tr')
-    expect(tr.textContent).toContain('Date')
-    expect(tr.textContent).toContain('Type/Status')
-    expect(tr.textContent).toContain('County/Office')
-    expect(tr.textContent).toContain('People and Roles')
+    const component = shallow(<HistoryCard />)
+    const tr = component.find('tr')
+    expect(tr.text()).toContain('Date')
+    expect(tr.text()).toContain('Type/Status')
+    expect(tr.text()).toContain('County/Office')
+    expect(tr.text()).toContain('People and Roles')
   })
 })
