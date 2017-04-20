c6f4981855919a4c6b9d6edb6266b23e4d3853fc
Aaron Smith and Nate Jones <pair+Aaron.Smith+Nate.Jones@OSI.ca.gov>
2017-04-20 14:18:55 -0700

    Refactor login spec for stability in dev environment
    [#143818717]

--- spec/support/assets_gulp.rb
+++ spec/support/assets_gulp.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 RSpec.configure do |config|
-  config.before(:suite) do |example|
+  config.before(:suite) do |_example|
     raise 'Failed to build modular JS assets!' unless system 'bin/gulp'
   end
 end
