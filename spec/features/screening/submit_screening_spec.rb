784519315361a602a292b5329062fee9e715e6ef
Nate Jones <Nate.Jones@OSI.ca.gov>
2017-05-05 15:19:25 -0700

    Fix brittle submit screening spec
    [#143996393]

--- spec/features/screening/submit_screening_spec.rb
+++ spec/features/screening/submit_screening_spec.rb
@@ -7,74 +7,94 @@ require 'feature/testing'
 feature 'Submit Screening' do
   let(:existing_screening) { FactoryGirl.create(:screening) }
   before do
+    no_screenings = []
     stub_request(:get, intake_api_screening_url(existing_screening.id))
       .and_return(json_body(existing_screening.to_json, status: 200))
+    stub_request(:get, intake_api_screenings_url)
+      .and_return(json_body(no_screenings.to_json, status: 200))
   end
 
-  context 'when successfully submmitting referral' do
-    before do
-      stub_request(:post, intake_api_screening_submit_url(existing_screening.id))
-        .and_return(status: 200)
-    end
-
-    scenario 'displays a success alert and submits a screening to the API' do
+  context 'when referral submit is activated' do
+    around do |example|
       Feature.run_with_activated(:referral_submit) do
-        visit edit_screening_path(existing_screening.id)
-        click_button 'Submit'
-        expect(
-          a_request(:post, intake_api_screening_submit_url(existing_screening.id))
-        ).to have_been_made
-        expect(page.driver.browser.switch_to.alert.text).to eq('Successfully submitted screening')
-        page.driver.browser.switch_to.alert.accept
+        example.run
       end
     end
-  end
 
-  context 'when error submitting referral' do
-    let(:error_json) { 'Unable to process JSON' }
-    before do
-      stub_request(:post, intake_api_screening_submit_url(existing_screening.id))
-        .and_return(json_body(error_json, status: 400))
+    context 'when successfully submmitting referral' do
+      before do
+        stub_request(:post, intake_api_screening_submit_url(existing_screening.id))
+          .and_return(status: 200)
+      end
+
+      scenario 'displays a success modal and submits a screening to the API' do
+        visit edit_screening_path(existing_screening.id)
+        click_button 'Submit'
+
+        expect(
+          a_request(:post, intake_api_screening_submit_url(existing_screening.id))
+        ).to have_been_made
+
+        expect(alert_dialog.text).to eq('Successfully submitted screening')
+        alert_dialog.accept
+
+        within '#submitModal' do
+          expect(page).to have_content 'You have completed the process to submit a screening.'
+        end
+      end
     end
 
-    scenario 'displays alert with the error responseText' do
-      Feature.run_with_activated(:referral_submit) do
+    context 'when error submitting referral' do
+      let(:error_json) { 'Unable to process JSON' }
+      before do
+        stub_request(:post, intake_api_screening_submit_url(existing_screening.id))
+          .and_return(json_body(error_json, status: 400))
+      end
+
+      scenario 'displays a success modal and alert with the error responseText' do
         visit edit_screening_path(existing_screening.id)
         click_button 'Submit'
         expect(
           a_request(:post, intake_api_screening_submit_url(existing_screening.id))
         ).to have_been_made
-        expect(page.driver.browser.switch_to.alert.text).to include(error_json)
-        page.driver.browser.switch_to.alert.accept
+
+        expect(alert_dialog.text).to include(error_json)
+        alert_dialog.accept
+
+        within '#submitModal' do
+          expect(page).to have_content 'You have completed the process to submit a screening.'
+        end
       end
     end
   end
 
   context 'when referral submit is deactivated' do
-    scenario 'The user submits and clicks proceed' do
+    around do |example|
       Feature.run_with_deactivated(:referral_submit) do
-        visit edit_screening_path(existing_screening.id)
-        click_button 'Submit'
+        example.run
+      end
+    end
 
-        within '#submitModal' do
-          expect(page).to have_content 'You have completed the process to submit a screening.'
-          click_button 'Proceed'
-          expect(page).to have_current_path('/')
-        end
+    scenario 'The user submits and clicks proceed' do
+      visit edit_screening_path(existing_screening.id)
+      click_button 'Submit'
+
+      within '#submitModal' do
+        expect(page).to have_content 'You have completed the process to submit a screening.'
+        click_button 'Proceed'
+        expect(page).to have_current_path('/')
       end
     end
 
     scenario 'The user submits the screening and clicks cancel' do
-      Feature.run_with_deactivated(:referral_submit) do
-        visit edit_screening_path(existing_screening.id)
-        click_button 'Submit'
+      visit edit_screening_path(existing_screening.id)
+      click_button 'Submit'
 
-        within '#submitModal' do
-          within '.modal-footer' do
-            click_button 'Close'
-          end
-          expect(page).to have_current_path(edit_screening_path(id: existing_screening.id))
+      within '#submitModal' do
+        within '.modal-footer' do
+          click_button 'Close'
         end
+        expect(page).to have_current_path(edit_screening_path(id: existing_screening.id))
       end
     end
   end
