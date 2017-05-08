8c29688dc37fc23cb9efa08af7cca3d98bb4e5c7
Aman Alshurafa and Sara Bocciardi <pair+Aman.Alshurafa+Sara.Bocciardi@OSI.ca.gov>
2017-05-08 15:48:03 -0700

    Render component using new json data structure
    [#143891865]

--- app/assets/javascripts/components/screenings/HistoryCard.jsx
+++ app/assets/javascripts/components/screenings/HistoryCard.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import moment from 'moment'
 import nameFormatter from 'utils/nameFormatter'
-import {ROLE_TYPE_REPORTER, ROLE_TYPE_NON_REPORTER} from 'RoleType'
+import {ROLE_TYPE_NON_REPORTER} from 'RoleType'
 
 export default class HistoryCard extends React.Component {
   constructor(props, context) {
@@ -44,22 +44,18 @@ export default class HistoryCard extends React.Component {
                 <tbody>
                   {
                     this.props.involvements.map((involvement, index) => {
-                      const startedAt = involvement.get('started_at')
-                      const endedAt = involvement.get('ended_at')
-                      const incidentCounty = involvement.get('incident_county')
-                      const participants = involvement.get('participants')
-                      const assignee = involvement.get('assignee')
-                      const reporterTypes = Immutable.fromJS(ROLE_TYPE_REPORTER)
+                      const startedAt = involvement.get('start_date')
+                      const endedAt = involvement.get('end_date')
+                      const incidentCounty = involvement.get('county_name')
+                      const participants = involvement.get('all_people')
+                      const reporter = involvement.get('reporter')
+                      const assignee = involvement.get('assigned_social_worker')
                       const nonReporterTypes = Immutable.fromJS(ROLE_TYPE_NON_REPORTER)
 
                       const nonOnlyReporters = participants.filter((p) => {
                         const roles = p.get('roles')
                         return roles.some((role) => nonReporterTypes.includes(role)) || roles.isEmpty()
                       })
-                      const reporter = participants.filter((p) => {
-                        const roles = p.get('roles')
-                        return reporterTypes.some((role) => roles.includes(role))
-                      }).first()
 
                       const status = endedAt ? 'Closed' : 'In Progress'
                       return (
@@ -81,7 +77,7 @@ export default class HistoryCard extends React.Component {
                                 {`Reporter: ${reporter ? nameFormatter(reporter) : '' }`}
                               </span>
                               <span className='col-md-6 assignee'>
-                                {`Worker: ${assignee ? assignee : ''}`}
+                                {`Worker: ${assignee ? nameFormatter(assignee) : ''}`}
                               </span>
                             </div>
                           </td>
