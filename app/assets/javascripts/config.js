fc1d47ba0acee8777109dc95ac948bc7b31c183a
Nate Jones and Sara Bocciardi <pair+Nate.Jones+Sara.Bocciardi@OSI.ca.gov>
2017-06-08 11:00:08 -0700

    Write all active features to window so JS can access them
    [#146835671]

--- app/assets/javascripts/config.js
+++ app/assets/javascripts/config.js
@@ -1,3 +1,7 @@
 export function config() {
   return window.org.intake.config
 }
+
+export function isFeatureActive(feature) {
+  return config().active_features.includes(feature)
+}
---
+++ spec/javascripts/configSpec.js
@@ -0,0 +1,39 @@
+import {isFeatureActive, config} from 'config'
+
+describe('intake config', () => {
+  let windowOrg
+  beforeEach(() => { windowOrg = window.org })
+  afterEach(() => { window.org = windowOrg })
+
+  describe('.config', () => {
+    beforeEach(() => {
+      window.org = Object.freeze({intake: {config: {test_config: true}}})
+    })
+
+    it('references window org intake config', () => {
+      expect(config()).toEqual({test_config: true})
+    })
+  })
+
+  describe('.isFeatureActive', () => {
+    describe('when a feature is present in active features', () => {
+      beforeEach(() => {
+        window.org = Object.freeze({intake: {config: {active_features: ['enabled_feature']}}})
+      })
+
+      it('returns true', () => {
+        expect(isFeatureActive('enabled_feature')).toEqual(true)
+      })
+    })
+
+    describe('when a feature is not present in active features', () => {
+      beforeEach(() => {
+        window.org = Object.freeze({intake: {config: {active_features: []}}})
+      })
+
+      it('returns false', () => {
+        expect(isFeatureActive('enabled_feature')).toEqual(false)
+      })
+    })
+  })
+})
