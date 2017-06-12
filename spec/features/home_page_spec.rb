6ee4bfaaa1ca8658e9be22fb70fd4115b28a6533
Nate Jones and Sara Bocciardi <pair+Nate.Jones+Sara.Bocciardi@OSI.ca.gov>
2017-06-12 09:44:51 -0700

    Add additional context for clarity
    [#146835671]

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -73,61 +73,63 @@ feature 'home page' do
     end
   end
 
-  scenario 'includes title and navigation links when release one is disabled' do
-    visit root_path
-    expect(page).to have_title 'Intake'
-    expect(page).to have_link 'Start Screening'
-  end
-
-  scenario 'includes a list of saved screenings' do
-    screening1 = FactoryGirl.create(
-      :screening,
-      name: 'Little Shop of Horrors',
-      reference: 'H9S83',
-      started_at: '2016-08-11T18:24:22.157Z',
-      screening_decision: 'screen_out'
-    )
-    screening2 = FactoryGirl.create(
-      :screening,
-      name: 'The Shining',
-      reference: 'DF90W5',
-      started_at: '2016-08-12T18:24:22.157Z',
-      screening_decision: 'promote_to_referral'
-    )
-    screening3 = FactoryGirl.create(
-      :screening,
-      name: 'It Follows',
-      reference: 'Q7W0B6',
-      started_at: '2016-08-17T18:24:22.157Z',
-      screening_decision: 'differential_response'
-    )
-    stub_request(:get, host_url(ExternalRoutes.intake_api_screenings_path))
-      .and_return(json_body([screening1, screening2, screening3].to_json, status: 200))
-
-    visit root_path
-    within 'thead' do
-      expect(page).to have_css('th', text: 'Name & ID')
-      expect(page).to have_css('th', text: 'Decision')
-      expect(page).to have_css('th', text: 'Report Date')
+  context 'when no releases are enabled' do
+    scenario 'includes title and navigation links' do
+      visit root_path
+      expect(page).to have_title 'Intake'
+      expect(page).to have_link 'Start Screening'
     end
 
-    within 'tbody' do
-      expect(page).to have_css('tr', count: 3)
-      rows = all('tr')
-      within rows[0] do
-        expect(page).to have_content('H9S83')
-        expect(page).to have_content('08/11/2016')
-        expect(page).to have_content('Screen out')
+    scenario 'includes a list of saved screenings' do
+      screening1 = FactoryGirl.create(
+        :screening,
+        name: 'Little Shop of Horrors',
+        reference: 'H9S83',
+        started_at: '2016-08-11T18:24:22.157Z',
+        screening_decision: 'screen_out'
+      )
+      screening2 = FactoryGirl.create(
+        :screening,
+        name: 'The Shining',
+        reference: 'DF90W5',
+        started_at: '2016-08-12T18:24:22.157Z',
+        screening_decision: 'promote_to_referral'
+      )
+      screening3 = FactoryGirl.create(
+        :screening,
+        name: 'It Follows',
+        reference: 'Q7W0B6',
+        started_at: '2016-08-17T18:24:22.157Z',
+        screening_decision: 'differential_response'
+      )
+      stub_request(:get, host_url(ExternalRoutes.intake_api_screenings_path))
+        .and_return(json_body([screening1, screening2, screening3].to_json, status: 200))
+
+      visit root_path
+      within 'thead' do
+        expect(page).to have_css('th', text: 'Name & ID')
+        expect(page).to have_css('th', text: 'Decision')
+        expect(page).to have_css('th', text: 'Report Date')
       end
-      within rows[1] do
-        expect(page).to have_content('DF90W5')
-        expect(page).to have_content('08/12/2016')
-        expect(page).to have_content('Promote to referral')
-      end
-      within rows[2] do
-        expect(page).to have_content('Q7W0B6')
-        expect(page).to have_content('08/17/2016')
-        expect(page).to have_content('Differential response')
+
+      within 'tbody' do
+        expect(page).to have_css('tr', count: 3)
+        rows = all('tr')
+        within rows[0] do
+          expect(page).to have_content('H9S83')
+          expect(page).to have_content('08/11/2016')
+          expect(page).to have_content('Screen out')
+        end
+        within rows[1] do
+          expect(page).to have_content('DF90W5')
+          expect(page).to have_content('08/12/2016')
+          expect(page).to have_content('Promote to referral')
+        end
+        within rows[2] do
+          expect(page).to have_content('Q7W0B6')
+          expect(page).to have_content('08/17/2016')
+          expect(page).to have_content('Differential response')
+        end
       end
     end
   end
