20a10066fafc8f00e6288f7ac59f38c20049d8de
Aman Alshurafa and Sara Bocciardi <pair+Aman.Alshurafa+Sara.Bocciardi@OSI.ca.gov>
2017-06-20 08:50:46 -0700

    Use a shared props object for props that don't need to be changed
    [#refactor]

--- spec/javascripts/components/common/ShowFieldSpec.jsx
+++ spec/javascripts/components/common/ShowFieldSpec.jsx
@@ -4,49 +4,52 @@ import ShowField from 'components/common/ShowField'
 
 describe('ShowField', () => {
   let component
-  beforeEach(() => {
-    const props = {
-      gridClassName: 'myWrapperTest',
-      labelClassName: 'myLabelTest',
-      label: 'this is my label',
-    }
-    component = shallow(
-      <ShowField {...props}>This is the show field value</ShowField>
-    )
+
+  const requiredProps = {
+    gridClassName: 'myWrapperTest',
+    labelClassName: 'myLabelTest',
+    label: 'this is my label',
+  }
+
+  describe('without required flag or errors', () => {
+    beforeEach(() => {
+      component = shallow(
+        <ShowField {...requiredProps}>This is the show field value</ShowField>
+      )
+    })
+
+    it('renders the wrapperClass', () => {
+      expect(component.html()).toContain('class="myWrapperTest"')
+    })
+
+    it('renders the label', () => {
+      const labelElement = component.find('label')
+      expect(labelElement.length).toEqual(1)
+      expect(component.find('label.myLabelTest').not('.required').exists()).toEqual(true)
+      expect(labelElement.text()).toEqual('this is my label')
+    })
+
+    it('renders the show field value', () => {
+      const valueElement = component.find('div[className="c-gray"]')
+      expect(valueElement.length).toEqual(1)
+      expect(valueElement.text()).toEqual('This is the show field value')
+    })
   })
 
-  it('renders the wrapperClass', () => {
-    expect(component.html()).toContain('class="myWrapperTest"')
-  })
+  describe('ShowField as required', () => {
+    beforeEach(() => {
+      const props = {
+        ...requiredProps,
+        required: true,
+      }
+      component = shallow(
+        <ShowField {...props}>This is the show field value</ShowField>
+      )
+    })
 
-  it('renders the label', () => {
-    const labelElement = component.find('label')
-    expect(labelElement.length).toEqual(1)
-    expect(component.find('label.myLabelTest').not('.required').exists()).toEqual(true)
-    expect(labelElement.text()).toEqual('this is my label')
-  })
-
-  it('renders the show field value', () => {
-    const valueElement = component.find('div[className="c-gray"]')
-    expect(valueElement.length).toEqual(1)
-    expect(valueElement.text()).toEqual('This is the show field value')
+    it('renders the label as required', () => {
+      expect(component.find('label.required.myLabelTest').exists()).toEqual(true)
+    })
   })
 })
 
-describe('ShowField as required', () => {
-  let component
-  beforeEach(() => {
-    const props = {
-      gridClassName: 'myWrapperTest',
-      labelClassName: 'myLabelTest',
-      label: 'this is my label',
-      required: true,
-    }
-    component = shallow(
-      <ShowField {...props}>This is the show field value</ShowField>
-    )
-  })
-  it('renders the label as required', () => {
-    expect(component.find('label.required.myLabelTest').exists()).toEqual(true)
-  })
-})
