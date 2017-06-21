5b4f199d29ef6b779bc6e5e452a206be28cece12
Nate Jones <Nate.Jones@OSI.ca.gov>
2017-06-21 09:34:00 -0700

    Fix flakey create screening test
    Get stub was never working as screening id was never set
    
    [#146835707]

--- spec/features/screening/create_screening_spec.rb
+++ spec/features/screening/create_screening_spec.rb
@@ -47,7 +47,7 @@ feature 'Create Screening' do
 
   scenario 'via start screening link' do
     allow(LUID).to receive(:generate).and_return(['DQJIYK'])
-    new_screening = FactoryGirl.build(
+    new_screening = FactoryGirl.create(
       :screening,
       reference: 'DQJIYK',
       safety_alerts: [],
@@ -61,7 +61,7 @@ feature 'Create Screening' do
     stub_request(:get, host_url(ExternalRoutes.intake_api_screenings_path))
       .and_return(json_body([].to_json, status: 200))
 
-    stub_request(:get, host_url(ExternalRoutes.intake_api_screening_path(1)))
+    stub_request(:get, host_url(ExternalRoutes.intake_api_screening_path(new_screening.id)))
       .and_return(json_body(new_screening.to_json, status: 200))
 
     visit root_path
@@ -72,6 +72,7 @@ feature 'Create Screening' do
         :post, host_url(ExternalRoutes.intake_api_screenings_path)
       ).with(body: as_json_without_root_id(new_screening))
     ).to have_been_made
+
     expect(page).to have_content('Edit Screening #DQJIYK')
   end
 end
