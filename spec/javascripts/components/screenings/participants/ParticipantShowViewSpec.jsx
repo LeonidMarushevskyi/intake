0e61b16ab84a0837af718646eb7a64a98c5ae0dd
Nate Jones <Nate.Jones@OSI.ca.gov>
2017-06-21 16:25:02 -0700

    Cleanup participant show view spec
    [#146835777]

--- spec/javascripts/components/screenings/participants/ParticipantShowViewSpec.jsx
+++ spec/javascripts/components/screenings/participants/ParticipantShowViewSpec.jsx
@@ -4,9 +4,41 @@ import React from 'react'
 import {shallow} from 'enzyme'
 
 describe('ParticipantShowView', () => {
-  let component
-  let onEdit
-  beforeEach(() => {
+  it('renders a participant show view card', () => {
+    const participant = Immutable.fromJS({id: '200'})
+    const component = shallow(<ParticipantShowView participant={participant} onEdit={() => null}/>)
+    expect(component.find('.card.show').length).toEqual(1)
+    expect(component.find('#participants-card-200').length).toEqual(1)
+  })
+
+  it('renders the participants full name', () => {
+    const participant = Immutable.fromJS({
+      id: '200',
+      first_name: 'Kevin',
+      middle_name: 'Home Alone',
+      last_name: 'McCallister',
+      name_suffix: 'iv',
+    })
+    const component = shallow(<ParticipantShowView participant={participant} onEdit={() => null}/>)
+    expect(component.find('.card-header').text()).toContain('Kevin Home Alone McCallister IV')
+  })
+
+  it('renders the delete link', () => {
+    const component = shallow(<ParticipantShowView participant={Immutable.Map()} onEdit={() => null}/>)
+    expect(component.find('.fa-times').length).toEqual(1)
+  })
+
+  it('renders the edit link', () => {
+    const component = shallow(<ParticipantShowView participant={Immutable.Map()} onEdit={() => null}/>)
+    expect(component.find('EditLink').props().ariaLabel).toEqual('Edit participant')
+  })
+
+  it('renders the default avatar', () => {
+    const component = shallow(<ParticipantShowView participant={Immutable.Map()} onEdit={() => null}/>)
+    expect(component.find('img').props().src).toEqual('/assets/default-profile.svg')
+  })
+
+  it('renders the participant show fields', () => {
     const participant = Immutable.fromJS({
       id: '200',
       first_name: 'Kevin',
@@ -18,32 +50,7 @@ describe('ParticipantShowView', () => {
       date_of_birth: '1990-11-16',
       ssn: '111-22-33__',
     })
-    onEdit = jasmine.createSpy()
-    component = shallow(<ParticipantShowView participant={participant} onEdit={onEdit}/>)
-  })
-
-  it('renders a participant show view card', () => {
-    expect(component.find('.card.show').length).toEqual(1)
-    expect(component.find('#participants-card-200').length).toEqual(1)
-  })
-
-  it('renders the participants full name', () => {
-    expect(component.find('.card-header').text()).toContain('Kevin Home Alone McCallister IV')
-  })
-
-  it('renders the delete link', () => {
-    expect(component.find('.fa-times').length).toEqual(1)
-  })
-
-  it('renders the edit link', () => {
-    expect(component.find('EditLink').props().ariaLabel).toEqual('Edit participant')
-  })
-
-  it('renders the default avatar', () => {
-    expect(component.find('img').props().src).toEqual('/assets/default-profile.svg')
-  })
-
-  it('renders the participant show fields', () => {
+    const component = shallow(<ParticipantShowView participant={participant} onEdit={() => null}/>)
     expect(component.find('ShowField').length).toEqual(5)
     expect(component.find('ShowField[label="Name"]').html())
       .toContain('Kevin Home Alone McCallister IV')
@@ -58,121 +65,108 @@ describe('ParticipantShowView', () => {
   })
 
   it('calls the onEdit function when edit link is clicked', () => {
+    const onEdit = jasmine.createSpy()
+    const component = shallow(<ParticipantShowView participant={Immutable.Map()} onEdit={onEdit}/>)
     component.find('EditLink').simulate('click')
     expect(onEdit).toHaveBeenCalled()
   })
 
   it('calls the onDelete function when delete link is clicked', () => {
-    const participant = Immutable.fromJS({
-      id: '199',
-      first_name: 'Lisa',
-      last_name: 'Simpson',
-      date_of_birth: '2016-12-31',
-      gender: 'female',
-      ssn: 'ssn-1',
-    })
-
     const onDelete = jasmine.createSpy('onDelete')
-
-    component = shallow(<ParticipantShowView participant={participant} onEdit={() => {}} onDelete={onDelete}/>)
+    const component = shallow(<ParticipantShowView participant={Immutable.Map()} onEdit={() => {}} onDelete={onDelete}/>)
     component.find('.delete-button').simulate('click')
     expect(onDelete).toHaveBeenCalled()
   })
-})
 
-describe('ParticipantShowView with partial name', () => {
-  let component
-  it('renders the participant header correctly with null last name', () => {
-    const participant = Immutable.fromJS({
-      id: '200',
-      first_name: 'Kevin',
-      last_name: null,
+  describe('when participant has a partial name', () => {
+    it('renders the participant header correctly with null last name', () => {
+      const participant = Immutable.fromJS({
+        id: '200',
+        first_name: 'Kevin',
+        last_name: null,
+      })
+      const component = shallow(<ParticipantShowView participant={participant} onEdit={() => {}}/>)
+      expect(component.find('.card-header').text()).toContain('Kevin (Unknown last name)')
     })
-    component = shallow(<ParticipantShowView participant={participant} onEdit={() => {}}/>)
-    expect(component.find('.card-header').text()).toContain('Kevin (Unknown last name)')
-  })
 
-  it('renders the participant header correctly with null first name', () => {
-    const participant = Immutable.fromJS({
-      id: '200',
-      first_name: null,
-      last_name: 'McAllister',
+    it('renders the participant header correctly with null first name', () => {
+      const participant = Immutable.fromJS({
+        id: '200',
+        first_name: null,
+        last_name: 'McAllister',
+      })
+      const component = shallow(<ParticipantShowView participant={participant} onEdit={() => {}}/>)
+      expect(component.find('.card-header').text()).toContain('(Unknown first name) McAllister')
     })
-    component = shallow(<ParticipantShowView participant={participant} onEdit={() => {}}/>)
-    expect(component.find('.card-header').text()).toContain('(Unknown first name) McAllister')
-  })
 
-  it('renders the participant name show fields', () => {
-    const participant = Immutable.fromJS({
-      id: '200',
-      first_name: 'Kevin',
-      last_name: null,
+    it('renders the participant name show fields', () => {
+      const participant = Immutable.fromJS({
+        id: '200',
+        first_name: 'Kevin',
+        last_name: null,
+      })
+      const component = shallow(<ParticipantShowView participant={participant} onEdit={() => {}}/>)
+      expect(component.find('ShowField[label="Name"]').html()).not.toContain('null')
     })
-    component = shallow(<ParticipantShowView participant={participant} onEdit={() => {}}/>)
-    expect(component.find('ShowField[label="Name"]').html()).not.toContain('null')
   })
-})
 
-describe('ParticipantShowView with no name', () => {
-  let component
-  it('does not render when not present', () => {
-    const participant = Immutable.fromJS({
-      id: '200',
-      first_name: null,
-      last_name: null,
-      gender: 'male',
-      date_of_birth: '1990-11-16',
-      ssn: '111223333',
+  describe('when participant has no name', () => {
+    it('does not render when not present', () => {
+      const participant = Immutable.fromJS({
+        id: '200',
+        first_name: null,
+        last_name: null,
+        gender: 'male',
+        date_of_birth: '1990-11-16',
+        ssn: '111223333',
+      })
+      const component = shallow(<ParticipantShowView participant={participant} onEdit={() => {}}/>)
+      expect(component.find('.card-header').html()).toContain('Unknown Person')
     })
-    component = shallow(<ParticipantShowView participant={participant} onEdit={() => {}}/>)
-    expect(component.find('.card-header').html()).toContain('Unknown Person')
   })
-})
 
-describe('ParticipantShowView with addresses', () => {
-  let component
-  beforeEach(() => {
-    const participant = Immutable.fromJS({
-      id: '5',
-      addresses: [{
-        id: '1',
-        street_address: '671 Lincoln Avenue',
-        city: 'Winnetka',
-        state: 'IL',
-        zip: '60093',
-        type: 'Placement',
-      }],
+  describe('when participant has addresses', () => {
+    it('renders participant with address', () => {
+      const participant = Immutable.fromJS({
+        id: '5',
+        addresses: [{
+          id: '1',
+          street_address: '671 Lincoln Avenue',
+          city: 'Winnetka',
+          state: 'IL',
+          zip: '60093',
+          type: 'Placement',
+        }],
+      })
+      const component = shallow(<ParticipantShowView participant={participant} onEdit={() => {}}/>)
+      expect(component.find('ShowField[label="Address"]').html())
+        .toContain('671 Lincoln Avenue')
+      expect(component.find('ShowField[label="City"]').html())
+        .toContain('Winnetka')
+      expect(component.find('ShowField[label="State"]').html())
+        .toContain('Illinois')
+      expect(component.find('ShowField[label="Zip"]').html())
+        .toContain('60093')
+      expect(component.find('ShowField[label="Address Type"]').html())
+        .toContain('Placement')
     })
-    component = shallow(<ParticipantShowView participant={participant} onEdit={() => {}}/>)
   })
-  it('renders participant with address', () => {
-    expect(component.find('ShowField[label="Address"]').html())
-      .toContain('671 Lincoln Avenue')
-    expect(component.find('ShowField[label="City"]').html())
-      .toContain('Winnetka')
-    expect(component.find('ShowField[label="State"]').html())
-      .toContain('Illinois')
-    expect(component.find('ShowField[label="Zip"]').html())
-      .toContain('60093')
-    expect(component.find('ShowField[label="Address Type"]').html())
-      .toContain('Placement')
-  })
-})
 
-describe('ParticipantShowView with phone numbers', () => {
-  const participant = Immutable.fromJS({
-    id: '7',
-    phone_numbers: [{
-      id: '3',
-      number: '789-456-1235',
-      type: 'Work',
-    }],
-  })
-  const component = shallow(<ParticipantShowView participant={participant} onEdit={() => {}} />)
-  it('renders the participant with phone numbers', () => {
-    expect(component.find('ShowField[label="Phone Number"]').html())
-      .toContain('789-456-1235')
-    expect(component.find('ShowField[label="Phone Number Type"]').html())
-      .toContain('Work')
+  describe('when participant has phone numbers', () => {
+    it('renders the participant with phone numbers', () => {
+      const participant = Immutable.fromJS({
+        id: '7',
+        phone_numbers: [{
+          id: '3',
+          number: '789-456-1235',
+          type: 'Work',
+        }],
+      })
+      const component = shallow(<ParticipantShowView participant={participant} onEdit={() => {}} />)
+      expect(component.find('ShowField[label="Phone Number"]').html())
+        .toContain('789-456-1235')
+      expect(component.find('ShowField[label="Phone Number Type"]').html())
+        .toContain('Work')
+    })
   })
 })
