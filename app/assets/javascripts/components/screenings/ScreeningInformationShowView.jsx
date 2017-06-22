23a441b92b4fca25528f299d2846657ac54e7a0a
John Gedeon and Sara Bocciardi <pair+John.Gedeon+Sara.Bocciardi@OSI.ca.gov>
2017-06-22 11:20:38 -0700

    Display errors in show view for start date/time
    [#146334465]

--- app/assets/javascripts/components/screenings/ScreeningInformationShowView.jsx
+++ app/assets/javascripts/components/screenings/ScreeningInformationShowView.jsx
@@ -23,7 +23,7 @@ const ScreeningInformationShowView = ({errors, screening, onEdit}) => (
         </ShowField>
       </div>
       <div className='row double-gap-top'>
-        <ShowField gridClassName='col-md-6' label='Screening Start Date/Time' required>
+        <ShowField gridClassName='col-md-6' label='Screening Start Date/Time' errors={errors.get('started_at')} required>
           {dateTimeFormatter(screening.get('started_at'))}
         </ShowField>
         <ShowField gridClassName='col-md-6' label='Screening End Date/Time'>
--- spec/javascripts/components/screenings/screeningInformation/ScreeningInformationShowViewSpec.jsx
+++ spec/javascripts/components/screenings/screeningInformation/ScreeningInformationShowViewSpec.jsx
@@ -41,6 +41,11 @@ describe('ScreeningInformationShowView', () => {
       const props = {
         ...requiredProps,
         screening,
+        errors: Immutable.fromJS({
+          assignee: ['Error 1', 'Error 2'],
+          started_at: ['Error 3', 'Error 4'],
+          communication_method: ['My Spider-Sense is tingling.'],
+        }),
       }
 
       component = shallow(<ScreeningInformationShowView {...props} />)
@@ -70,15 +75,29 @@ describe('ScreeningInformationShowView', () => {
       expect(component.find('ShowField[label="Communication Method"]').html())
       .toContain('<div class="c-gray"></div>')
     })
+
+    it('renders errors for the Assigned Social Worker', () => {
+      const socialWorker = component.find('ShowField[label="Assigned Social Worker"]')
+      expect(socialWorker.html()).toContain('Error 1')
+      expect(socialWorker.html()).toContain('Error 2')
+    })
+
+    it('renders errors for the Start Date/Time', () => {
+      const startDate = component.find('ShowField[label="Screening Start Date/Time"]')
+      expect(startDate.html()).toContain('Error 3')
+      expect(startDate.html()).toContain('Error 4')
+    })
+
+    it('renders errors for Communication Method', () => {
+      const socialWorker = component.find('ShowField[label="Communication Method"]')
+      expect(socialWorker.html()).toContain('My Spider-Sense is tingling.')
+    })
+
   })
 
   describe('Show fields', () => {
     const props = {
       ...requiredProps,
-      errors: Immutable.fromJS({
-        assignee: ['Error 1', 'Error 2'],
-        communication_method: ['My Spider-Sense is tingling.'],
-      }),
       screening: Immutable.fromJS({
         name: 'The Rocky Horror Picture Show',
         assignee: 'Michael Bluth',
@@ -123,17 +142,6 @@ describe('ScreeningInformationShowView', () => {
       const socialWorker = component.find('ShowField[label="Assigned Social Worker"]')
       expect(socialWorker.props().required).toEqual(true)
       expect(socialWorker.html()).toContain('Michael Bluth')
-    })
-
-    it('renders errors for the Assigned Social Worker', () => {
-      const socialWorker = component.find('ShowField[label="Assigned Social Worker"]')
-      expect(socialWorker.html()).toContain('Error 1')
-      expect(socialWorker.html()).toContain('Error 2')
-    })
-
-    it('renders errors for Communication Method', () => {
-      const socialWorker = component.find('ShowField[label="Communication Method"]')
-      expect(socialWorker.html()).toContain('My Spider-Sense is tingling.')
     })
   })
 })
