a98fbddbfd7b04f4874e9b83b1d52521052b484d
Sara Bocciardi <Sara.Bocciardi@OSI.ca.gov>
2017-06-27 16:38:11 -0700

    Remove MaskedInput info from the InputField component
    [#147960533]

--- app/assets/javascripts/components/common/InputField.jsx
+++ app/assets/javascripts/components/common/InputField.jsx
@@ -1,19 +1,14 @@
 import FormField from 'components/common/FormField'
-import MaskedInput from 'react-maskedinput'
 import PropTypes from 'prop-types'
 import React from 'react'
-import _ from 'lodash'
 
 const InputField = ({
-  blurPlaceholder,
   errors,
-  focusPlaceholder,
   gridClassName,
   id,
   label,
   labelClassName,
   maxLength,
-  mask,
   onBlur,
   onChange,
   placeholder,
@@ -30,30 +25,12 @@ const InputField = ({
     required: required,
   }
 
-  let input =
-    <input id={id} type={type} placeholder={placeholder}
-      value={value} onChange={onChange} maxLength={maxLength} onBlur={onBlur}
-      aria-required={required} required={required}
-    />
-
-  if (!_.isEmpty(mask)) {
-    input =
-      <MaskedInput id={id} type={type} value={value} mask={mask}
-        placeholder={placeholder} required={required} aria-required={required}
-        onBlur={(event) => {
-          event.target.placeholder = blurPlaceholder
-          if (!_.isEmpty(onBlur)) onBlur(id, event.target.value)
-        }}
-        onFocus={(event) => {
-          event.target.placeholder = focusPlaceholder
-        }}
-        onChange={onChange}
-      />
-  }
-
   return (
     <FormField {...formFieldProps}>
-      {input}
+      <input id={id} type={type} placeholder={placeholder}
+        value={value} onChange={onChange} maxLength={maxLength} onBlur={onBlur}
+        aria-required={required} required={required}
+      />
     </FormField>
   )
 }
@@ -64,14 +41,11 @@ InputField.defaultProps = {
 }
 
 InputField.propTypes = {
-  blurPlaceholder: PropTypes.string,
   errors: PropTypes.object,
-  focusPlaceholder: PropTypes.string,
   gridClassName: PropTypes.string,
   id: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
   labelClassName: PropTypes.string,
-  mask: PropTypes.string,
   maxLength: PropTypes.string,
   onBlur: PropTypes.func,
   onChange: PropTypes.func.isRequired,
