8c24f179e94ad0c35726b42ff28aef958385906b
Sara Bocciardi <Sara.Bocciardi@OSI.ca.gov>
2017-06-27 16:37:41 -0700

    Add new MaskedInputField component
    [#147960533]

---
+++ app/assets/javascripts/components/common/MaskedInputField.jsx
@@ -0,0 +1,74 @@
+import FormField from 'components/common/FormField'
+import MaskedInput from 'react-maskedinput'
+import PropTypes from 'prop-types'
+import React from 'react'
+import _ from 'lodash'
+
+const MaskedInputField = ({
+  blurPlaceholder,
+  errors,
+  focusPlaceholder,
+  gridClassName,
+  id,
+  label,
+  labelClassName,
+  mask,
+  onBlur,
+  onChange,
+  placeholder,
+  required,
+  type,
+  value,
+}) => {
+  const formFieldProps = {
+    errors: errors,
+    gridClassName: gridClassName,
+    id: id,
+    label: label,
+    labelClassName: labelClassName,
+    required: required,
+  }
+
+  return (
+    <FormField {...formFieldProps}>
+      <MaskedInput id={id} type={type} value={value} mask={mask}
+        placeholder={placeholder} required={required} aria-required={required}
+        onBlur={(event) => {
+          event.target.placeholder = blurPlaceholder
+          if (!_.isEmpty(onBlur)) onBlur(id, event.target.value)
+        }}
+        onFocus={(event) => {
+          event.target.placeholder = focusPlaceholder
+        }}
+        onChange={onChange}
+      />
+    </FormField>
+  )
+}
+
+MaskedInputField.defaultProps = {
+  type: 'text',
+  mask: '',
+}
+
+MaskedInputField.propTypes = {
+  blurPlaceholder: PropTypes.string,
+  errors: PropTypes.object,
+  focusPlaceholder: PropTypes.string,
+  gridClassName: PropTypes.string,
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  labelClassName: PropTypes.string,
+  mask: PropTypes.string,
+  onBlur: PropTypes.func,
+  onChange: PropTypes.func.isRequired,
+  placeholder: PropTypes.string,
+  required: PropTypes.bool,
+  type: PropTypes.string,
+  value: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]),
+}
+export default MaskedInputField
+
---
+++ spec/javascripts/components/common/MaskedInputFieldSpec.jsx
@@ -0,0 +1,117 @@
+import React from 'react'
+import Immutable from 'immutable'
+import {shallow} from 'enzyme'
+import MaskedInputField from 'components/common/MaskedInputField'
+
+describe('MaskedInputField', () => {
+  let component
+  let formField
+  let maskedInput
+  let onChange
+  let onBlur
+
+  const props = {
+    gridClassName: 'myWrapperTest',
+    labelClassName: 'myLabelTest',
+    id: 'myInputFieldId',
+    label: 'this is my label',
+    onChange: onChange,
+    onBlur: onBlur,
+    value: 'this is my field value',
+    mask: '111-11-1111',
+    errors: Immutable.Map(),
+    required: false,
+  }
+
+  beforeEach(() => {
+    onChange = jasmine.createSpy('onChange')
+    onBlur = jasmine.createSpy('onBlur')
+  })
+
+  describe('without placeholder props', () => {
+    beforeEach(() => {
+      component = shallow(<MaskedInputField {...props} onChange={onChange} onBlur={onBlur} />)
+      formField = component.find('FormField')
+      maskedInput = component.find('MaskedInput')
+    })
+
+    it('passes props to the FormField', () => {
+      expect(formField.props().labelClassName).toEqual('myLabelTest')
+      expect(formField.props().gridClassName).toEqual('myWrapperTest')
+      expect(formField.props().id).toEqual('myInputFieldId')
+      expect(formField.props().label).toEqual('this is my label')
+      expect(formField.props().errors).toEqual(Immutable.Map())
+      expect(formField.props().required).toEqual(false)
+      expect(maskedInput.exists()).toEqual(true)
+    })
+
+    it('renders a MaskedInput field', () => {
+      expect(maskedInput.props().mask).toEqual('111-11-1111')
+      expect(maskedInput.props().placeholder).toEqual(undefined)
+    })
+
+    it('renders the input value', () => {
+      expect(maskedInput.props().value).toEqual('this is my field value')
+    })
+
+    it('renders the input type', () => {
+      const maskedInputWithType = shallow(
+        <MaskedInputField {...props} onChange={onChange} onBlur={onBlur} type='tel'/>
+      ).find('MaskedInput')
+
+      expect(maskedInput.props().type).toEqual('text')
+      expect(maskedInputWithType.props().type).toEqual('tel')
+    })
+
+    it('calls onChange when a change event occurs on input field', () => {
+      maskedInput.simulate('change')
+      expect(onChange).toHaveBeenCalled()
+    })
+
+    it('calls onBlur when a blur event occurs on input field', () => {
+      maskedInput.simulate('blur', {target: {value: '1234'}})
+      expect(onBlur).toHaveBeenCalled()
+    })
+  })
+
+  describe('with placeholder props', () => {
+    beforeEach(() => {
+      const propsWithMaskedInput = {
+        ...props,
+        blurPlaceholder: 'I feel lonely :( ',
+        focusPlaceholder: 'I like attention :) ',
+      }
+      component = shallow(<MaskedInputField {...propsWithMaskedInput} onChange={onChange} onBlur={onBlur} />)
+      maskedInput = component.find('MaskedInput')
+    })
+
+    it('assigns placeholder props properly', () => {
+      const event = {target: {placeholder: null}}
+      expect(maskedInput.props().mask).toEqual('111-11-1111')
+      maskedInput.props().onBlur(event)
+      expect(event.target.placeholder).toEqual('I feel lonely :( ')
+      maskedInput.props().onFocus(event)
+      expect(event.target.placeholder).toEqual('I like attention :) ')
+    })
+  })
+
+  describe('when it is not required', () => {
+    it('renders an input field', () => {
+      component = shallow(<MaskedInputField {...props} onChange={onChange} onBlur={onBlur}/>)
+      expect(component.find('label.required').exists()).toEqual(false)
+      expect(component.find('FormField').props().required).toEqual(false)
+    })
+  })
+
+  describe('when it is required', () => {
+    it('renders a required MaskedInput field', () => {
+      component = shallow(<MaskedInputField {...props} onChange={onChange} onBlur={onBlur} required />)
+      maskedInput = component.find('MaskedInput')
+      expect(component.find('FormField').props().required).toEqual(true)
+      expect(maskedInput.prop('required')).toEqual(true)
+      expect(maskedInput.prop('aria-required')).toEqual(true)
+      expect(maskedInput.props().mask).toEqual('111-11-1111')
+    })
+  })
+})
+
