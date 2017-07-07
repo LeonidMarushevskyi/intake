438148fb1c591b4a2cca479a534602567814fbce
Aaron Smith <Aaron.Smith@OSI.ca.gov>
2017-07-07 08:29:49 -0700

    Remove broken datepicker helper
    [#148111471]

--- spec/support/helpers/datetime_helpers.rb
+++ spec/support/helpers/datetime_helpers.rb
@@ -1,14 +1,6 @@
 # frozen_string_literal: true
 
 module DateTimeHelpers
-  def mouse_select_datepicker(locator, date)
-    zero_indexed_month = date.month - 1
-    within locator do
-      find('.rw-btn-calendar').click
-      find("[id$='cal_calendar__month_#{zero_indexed_month}-#{date.day}']", visible: true).click
-    end
-  end
-
   def select_time_from_timepicker(locator, time)
     within locator do
       find('.rw-btn-time').native.click
