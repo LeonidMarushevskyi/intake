c1e6f7a256c62dc10175c480e6fff5177cff1054
Sara Bocciardi <Sara.Bocciardi@OSI.ca.gov>
2017-07-21 11:20:00 -0700

    Styling new help message
    [#148086495]

--- app/assets/javascripts/components/common/HelpMessage.jsx
+++ app/assets/javascripts/components/common/HelpMessage.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const HelpMessage = ({message}) => (
-  <div>
-    <p>{message}</p>
+  <div className='row'>
+    <div className='col-md-12'>
+      <div className='alert-message info-message'>
+        <div className='alert-icon'>
+          <i className='fa fa-info-circle' />
+        </div>
+        <div className='alert-text'>
+          {message}
+        </div>
+      </div>
+    </div>
   </div>
 )
 
