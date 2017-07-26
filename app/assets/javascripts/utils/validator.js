e4b737c87c59440bca9c8441fba8dee00be1952a
Aaron Smith and Aman Alshurafa <pair+Aaron.Smith+Aman.Alshurafa@OSI.ca.gov>
2017-07-26 15:41:44 -0700

    Prepare validator for cross report validations
    - Modify isRequired to work with booleans
    - Implement isRequiredIf for conditional validation
    - Pass named args to VALIDATORS
    - Make validateField not explode when rules are empty
    
    [#146467557]

--- app/assets/javascripts/utils/validator.js
+++ app/assets/javascripts/utils/validator.js
@@ -2,15 +2,21 @@ import Immutable from 'immutable'
 import _ from 'lodash'
 import moment from 'moment'
 
-const isRequired = (value, errorMessage) => {
-  if (_.isEmpty(value) || _.isEmpty(value.trim())) {
+const isRequired = ({value, errorMessage}) => {
+  if ((_.isEmpty(value) || _.isEmpty(value.trim())) && value !== true) {
     return errorMessage
-  } else {
-    return undefined
   }
+  return undefined
 }
 
-const isNotInTheFuture = (value, errorMessage) => {
+const isRequiredIf = ({value, errorMessage, condition}) => {
+  if (condition()) {
+    return isRequired({value, errorMessage})
+  }
+  return undefined
+}
+
+const isNotInTheFuture = ({value, errorMessage}) => {
   const now = moment().toISOString()
   if (value > now) {
     return errorMessage
@@ -19,7 +25,7 @@ const isNotInTheFuture = (value, errorMessage) => {
   }
 }
 
-const isBeforeOtherDate = (value, errorMessage, otherValue) => {
+const isBeforeOtherDate = ({value, errorMessage, otherValue}) => {
   if (value && otherValue() && value >= otherValue()) {
     return errorMessage
   }
@@ -28,18 +34,26 @@ const isBeforeOtherDate = (value, errorMessage, otherValue) => {
 
 const VALIDATORS = Immutable.fromJS({
   isRequired: isRequired,
+  isRequiredIf: isRequiredIf,
   isNotInTheFuture: isNotInTheFuture,
   isBeforeOtherDate: isBeforeOtherDate,
 })
 
 export function validateField({value, rules}) {
+  if (_.isEmpty(rules)) {
+    return Immutable.List()
+  }
   const errorMessages = rules.map((ruleOptions) => {
-    const ruleName = ruleOptions.get('rule')
-    const errorMessage = ruleOptions.get('message')
-    const otherValue = ruleOptions.get('otherValue')
+    const opts = {
+      value: value,
+      ruleName: ruleOptions.get('rule'),
+      errorMessage: ruleOptions.get('message'),
+      condition: ruleOptions.get('condition'),
+      otherValue: ruleOptions.get('otherValue'),
+    }
 
-    const validation = VALIDATORS.get(ruleName)
-    return validation(value, errorMessage, otherValue)
+    const validation = VALIDATORS.get(opts.ruleName)
+    return validation(opts)
   })
   return errorMessages.filterNot((message) => message === undefined)
 }
