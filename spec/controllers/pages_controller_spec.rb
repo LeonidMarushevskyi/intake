4592f4347d0f4973d3c60f4eb098a3d1dab2e9d1
Bruno Fagundez <Bruno.Fagundez@OSI.ca.gov>
2017-07-28 13:12:29 -0700

    Added tests for authentication
    [#149580575]

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
+require 'controllers/shared_examples_for_authenticated_controllers'
 
 describe PagesController, '#show' do
-  %w(privacy conditions_of_use).each do |page|
+  %w[privacy conditions_of_use].each do |page|
     context "on GET to /pages/#{page}" do
       before do
         process :show, method: :get, params: { id: page }
@@ -12,6 +15,12 @@ describe PagesController, '#show' do
       end
       it 'renders the template on views/pages' do
         should render_template(page)
+      end
+    end
+
+    context 'endpoints should work with authentication' do
+      it_behaves_like 'an authenticated controller action' do
+        subject { process :show, method: :get, params: { id: page } }
       end
     end
   end
