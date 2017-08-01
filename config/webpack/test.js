b7a737db524aaae4caa9ba14c649e4eda3b4bc42
Nate Jones <Nate.Jones@OSI.ca.gov>
2017-08-01 11:15:53 -0700

    Don't load react addons when compiling test

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -4,13 +4,17 @@ const merge = require('webpack-merge')
 const sharedConfig = require('./shared.js')
 const { env, settings, output, loadersDir } = require('./configuration.js')
 const { basename, dirname, join, relative, resolve } = require('path')
-console.log(settings.source_path, settings.source_entry_path,'<<<<------------ settings current .path')
 module.exports = merge(sharedConfig, {
   entry: './spec/karma_tests.js',
   output: {
     // client assets are output to dist/test/
     path: join(output.path, 'test'),
     publicPath: undefined // no assets CDN
+  },
+  externals: {
+    'react/addons': 'react/addons',
+    'react/lib/ReactContext': 'react/lib/ReactContext',
+    'react/lib/ExecutionEnvironment': 'react/lib/ExecutionEnvironment',
   },
   devtool: 'inline-source-map', // sourcemap support
 })
