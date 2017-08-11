ceb2e3b3c797a60be8da4d1e5994a18afd8c587a
Aman Alshurafa and Nate Jones <pair+Aman.Alshurafa+Nate.Jones@OSI.ca.gov>
2017-08-11 11:22:42 -0700

    Cleanup phone number spec

--- spec/features/screening/participant/phone_number_spec.rb
+++ spec/features/screening/participant/phone_number_spec.rb
@@ -4,12 +4,8 @@ require 'rails_helper'
 require 'spec_helper'
 
 feature 'Participant Phone Number' do
-  let(:marge) do
-    FactoryGirl.create(
-      :participant,
-      :with_complete_phone_number
-    )
-  end
+  let(:existing_phone_number) { PhoneNumber.new(id: '1', number: '917-555-5555', type: 'Work') }
+  let(:marge) { FactoryGirl.create(:participant, phone_numbers: [existing_phone_number]) }
   let(:screening) { FactoryGirl.create(:screening, participants: [marge]) }
 
   before do
@@ -23,76 +19,96 @@ feature 'Participant Phone Number' do
       :get,
       host_url(ExternalRoutes.intake_api_relationships_by_screening_path(screening.id))
     ).and_return(json_body([].to_json, status: 200))
+
   end
 
-  scenario 'editing and then removing a phone number from a participant' do
+  scenario 'adding a new phone number to a participant' do
     visit edit_screening_path(id: screening.id)
-    old_phone = marge.phone_numbers.first.number
 
-    within edit_participant_card_selector(marge.id) do
-      within '.card-body' do
-        expect(page).to have_selector('#phone-numbers')
-        expect(page).to have_field('Phone Number', with: marge.phone_numbers.first.number)
-        expect(page).to have_field('Phone Number Type', with: marge.phone_numbers.first.type)
-        expect(page).to have_button 'Cancel'
-        expect(page).to have_button 'Save'
-        fill_in 'Phone Number', with: '789-456-1245'
-      end
-
-      marge.phone_numbers.first.number = '789-456-1245'
-
-      stub_request(:put, host_url(ExternalRoutes.intake_api_participant_path(marge.id)))
-        .with(body: as_json_without_root_id(marge))
-        .and_return(json_body(marge.to_json, status: 200))
-    end
-
-    within edit_participant_card_selector(marge.id) do
-      within '.card-body' do
-        click_button 'Save'
-      end
-      expect(
-        a_request(:put, host_url(ExternalRoutes.intake_api_participant_path(marge.id)))
-        .with(json_body(as_json_without_root_id(marge)))
-      ).to have_been_made
-    end
-
-    within show_participant_card_selector(marge.id) do
-      within '.card-body' do
-        expect(page).to have_selector("#phone-number-#{marge.phone_numbers.first.id}")
-        expect(page).to have_content('789-456-1245')
-        expect(page).to_not have_content(old_phone)
-      end
-
-      within '.card-header' do
-        click_link 'Edit participant'
-      end
-    end
-
-    within edit_participant_card_selector(marge.id) do
-      within '.card-body' do
-        within '#phone-numbers' do
-          click_link 'Delete phone number'
-        end
-
-        expect(page).to_not have_content('789-456-1245')
-      end
-    end
-
-    marge.phone_numbers = []
-
+    marge.phone_numbers << PhoneNumber.new(number: '789-456-1245', type: 'Home')
     stub_request(:put, host_url(ExternalRoutes.intake_api_participant_path(marge.id)))
       .with(body: as_json_without_root_id(marge))
       .and_return(json_body(marge.to_json, status: 200))
 
     within edit_participant_card_selector(marge.id) do
-      within '.card-body' do
-        click_button 'Save'
+      click_button 'Add new phone number'
+
+      within '#phone-numbers' do
+        within all('.list-item').last do
+          fill_in 'Phone Number', with: '789-456-1245'
+          select 'Home', from: 'Phone Number Type'
+        end
       end
-      expect(
-        a_request(:put, host_url(ExternalRoutes.intake_api_participant_path(marge.id)))
-        .with(json_body(as_json_without_root_id(marge)))
-      ).to have_been_made
+
+      click_button 'Save'
     end
+
+    expect(
+      a_request(:put, host_url(ExternalRoutes.intake_api_participant_path(marge.id)))
+      .with(json_body(as_json_without_root_id(marge)))
+    ).to have_been_made
+
+    within show_participant_card_selector(marge.id) do
+      expect(page).to have_content('917-555-5555')
+      expect(page).to have_content('Work')
+      expect(page).to have_content('789-456-1245')
+      expect(page).to have_content('Home')
+    end
+  end
+
+  scenario 'editing a phone number from a participant' do
+    visit edit_screening_path(id: screening.id)
+
+    marge.phone_numbers.first.number = '789-456-1245'
+    stub_request(:put, host_url(ExternalRoutes.intake_api_participant_path(marge.id)))
+      .with(body: as_json_without_root_id(marge))
+      .and_return(json_body(marge.to_json, status: 200))
+
+    within edit_participant_card_selector(marge.id) do
+      within '#phone-numbers' do
+        expect(page).to have_field('Phone Number', with: '917-555-5555')
+        expect(page).to have_field('Phone Number Type', with: 'Work')
+        fill_in 'Phone Number', with: '789-456-1245'
+      end
+
+      click_button 'Save'
+    end
+
+    expect(
+      a_request(:put, host_url(ExternalRoutes.intake_api_participant_path(marge.id)))
+      .with(json_body(as_json_without_root_id(marge)))
+    ).to have_been_made
+  end
+
+  scenario 'deleting an existing phone number from a participant' do
+    visit screening_path(id: screening.id)
+
+    within show_participant_card_selector(marge.id) do
+      within "#phone-number-#{existing_phone_number.id}" do
+        expect(page).to have_content('917-555-5555')
+      end
+
+      click_link 'Edit participant'
+    end
+
+    marge.phone_numbers = []
+    stub_request(:put, host_url(ExternalRoutes.intake_api_participant_path(marge.id)))
+      .with(body: as_json_without_root_id(marge))
+      .and_return(json_body(marge.to_json, status: 200))
+
+    within edit_participant_card_selector(marge.id) do
+      within '#phone-numbers' do
+        click_link 'Delete phone number'
+      end
+      expect(page).to_not have_content('917-555-5555')
+
+      click_button 'Save'
+    end
+
+    expect(
+      a_request(:put, host_url(ExternalRoutes.intake_api_participant_path(marge.id)))
+      .with(json_body(as_json_without_root_id(marge)))
+    ).to have_been_made
   end
 end
 
