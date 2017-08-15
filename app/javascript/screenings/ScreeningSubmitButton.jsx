14d679fba8928e669cda7b7f6014fdaf1d9810a8
Sara Bocciardi <Sara.Bocciardi@OSI.ca.gov>
2017-08-15 11:13:12 -0700

    Remove modal from submit button that actually submits a referral
    [#148046881]

--- app/javascript/screenings/ScreeningSubmitButton.jsx
+++ app/javascript/screenings/ScreeningSubmitButton.jsx
@@ -7,37 +7,10 @@ const ScreeningSubmitButton = ({actions, params}) => (
     <div className='centered'>
       <button
         className='btn btn-primary'
-        data-toggle='modal'
-        data-target='#submitModal'
-        onClick={(_event) => IntakeConfig.isFeatureActive('referral_submit') && actions.submitScreening(params.id)}
+        onClick={() => actions.submitScreening(params.id)}
       >
         Submit
       </button>
-    </div>
-    <div aria-label='submit modal confirmation' className='modal fade' id='submitModal'>
-      <div className='modal-dialog' role='document'>
-        <div className='modal-content'>
-          <div className='modal-body'>
-            <p>
-              Congratulations! You have completed the process to submit a screening.
-            </p>
-            <p>
-              This is just a learning environment. If your Decision was to promote to referral,
-              this does NOT create an actual referral and it will not appear in CWS/CMS.
-            </p>
-          </div>
-          <div className='modal-footer'>
-            <div className='row'>
-              <div className='centered'>
-                <a href='/' >
-                  <button className='btn btn-primary' href='/' type='button'>Proceed</button>
-                </a>
-                <button aria-label='Close' className='btn btn-default' data-dismiss='modal' type='button'>Cancel</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   </div>
 )
--- spec/javascripts/components/screenings/ScreeningSubmitButtonSpec.jsx
+++ spec/javascripts/components/screenings/ScreeningSubmitButtonSpec.jsx
@@ -1,4 +1,3 @@
-import * as config from 'common/config'
 import React from 'react'
 import ScreeningSubmitButton from 'screenings/ScreeningSubmitButton'
 import {shallow} from 'enzyme'
@@ -6,35 +5,15 @@ import {shallow} from 'enzyme'
 describe('ScreeningSubmitButton', () => {
   let component
   let submitScreening
+
   beforeEach(() => {
     submitScreening = jasmine.createSpy('submitScreening')
     const props = {params: {id: '99'}, actions: {submitScreening}}
     component = shallow(<ScreeningSubmitButton {...props} />)
   })
 
-  describe('when referral_submit is active', () => {
-    beforeEach(() => {
-      spyOn(config, 'isFeatureActive')
-      config.isFeatureActive.and.returnValue(true)
-    })
-
-    it('clicking the submit button submits the screening', () => {
-      component.find('button[children="Submit"]').simulate('click')
-      expect(config.isFeatureActive).toHaveBeenCalledWith('referral_submit')
-      expect(submitScreening).toHaveBeenCalledWith('99')
-    })
-  })
-
-  describe('when referral_submit is not active', () => {
-    beforeEach(() => {
-      spyOn(config, 'isFeatureActive')
-      config.isFeatureActive.and.returnValue(false)
-    })
-
-    it('clicking the submit button does not submits the screening', () => {
-      component.find('button[children="Submit"]').simulate('click')
-      expect(config.isFeatureActive).toHaveBeenCalledWith('referral_submit')
-      expect(submitScreening).not.toHaveBeenCalled()
-    })
+  it('clicking the submit button submits the screening', () => {
+    component.find('button[children="Submit"]').simulate('click')
+    expect(submitScreening).toHaveBeenCalledWith('99')
   })
 })
