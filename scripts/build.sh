27e0191d4586fd321dea84ed40aa7c42ec0e3950
Nate Jones <Nate.Jones@OSI.ca.gov>
2017-08-22 15:47:41 -0700

    Clean tmp directory before building assets

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,7 @@ revision=$(git rev-parse --short HEAD)
 output_dir="/build_artefacts"
 input_dir="/ca_intake"
 
-rm -rf public/assets && rm -rf public/packs && RAILS_ENV=production bin/webpack
+rm -rf public/assets && rm -rf tmp && rm -rf public/packs && RAILS_ENV=production bin/webpack
 
 bundle exec fpm --deb-no-default-config-files -s dir -t deb --name "intake_accelerator" \
 --version "${APP_VERSION}" --package "$output_dir" "$input_dir"
