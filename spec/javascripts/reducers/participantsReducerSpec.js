d343ad03d837d808a96e2dbd61400276d5465ed6
Nate Jones <Nate.Jones@OSI.ca.gov>
2017-08-23 11:43:52 -0700

    Cleanup reducer specs
    - import specific functions and constants

--- spec/javascripts/reducers/participantsReducerSpec.js
+++ spec/javascripts/reducers/participantsReducerSpec.js
@@ -1,13 +1,20 @@
-import * as screeningActions from 'actions/screeningActions'
+import {
+  createScreeningSuccess,
+  fetchScreeningSuccess,
+  updateScreeningSuccess,
+  createParticipantSuccess,
+  deleteParticipantSuccess,
+  updateParticipantSuccess,
+} from 'actions/screeningActions'
 import participantsReducer from 'reducers/participantsReducer'
-import Immutable from 'immutable'
+import {Map, List, fromJS} from 'immutable'
 
 describe('participantsReducer', () => {
   describe('on CREATE_SCREENING_SUCCESS', () => {
     it('returns the participants from the action', () => {
       const screening = {id: '1', name: 'mock_screening', participants: []}
-      const action = screeningActions.createScreeningSuccess(screening)
-      expect(participantsReducer(Immutable.Map(), action).toJS()).toEqual([])
+      const action = createScreeningSuccess(screening)
+      expect(participantsReducer(Map(), action).toJS()).toEqual([])
     })
   })
 
@@ -15,8 +22,8 @@ describe('participantsReducer', () => {
     it('returns the participants from the action', () => {
       const participant = {id: '2'}
       const screening = {id: '1', name: 'mock_screening', participants: [participant]}
-      const action = screeningActions.fetchScreeningSuccess(screening)
-      expect(participantsReducer(Immutable.Map(), action).toJS()).toEqual([participant])
+      const action = fetchScreeningSuccess(screening)
+      expect(participantsReducer(Map(), action).toJS()).toEqual([participant])
     })
   })
 
@@ -24,21 +31,21 @@ describe('participantsReducer', () => {
     it('returns the screening from the action', () => {
       const participant = {id: '2'}
       const screening = {id: '1', name: 'mock_screening', participants: [participant]}
-      const action = screeningActions.updateScreeningSuccess(screening)
-      expect(participantsReducer(Immutable.Map(), action).toJS()).toEqual([participant])
+      const action = updateScreeningSuccess(screening)
+      expect(participantsReducer(Map(), action).toJS()).toEqual([participant])
     })
   })
 
   describe('on CREATE_PARTICIPANT_SUCCESS', () => {
     const newParticipant = {id: '2', screening_id: '1', person_id: '3'}
-    const oldParticipants = Immutable.List([{id: '3', screening_id: '1', person_id: '4'}])
+    const oldParticipants = List([{id: '3', screening_id: '1', person_id: '4'}])
     let action
     beforeEach(() => {
-      action = screeningActions.createParticipantSuccess(newParticipant)
+      action = createParticipantSuccess(newParticipant)
     })
 
     it('returns the screening with new participant from the action', () => {
-      expect(participantsReducer(Immutable.List([]), action).toJS()).toEqual([
+      expect(participantsReducer(List([]), action).toJS()).toEqual([
         newParticipant,
       ])
     })
@@ -55,8 +62,8 @@ describe('participantsReducer', () => {
     it('returns the screening with updated participants from the action', () => {
       const oldParticipant = {id: '2', screening_id: '3', legacy_id: '10', ssn: '12345'}
       const newParticipant = {id: '2', screening_id: '3', legacy_id: '10', ssn: '78456'}
-      const participants = Immutable.fromJS([oldParticipant])
-      const action = screeningActions.updateParticipantSuccess(newParticipant)
+      const participants = fromJS([oldParticipant])
+      const action = updateParticipantSuccess(newParticipant)
       expect(participantsReducer(participants, action).toJS()).toEqual([newParticipant])
     })
   })
@@ -65,8 +72,8 @@ describe('participantsReducer', () => {
     it('returns the screening without the deleted participant from the action', () => {
       const firstParticipant = {id: '2', screening_id: '1', legacy_id: '3'}
       const secondParticipant = {id: '3', screening_id: '1', legacy_id: '4'}
-      const participants = Immutable.fromJS([firstParticipant, secondParticipant])
-      const action = screeningActions.deleteParticipantSuccess(secondParticipant.id)
+      const participants = fromJS([firstParticipant, secondParticipant])
+      const action = deleteParticipantSuccess(secondParticipant.id)
       expect(participantsReducer(participants, action).toJS()).toEqual([
         firstParticipant,
       ])
--- spec/javascripts/reducers/relationshipsReducerSpec.js
+++ spec/javascripts/reducers/relationshipsReducerSpec.js
@@ -1,20 +1,23 @@
 import relationshipsReducer from 'reducers/relationshipsReducer'
-import * as screeningActions from 'actions/screeningActions'
-import Immutable from 'immutable'
+import {
+  fetchRelationshipsByScreeningIdSuccess,
+  createScreeningSuccess,
+} from 'actions/screeningActions'
+import {List, fromJS} from 'immutable'
 
 describe('relationshipsReducer', () => {
   describe('on FETCH_RELATIONSHIPS_SUCCESS', () => {
     it('returns the relationships from the action', () => {
       const relationships = [{id: 1}, {id: 2}]
-      const action = screeningActions.fetchRelationshipsByScreeningIdSuccess(relationships)
-      expect(relationshipsReducer(Immutable.List(), action).toJS()).toEqual(relationships)
+      const action = fetchRelationshipsByScreeningIdSuccess(relationships)
+      expect(relationshipsReducer(List(), action).toJS()).toEqual(relationships)
     })
   })
 
   describe('on CREATE_SCREENING_SUCCESS', () => {
     it('returns an empty immutable list', () => {
-      const action = screeningActions.createScreeningSuccess({})
-      expect(relationshipsReducer(Immutable.fromJS({relationships: [{id: 1}]}), action)).toEqual(Immutable.List())
+      const action = createScreeningSuccess({})
+      expect(relationshipsReducer(fromJS({relationships: [{id: 1}]}), action)).toEqual(List())
     })
   })
 })
