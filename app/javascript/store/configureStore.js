069875d1f175f1dc402a4a4b1b1633adfd51a961
Nate Jones <Nate.Jones@OSI.ca.gov>
2017-08-26 12:49:23 -0700

    Add redux-saga middleware to store

--- app/javascript/store/configureStore.js
+++ app/javascript/store/configureStore.js
@@ -2,13 +2,15 @@ import rootReducer from 'reducers'
 import thunk from 'redux-thunk'
 import {createStore, applyMiddleware} from 'redux'
 import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly'
+import createSagaMiddleware from 'redux-saga'
 
 export default function configureStore(initialState) {
+  const sagaMiddleware = createSagaMiddleware()
   return createStore(
     rootReducer, /* preloadedState, */
     initialState,
     composeWithDevTools(
-      applyMiddleware(thunk)
+      applyMiddleware(sagaMiddleware, thunk)
     )
   )
 }
