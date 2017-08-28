bdea5caf09746ca572b7ff9140c8706c5d77459d
Nate Jones <Nate.Jones@OSI.ca.gov>
2017-08-28 15:53:34 -0700

    Extract function for rendering screening status
    [#150318232]

--- app/javascript/screenings/ScreeningsTable.jsx
+++ app/javascript/screenings/ScreeningsTable.jsx
@@ -5,45 +5,50 @@ import SCREENING_DECISION_OPTIONS from '../enums/ScreeningDecisionOptions'
 import moment from 'moment'
 import {Link} from 'react-router'
 
-const ScreeningsTable = ({screenings}) => (
-  <div className='table-responsive'>
-    <table className='table table-hover'>
-      <thead>
-        <tr>
-          <th className='col-md-3' scope='col'>Screening Name</th>
-          <th scope='col'>Type/Decision</th>
-          <th scope='col'>Status</th>
-          <th scope='col'>Assignee</th>
-          <th scope='col'>Report Date and Time</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          screenings.map(({id, name, screening_decision, screening_decision_detail, assignee, started_at}) => {
-            const screeningName = name ? name : id
-            const responseTimes = SCREENING_DECISION_OPTIONS[screening_decision]
-            const screeningDecision =
-              (['promote_to_referral', 'screen_out'].includes(screening_decision)) ?
-                responseTimes.values[screening_decision_detail] :
-                SCREENING_DECISION[screening_decision]
-            return (
-              <tr key={id}>
-                <td><Link to={`/screenings/${id}`}>{screeningName}</Link></td>
-                <td>{screeningDecision}</td>
-                <td>&nbsp;</td>
-                <td>{assignee}</td>
-                <td>
-                  {moment(started_at).format('L LT')} <br/>
-                  <em className='text-muted'>({moment(started_at).fromNow()})</em>
-                </td>
-              </tr>
-            )
-          })
-        }
-      </tbody>
-    </table>
-  </div>
-)
+const ScreeningsTable = ({screenings}) => {
+  const screeningStatus = (screening_decision, screening_decision_detail) => {
+    if (['promote_to_referral', 'screen_out'].includes(screening_decision)) {
+      const responseTimes = SCREENING_DECISION_OPTIONS[screening_decision]
+      return responseTimes.values[screening_decision_detail]
+    } else {
+      return SCREENING_DECISION[screening_decision]
+    }
+  }
+  return (
+    <div className='table-responsive'>
+      <table className='table table-hover'>
+        <thead>
+          <tr>
+            <th className='col-md-3' scope='col'>Screening Name</th>
+            <th scope='col'>Type/Decision</th>
+            <th scope='col'>Status</th>
+            <th scope='col'>Assignee</th>
+            <th scope='col'>Report Date and Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            screenings.map(({id, name, screening_decision, screening_decision_detail, assignee, started_at, referral_id}) => {
+              const screeningName = name ? name : id
+              return (
+                <tr key={id}>
+                  <td><Link to={`/screenings/${id}`}>{screeningName}</Link></td>
+                  <td>{screeningStatus(screening_decision, screening_decision_detail)}</td>
+                  <td>&nbsp;</td>
+                  <td>{assignee}</td>
+                  <td>
+                    {moment(started_at).format('L LT')} <br/>
+                    <em className='text-muted'>({moment(started_at).fromNow()})</em>
+                  </td>
+                </tr>
+                )
+            })
+          }
+        </tbody>
+      </table>
+    </div>
+  )
+}
 
 ScreeningsTable.propTypes = {
   screenings: PropTypes.array,
