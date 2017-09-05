5704e769fd8dcc7acc4f48e9810fa4734ac22a3b
John Gedeon and Thomas Ramirez <pair+John.Gedeon+Thomas.Ramirez@OSI.ca.gov>
2017-09-05 16:27:34 -0700

    Implement feature test
    [#148611067]

--- spec/features/screening/participant/create_participant_spec.rb
+++ spec/features/screening/participant/create_participant_spec.rb
@@ -62,6 +62,28 @@ feature 'Edit Screening' do
       last_name: 'Simpson',
       ssn: '123-23-1234',
       sealed: false,
+      sensitive: false,
+      languages: %w[French Italian],
+      legacy_descriptor: FactoryGirl.create(:legacy_descriptor),
+      addresses: [marge_address],
+      phone_numbers: [marge_phone_number],
+      races: [
+        { race: 'White', race_detail: 'European' },
+        { race: 'American Indian or Alaska Native' }
+      ],
+      ethnicity: { hispanic_latino_origin: 'Yes', ethnicity_detail: 'Central American' }
+    )
+  end
+  let(:homer) do
+    FactoryGirl.create(
+      :person_search,
+      legacy_source_table: 'CLIENT_T',
+      date_of_birth: marge_date_of_birth.to_s(:db),
+      first_name: 'Homer',
+      gender: 'male',
+      last_name: 'Simpson',
+      ssn: '123-23-1234',
+      sealed: false,
       sensitive: true,
       languages: %w[French Italian],
       legacy_descriptor: FactoryGirl.create(:legacy_descriptor),
@@ -83,6 +105,14 @@ feature 'Edit Screening' do
         :get,
         host_url(ExternalRoutes.intake_api_people_search_v2_path(search_term: search_text))
       ).and_return(json_body([marge].to_json, status: 200))
+    end
+    stub_request(:get, host_url(ExternalRoutes.intake_api_screening_path(existing_screening.id)))
+      .and_return(json_body(existing_screening.to_json, status: 200))
+    %w[Ho].each do |search_text|
+      stub_request(
+        :get,
+        host_url(ExternalRoutes.intake_api_people_search_v2_path(search_term: search_text))
+      ).and_return(json_body([homer].to_json, status: 200))
     end
     stub_empty_relationships_for_screening(existing_screening)
     stub_empty_history_for_screening(existing_screening)
@@ -183,6 +213,97 @@ feature 'Edit Screening' do
         expect(page).to have_field('Address Type', with: marge.addresses.first.type)
         expect(page).to have_button 'Cancel'
         expect(page).to have_button 'Save'
+      end
+    end
+  end
+
+  context 'adding a sensitive participant from search results' do
+    let(:sensitive_token) { 'SENSITIVE_TOKEN' }
+    let(:insensitive_token) { 'INSENSITIVE_TOKEN' }
+
+    before do
+      stub_request(:get, %r{https?://.*/authn/validate\?token=#{sensitive_token}})
+        .and_return(status: 200,
+                    body: { staffId: '123', privileges: ['Sensitive Persons'] }.to_json)
+      stub_request(:get, %r{https?://.*/authn/validate\?token=#{insensitive_token}})
+        .and_return(status: 200, body: { staffId: '123', privileges: [] }.to_json)
+      stub_request(:get, host_url(ExternalRoutes.intake_api_staff_path('123')))
+        .and_return(json_body({ staffId: '123', first_name: 'Bob', last_name: 'Boberson',
+                                county: 'San Francisco' }.to_json, status: 200))
+    end
+
+    scenario 'with NO privileges to add sensitive' do
+      Feature.run_with_activated(:authentication) do
+        visit edit_screening_path(id: existing_screening.id, token: insensitive_token)
+        sensitive_homer_attributes = build_participant_from_person_and_screening(
+          homer,
+          existing_screening
+        )
+        sensitive_participant_homer = FactoryGirl.build(:participant, sensitive_homer_attributes)
+        created_participant_homer = FactoryGirl.create(
+          :participant,
+          sensitive_participant_homer.as_json
+        )
+        stub_request(:post, host_url(ExternalRoutes.intake_api_participants_path))
+          .and_return(json_body(created_participant_homer.to_json, status: 201))
+
+        fill_in 'Title/Name of Screening', with: 'The Rocky Horror Picture Show'
+
+        within '#search-card', text: 'Search' do
+          fill_in_autocompleter 'Search for any person', with: 'Ho'
+          find('li', text: 'Homer Simpson').click
+        end
+        expect(a_request(:post, host_url(ExternalRoutes.intake_api_participants_path))
+          .with(json_body(sensitive_participant_homer.to_json(except: :id)))).to have_been_made
+
+        # adding participant doesnt change screening modifications
+        expect(page).to have_field('Title/Name of Screening', with: 'The Rocky Horror Picture Show')
+
+        # The new participant was added to the top of the list of participants
+        created_participant_selector = edit_participant_card_selector(created_participant_homer.id)
+        existing_participant_selector = edit_participant_card_selector(existing_participant.id)
+        expect(find("#{created_participant_selector}+div"))
+          .to match_css(existing_participant_selector)
+
+        expect(page)
+          .to_not have_selector(edit_participant_card_selector(created_participant_homer.id))
+      end
+    end
+
+    scenario 'with privileges to add sensitive' do
+      Feature.run_with_activated(:authentication) do
+        visit edit_screening_path(id: existing_screening.id, token: sensitive_token)
+        sensitive_homer_attributes = build_participant_from_person_and_screening(
+          homer,
+          existing_screening
+        )
+        sensitive_participant_homer = FactoryGirl.build(:participant, sensitive_homer_attributes)
+        created_participant_homer = FactoryGirl.create(
+          :participant,
+          sensitive_participant_homer.as_json
+        )
+        stub_request(:post, host_url(ExternalRoutes.intake_api_participants_path))
+          .and_return(json_body(created_participant_homer.to_json, status: 201))
+
+        fill_in 'Title/Name of Screening', with: 'The Rocky Horror Picture Show'
+
+        within '#search-card', text: 'Search' do
+          fill_in_autocompleter 'Search for any person', with: 'Ho'
+          find('li', text: 'Homer Simpson').click
+        end
+        expect(a_request(:post, host_url(ExternalRoutes.intake_api_participants_path))
+          .with(json_body(sensitive_participant_homer.to_json(except: :id)))).to have_been_made
+
+        # adding participant doesnt change screening modifications
+        expect(page).to have_field('Title/Name of Screening', with: 'The Rocky Horror Picture Show')
+
+        # The new participant was added to the top of the list of participants
+        created_participant_selector = edit_participant_card_selector(created_participant_homer.id)
+        existing_participant_selector = edit_participant_card_selector(existing_participant.id)
+        expect(find("#{created_participant_selector}+div"))
+          .to match_css(existing_participant_selector)
+
+        expect(page).to have_selector(edit_participant_card_selector(created_participant_homer.id))
       end
     end
   end
