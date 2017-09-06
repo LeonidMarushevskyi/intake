c416e80279635ae9d5d8e9856f6a0c389501838f
Bruno Fagundez <bruno.fagundez@osi.ca.gov>
2017-09-06 16:10:03 -0700

    Revert remove of babel-polyfill, still required

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,7 @@
 //
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
+import 'babel-polyfill'
 import 'jquery'
 import 'common/jquery-helpers'
 
