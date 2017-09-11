e766829d702fbe605ab10db00aa570bb69c60b0c
Sara Bocciardi <Sara.Bocciardi@OSI.ca.gov>
2017-09-11 11:00:53 -0700

    Only look within the current path, not whole page
    [#150411380]

--- spec/features/show_investigation_spec.rb
+++ spec/features/show_investigation_spec.rb
@@ -33,20 +33,20 @@ feature 'Show Investigation' do
     visit investigation_path(id: investigation_id)
     within '.card.show', text: 'Screening Summary' do
       within '.card-body' do
-        within :xpath, "//div[@class='row'][1]" do
+        within :xpath, ".//div[@class='row'][1]" do
           expect(page).to have_link 'My Screening', href: screening_path(id: '222')
           expect(page).to have_content 'Immediate'
           expect(page).to have_content 'Severe neglect', count: 1
           expect(page).to have_content 'Sexual abuse'
         end
-        within :xpath, "//div[@class='row'][2]" do
+        within :xpath, ".//div[@class='row'][2]" do
           expect(page).to have_content 'Dangerous Animal on Premises'
           expect(page).to have_content 'Firearms in Home'
           expect(page).to have_content 'Hostile, Aggressive Client'
           expect(page).to have_content 'Remote or Isolated Location'
           expect(page).to have_content 'Animal is a tiger'
         end
-        within :xpath, "//div[@class='row'][3]" do
+        within :xpath, ".//div[@class='row'][3]" do
           expect(page).to have_content 'There was considerable evidence abuse'
         end
       end
