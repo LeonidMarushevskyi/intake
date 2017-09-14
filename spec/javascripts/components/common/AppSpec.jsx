ee30f1199b659b9e30f866013afb4dd4705f6398
Nate Jones <Nate.Jones@OSI.ca.gov>
2017-09-14 09:23:50 -0700

    App component has children as a required prop
    [#150681054]

--- spec/javascripts/components/common/AppSpec.jsx
+++ spec/javascripts/components/common/AppSpec.jsx
@@ -5,7 +5,7 @@ import {mount} from 'enzyme'
 describe('App', () => {
   it('fetches the system codes when the component mounts', () => {
     const fetch = jasmine.createSpy('fetch')
-    mount(<App actions={{fetch}} />)
+    mount(<App actions={{fetch}}><div/></App>)
     expect(fetch).toHaveBeenCalled()
   })
 })
