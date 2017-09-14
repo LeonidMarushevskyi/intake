7af5223c7bb1e821247c955a07098dd6c8f0ad7e
Nate Jones <Nate.Jones@OSI.ca.gov>
2017-09-14 09:10:23 -0700

    Stub system codes for create contact spec
    [#150681054]

--- spec/features/contacts/create_investigation_contact_spec.rb
+++ spec/features/contacts/create_investigation_contact_spec.rb
@@ -4,10 +4,10 @@ require 'rails_helper'
 
 feature 'Create Investigation Contact' do
   scenario 'user can add new contact' do
-    investigation_id = '123ABC'
-    visit new_investigation_contact_path(investigation_id: investigation_id)
+    stub_system_codes
+    visit new_investigation_contact_path(investigation_id: '123ABC')
     within '.card-header' do
-      expect(page).to have_content("New Contact - Investigation #{investigation_id}")
+      expect(page).to have_content('New Contact - Investigation 123ABC')
     end
     within '.card-body' do
       fill_in_datepicker 'Date/Time', with: '08/17/2016 3:00 AM'
