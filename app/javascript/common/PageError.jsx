3136630f81b5b8079f64fa48a38aa9040ae0d1d4
Aaron Smith <aaron.smith@osi.ca.gov>
2017-09-18 11:35:52 -0700

    Add page-level remote error display component
    [#146390441]

---
+++ app/javascript/common/PageError.jsx
@@ -0,0 +1,44 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+export class PageError extends React.Component {
+  constructor(props, context) {
+    super(props, context)
+    this.toggleOpen = this.toggleOpen.bind(this)
+  }
+
+  componentWillMount() {
+    this.setState({expanded: false})
+  }
+
+  toggleOpen() {
+    this.setState({expanded: !this.state.expanded})
+  }
+
+  render() {
+    const messages = this.props.messageObject.toJS()
+    const problemMessage = 'There was a problem with your request and the server returned an error.'
+    const instructions = 'Please save the output below and include it with any support requests.'
+    const openClass = this.state.expanded ? 'expanded' : 'collapsed'
+    const buttonText = this.state.expanded ? 'Collapse' : 'Expand'
+    return (
+      <div className={`page-error ${openClass}`}>
+        <div className='container'>
+          <div className='row padded'>
+            <p className='no-gap'>{problemMessage} {instructions}</p>
+            <button onClick={this.toggleOpen}>{buttonText}</button>
+            {
+              Object.keys(messages).map((key) => <p className='no-gap no-gap-bottom' key={key}><strong>{key}:</strong> <span>{messages[key]}</span></p>)
+            }
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+PageError.propTypes = {
+  messageObject: PropTypes.object.isRequired,
+}
+
+export default PageError
