a036b6618e3877db76f66c2c628d9a314a599773
Nate Jones <Nate.Jones@OSI.ca.gov>
2017-09-20 16:45:51 -0700

    Extract findByCategory selector for system codes
    [#150680891]

--- app/javascript/reducers/systemCodes/contactPurposesReducer.js
+++ app/javascript/reducers/systemCodes/contactPurposesReducer.js
@@ -1,12 +1,11 @@
 import {createReducer} from 'utils/createReducer'
 import {List, fromJS} from 'immutable'
 import {FETCH_SYSTEM_CODES_SUCCESS} from 'actions/systemCodesActions'
+import {findByCategory} from 'reducers/systemCodes/contactStatusesReducer'
 const CONTACT_PURPOSE = 'contact_purpose'
-const findContactPurposes = (statusCodes = []) => (
-  statusCodes.filter(({category}) => category === CONTACT_PURPOSE)
-)
+
 export default createReducer(List(), {
   [FETCH_SYSTEM_CODES_SUCCESS](state, {systemCodes}) {
-    return fromJS(findContactPurposes(systemCodes))
+    return fromJS(findByCategory(systemCodes, CONTACT_PURPOSE))
   },
 })
--- app/javascript/reducers/systemCodes/contactStatusesReducer.js
+++ app/javascript/reducers/systemCodes/contactStatusesReducer.js
@@ -2,11 +2,12 @@ import {createReducer} from 'utils/createReducer'
 import {List, fromJS} from 'immutable'
 import {FETCH_SYSTEM_CODES_SUCCESS} from 'actions/systemCodesActions'
 const CONTACT_STATUS = 'contact_status'
-const findContactStatuses = (statusCodes = []) => (
-  statusCodes.filter(({category}) => category === CONTACT_STATUS)
+
+export const findByCategory = (statusCodes = [], selectedCategory) => (
+  statusCodes.filter(({category}) => category === selectedCategory)
 )
 export default createReducer(List(), {
   [FETCH_SYSTEM_CODES_SUCCESS](state, {systemCodes}) {
-    return fromJS(findContactStatuses(systemCodes))
+    return fromJS(findByCategory(systemCodes, CONTACT_STATUS))
   },
 })
