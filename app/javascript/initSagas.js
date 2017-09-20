a123d95d69255d05f45d3daec0e4623a1b2a0ef1
John Gedeon <John.Gedeon@OSI.ca.gov>
2017-09-20 15:22:04 -0700

    Fix lint error
    [#hotfix]

--- app/javascript/initSagas.js
+++ app/javascript/initSagas.js
@@ -3,5 +3,5 @@ import * as sagas from 'sagas'
 export const initSagas = (sagaMiddleware) => (
   // Replacing this statement because firefox 45 does not understand Object.values
   // Object.values(sagas) == Object.keys(sagas).map(e => sagas[e])
-  Object.keys(sagas).map(e => sagas[e]).forEach(sagaMiddleware.run.bind(sagaMiddleware))
+  Object.keys(sagas).map((e) => sagas[e]).forEach(sagaMiddleware.run.bind(sagaMiddleware))
 )
