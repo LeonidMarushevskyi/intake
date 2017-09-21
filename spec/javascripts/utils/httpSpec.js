b5eb39a01de2ca7fe1e00aa4b786da7ee6c07451
Nate Jones <Nate.Jones@OSI.ca.gov>
2017-09-20 17:02:09 -0700

    Add missing promise rejection to http spec
    [#fix]

--- spec/javascripts/utils/httpSpec.js
+++ spec/javascripts/utils/httpSpec.js
@@ -3,34 +3,26 @@ import * as Config from 'common/config'
 import {store} from 'store/configureStore'
 
 describe('request', () => {
-  const fakeConfig = {base_path: ''}
-  const mocks = {
-    error: {
-      status: 500,
-      responseText: JSON.stringify({why: 'Did not stick to the plan'}),
-    },
-  }
-
   beforeEach(() => {
     jasmine.Ajax.install()
+    const fakeConfig = {base_path: ''}
     spyOn(Config, 'config').and.callFake(() => fakeConfig)
   })
 
   afterEach(() => jasmine.Ajax.uninstall())
 
   describe('error handling', () => {
-    let storeSpy
-    let request
-
     beforeEach(() => {
-      storeSpy = spyOn(store, 'dispatch').and.callThrough()
+      spyOn(store, 'dispatch')//.and.callThrough()
     })
 
     it('dispatches the http error action', () => {
-      Http.request('GET', '/')
-      request = jasmine.Ajax.requests.mostRecent()
-      request.respondWith(mocks.error)
-      expect(storeSpy).toHaveBeenCalledWith({
+      Http.request('GET', '/').catch(() => null)
+      jasmine.Ajax.requests.mostRecent().respondWith({
+        status: 500,
+        responseText: JSON.stringify({why: 'Did not stick to the plan'}),
+      })
+      expect(store.dispatch).toHaveBeenCalledWith({
         type: 'HTTP_FAILURE',
         responseJSON: {why: 'Did not stick to the plan'},
       })
