79dce1a692badb7b06ca14e974a425cefe573e74
Nate Jones <Nate.Jones@OSI.ca.gov>
2017-09-25 15:19:33 -0700

    Combine provider component with routes
    no reason these components should be separated
    
    [#150898493]

--- app/javascript/common/routes.jsx
+++ app/javascript/common/routes.jsx
@@ -8,6 +8,8 @@ import ScreeningSummaryContainer from 'investigations/ScreeningSummaryContainer'
 import ContactLogContainer from 'investigations/ContactLogContainer'
 import ContactContainer from 'investigations/ContactContainer'
 import ContactShowContainer from 'investigations/ContactShowContainer'
+import {store} from 'store/configureStore'
+import {Provider} from 'react-redux'
 
 const InvestigationPage = (props) => (
   <div>
@@ -17,14 +19,16 @@ const InvestigationPage = (props) => (
 )
 
 export default (
-  <Router history={history} >
-    <Route path='/' component={App}>
-      <IndexRoute component={HomePage} />
-      <Route path='screenings/:id' component={ScreeningPage}/>
-      <Route path='screenings/:id/:mode' component={ScreeningPage} />
-      <Route path='investigations/:id' component={InvestigationPage} />
-      <Route path='investigations/:investigation_id/contacts/new' component={ContactContainer} />
-      <Route path='investigations/:investigation_id/contacts/:id' component={ContactShowContainer} />
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={history} >
+      <Route path='/' component={App}>
+        <IndexRoute component={HomePage} />
+        <Route path='screenings/:id' component={ScreeningPage}/>
+        <Route path='screenings/:id/:mode' component={ScreeningPage} />
+        <Route path='investigations/:id' component={InvestigationPage} />
+        <Route path='investigations/:investigation_id/contacts/new' component={ContactContainer} />
+        <Route path='investigations/:investigation_id/contacts/:id' component={ContactShowContainer} />
+      </Route>
+    </Router>
+  </Provider>
 )
