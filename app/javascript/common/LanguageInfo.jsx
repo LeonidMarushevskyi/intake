3aa974afe8e69815d72c5ab282ab7ec3c4238d2c
Aman Alshurafa <Aman.Alshurafa@OSI.ca.gov>
2017-10-03 20:54:21 -0700

    People search results flag primary language
    [#138673963]

--- app/javascript/common/LanguageInfo.jsx
+++ app/javascript/common/LanguageInfo.jsx
@@ -1,8 +1,13 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
+export const flagPrimaryLanguage = (languages) => {
+  if (languages[0]) languages[0] += ' (Primary)'
+  return languages
+}
+
 const LanguageInfo = ({languages}) => {
-  const lan = languages && languages.filter(Boolean).join(', ')
+  const lan = languages && flagPrimaryLanguage(languages.filter(Boolean)).join(', ')
   return (lan ? <div><strong className='c-gray half-pad-right'>Language</strong><span>{lan}</span></div> : null)
 }
 
