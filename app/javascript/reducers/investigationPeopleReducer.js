f44682698c7dc05b179cfd3000a1097eaaa93f8a
Nate Jones <Nate.Jones@OSI.ca.gov>
2017-10-04 16:32:05 -0700

    Investigation people reducer handles investigation fetch
    Investigation API includes investigation people which can used to
    update the investigation people reducer
    
    [#151504784]

--- app/javascript/reducers/investigationPeopleReducer.js
+++ app/javascript/reducers/investigationPeopleReducer.js
@@ -1,9 +1,12 @@
 import {createReducer} from 'utils/createReducer'
 import {fromJS, List} from 'immutable'
 import {FETCH_INVESTIGATION_PEOPLE_SUCCESS} from 'actions/investigationPeopleActions'
+import {FETCH_INVESTIGATION_SUCCESS} from 'actions/investigationActions'
 
-const getInvestigationPeople = (state, {people}) => (fromJS(people))
+const getPeople = (_, {people}) => fromJS(people)
+const getPeopleOnInvestigation = (_, {investigation}) => fromJS(investigation.people)
 
 export default createReducer(List(), {
-  [FETCH_INVESTIGATION_PEOPLE_SUCCESS]: getInvestigationPeople,
+  [FETCH_INVESTIGATION_PEOPLE_SUCCESS]: getPeople,
+  [FETCH_INVESTIGATION_SUCCESS]: getPeopleOnInvestigation,
 })
--- spec/javascripts/reducers/investigationPeopleReducerSpec.js
+++ spec/javascripts/reducers/investigationPeopleReducerSpec.js
@@ -1,15 +1,28 @@
 import * as matchers from 'jasmine-immutable-matchers'
 import {fetchSuccess} from 'actions/investigationPeopleActions'
+import {fetchSuccess as fetchInvestigationSuccess} from 'actions/investigationActions'
 import investigationPeopleReducer from 'reducers/investigationPeopleReducer'
 import {List, fromJS} from 'immutable'
 
 describe('investigationPeoplReducer', () => {
   beforeEach(() => jasmine.addMatchers(matchers))
+  const people = [{first_name: 'Bob'}]
+
   describe('on FETCH_INVESTIGATION_PEOPLE_SUCCESS', () => {
     it('returns the list of people from the action', () => {
-      const people = fromJS([{first_name: 'Bob'}])
-      const action = fetchSuccess(people.toJS())
-      expect(investigationPeopleReducer(List(), action)).toEqualImmutable(people)
+      const action = fetchSuccess(people)
+      expect(investigationPeopleReducer(List(), action)).toEqualImmutable(
+        fromJS(people)
+      )
+    })
+  })
+  describe('on FETCH_INVESTIGATION_SUCCESS', () => {
+    it('returns the list of people on the investigation action', () => {
+      const investigation = {people}
+      const action = fetchInvestigationSuccess(investigation)
+      expect(investigationPeopleReducer(List(), action)).toEqualImmutable(
+        fromJS(people)
+      )
     })
   })
 })
