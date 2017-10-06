dcd8dcc776a95b97c35d56023446b7ec8247d00a
Nate Jones <Nate.Jones@OSI.ca.gov>
2017-10-06 10:09:40 -0700

    Create contact with people
    When a contact is created people should be passed
    to the create contact API
    
    [#150898493]

--- app/javascript/actions/contactActions.js
+++ app/javascript/actions/contactActions.js
@@ -1,8 +1,8 @@
 export const CREATE_CONTACT = 'CREATE_CONTACT'
 export const CREATE_CONTACT_SUCCESS = 'CREATE_CONTACT_SUCCESS'
 export const CREATE_CONTACT_FAILURE = 'CREATE_CONTACT_FAILURE'
-export function create({investigation_id, started_at, status, note, purpose, communication_method, location}) {
-  return {type: CREATE_CONTACT, investigation_id, started_at, status, note, purpose, communication_method, location}
+export function create({investigation_id, started_at, status, note, purpose, communication_method, location, people}) {
+  return {type: CREATE_CONTACT, investigation_id, started_at, status, note, purpose, communication_method, location, people}
 }
 export function createSuccess({id, started_at, status, note, purpose, communication_method, location}) {
   return {type: CREATE_CONTACT_SUCCESS, id, started_at, status, note, purpose, communication_method, location}
