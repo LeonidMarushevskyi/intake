a844b8e08b61c6ed819b9cc56b0dee07dea87cf1
Sara Bocciardi <Sara.Bocciardi@OSI.ca.gov>
2017-10-13 17:21:44 -0700

    Add actions for narrative form
    [#refactor]

--- app/javascript/screenings/narrative/NarrativeFormContainer.jsx
+++ app/javascript/screenings/narrative/NarrativeFormContainer.jsx
@@ -1,25 +1,25 @@
 import NarrativeForm from 'screenings/narrative/NarrativeForm'
 // import {getErrorsSelector} from 'selectors/narrativeFormSelectors'
-// import * as narrativeFormActions from 'actions/narrativeFormActions'
-// import {saveScreening as 'save'} from 'actions/screeningActions'
+import * as narrativeFormActions from 'actions/narrativeFormActions'
+import {saveScreening} from 'actions/screeningActions'
 import {connect} from 'react-redux'
-// import {bindActionCreators} from 'redux'
+import {bindActionCreators} from 'redux'
 
-const mapStateToProps = (state, ownProps) => (
-  {
+const mapStateToProps = (state, ownProps) => {
+  const reportNarrative = state.getIn(['narrativeForm', 'report_narrative', 'value'])
+  return {
     onCancel: ownProps.onCancel,
     errors: [],
-    reportNarrative: state.getIn(['screening', 'report_narrative']),
+    reportNarrative,
     screening: state.get('screening').toJS(),
-    screeningWithEdits: {},
+    screeningWithEdits: state.get('screening').set('report_narrative', reportNarrative).toJS(),
   }
-)
+}
 
-const mapDispatchToProps = (_dispatch, _ownProps) => (
-  // const actions = Object.assign(narrativeFormActions, {saveScreening})
-  // return {actions: bindActionCreators(actions, dispatch)}
-  {actions: {}}
-)
+const mapDispatchToProps = (dispatch, _ownProps) => {
+  const actions = {...narrativeFormActions, saveScreening}
+  return {actions: bindActionCreators(actions, dispatch)}
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(NarrativeForm)
 
