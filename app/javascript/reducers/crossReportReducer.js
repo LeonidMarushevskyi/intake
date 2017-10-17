be8a2d03c9c72e9dfb7dfb565d059b7f9187aeee
John Gedeon <John.Gedeon@OSI.ca.gov>
2017-10-16 17:32:56 -0700

    Add reducer for CrossReportForm component
    [#150924573]

--- app/javascript/reducers/crossReportReducer.js
+++ app/javascript/reducers/crossReportReducer.js
@@ -1,9 +1,54 @@
 import {createReducer} from 'utils/createReducer'
-import {Map} from 'immutable'
+import {Map, fromJS} from 'immutable'
 import {SET_CROSS_REPORT_FIELD} from 'actions/crossReportActions'
+import {FETCH_SCREENING_SUCCESS} from 'actions/actionTypes'
+import {
+  AGENCY_TYPES,
+} from 'enums/CrossReport'
 
 export default createReducer(Map(), {
   [SET_CROSS_REPORT_FIELD](state, {field, value}) {
     return state.setIn([field, 'value'], value)
+  },
+  [FETCH_SCREENING_SUCCESS](_state, {screening: {cross_reports}}) {
+    if (cross_reports && cross_reports.length > 0) {
+      const crossReport = cross_reports[0]
+      const agencyInformation = Object.keys(AGENCY_TYPES).reduce((info, type) => {
+        info[type] = {
+          selected: false,
+          touched: false,
+          agency: {
+            value: '',
+            touched: false,
+          },
+        }
+        return info
+      }, {})
+      if (crossReport.agencies) {
+        crossReport.agencies.map((agency) => {
+          agencyInformation[agency.type].selected = true
+          if (agency.id) {
+            agencyInformation[agency.type].agency.value = agency.id
+          }
+        })
+      }
+      return fromJS({
+        county_id: {
+          value: crossReport.county_id,
+          touched: false,
+        },
+        inform_date: {
+          value: crossReport.inform_date,
+          touched: false,
+        },
+        method: {
+          value: crossReport.method,
+          touched: false,
+        },
+        ...agencyInformation,
+      })
+    } else {
+      return Map()
+    }
   },
 })
--- spec/javascripts/reducers/crossReportReducerSpec.js
+++ spec/javascripts/reducers/crossReportReducerSpec.js
@@ -1,18 +1,92 @@
-import {fromJS} from 'immutable'
+import {Map, fromJS} from 'immutable'
 import * as matchers from 'jasmine-immutable-matchers'
 import crossReportReducer from 'reducers/crossReportReducer'
 import {setField} from 'actions/crossReportActions'
+import {fetchScreeningSuccess} from 'actions/screeningActions'
 
 describe('crossReportReducer', () => {
   beforeEach(() => jasmine.addMatchers(matchers))
 
+  describe('on FETCH_SCREENING_SUCCESS', () => {
+    it('returns the cross report with the values from screening', () => {
+      const action = fetchScreeningSuccess({
+        cross_reports: [
+          {
+            county_id: '1234',
+            inform_date: '2017-02-20',
+            method: 'Child Abuse Form',
+            agencies: [
+              {id: '1234', type: 'DISTRICT_ATTORNEY'},
+              {id: '5234', type: 'LAW_ENFORCEMENT'},
+            ],
+          },
+        ],
+      })
+      expect(crossReportReducer(Map(), action)).toEqualImmutable(
+        fromJS({
+          county_id: {
+            value: '1234',
+            touched: false,
+          },
+          inform_date: {
+            value: '2017-02-20',
+            touched: false,
+          },
+          method: {
+            value: 'Child Abuse Form',
+            touched: false,
+          },
+          DISTRICT_ATTORNEY: {
+            selected: true,
+            touched: false,
+            agency: {
+              value: '1234',
+              touched: false,
+            },
+          },
+          LAW_ENFORCEMENT: {
+            selected: true,
+            touched: false,
+            agency: {
+              value: '5234',
+              touched: false,
+            },
+          },
+          DEPARTMENT_OF_JUSTICE: {
+            selected: false,
+            touched: false,
+            agency: {
+              value: '',
+              touched: false,
+            },
+          },
+          COUNTY_LICENSING: {
+            selected: false,
+            touched: false,
+            agency: {
+              value: '',
+              touched: false,
+            },
+          },
+          COMMUNITY_CARE_LICENSING: {
+            selected: false,
+            touched: false,
+            agency: {
+              value: '',
+              touched: false,
+            },
+          },
+        })
+      )
+    })
+  })
   describe('on SET_CROSS_REPORT_FIELD', () => {
     it('returns the cross report with the newly updated value, but touched remains the same', () => {
-      const action = setField('county', '123')
-      const state = fromJS({county: {value: '321', touched: false}})
+      const action = setField('county_id', '123')
+      const state = fromJS({county_id: {value: '321', touched: false}})
       expect(crossReportReducer(state, action)).toEqualImmutable(
         fromJS({
-          county: {
+          county_id: {
             value: '123',
             touched: false,
           },
