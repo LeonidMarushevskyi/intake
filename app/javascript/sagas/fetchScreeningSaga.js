7652874c8535fe40d0d85597176f659edc1f8340
John Gedeon <John.Gedeon@OSI.ca.gov>
2017-10-18 14:59:58 -0700

    Get countyAgency data on fetchScreening
    [#150924573]

--- app/javascript/sagas/fetchScreeningSaga.js
+++ app/javascript/sagas/fetchScreeningSaga.js
@@ -4,11 +4,19 @@ import {
   fetchScreeningSuccess,
   fetchScreeningFailure,
 } from 'actions/screeningActions'
+import {fetch as fetchCountyAgencies} from 'actions/countyAgenciesActions'
 import {FETCH_SCREENING} from 'actions/actionTypes'
 
 export function* fetchScreening({id}) {
   try {
     const response = yield call(get, `/api/v1/screenings/${id}`)
+    const {cross_reports} = response
+    if (cross_reports && cross_reports.length > 0) {
+      const {county_id} = cross_reports[0]
+      if (county_id && county_id !== '') {
+        yield put(fetchCountyAgencies(county_id))
+      }
+    }
     yield put(fetchScreeningSuccess(response))
   } catch (error) {
     yield put(fetchScreeningFailure(error.responseJSON))
