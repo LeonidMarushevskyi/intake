bdead6e3e5ea84b4b3a76faa536380fbba52be5d
Aman Alshurafa and Thomas Ramirez <pair+Aman.Alshurafa+Thomas.Ramirez@OSI.ca.gov>
2017-10-20 15:33:05 -0700

    Add feature spec
    [#151965191]

---
+++ spec/features/errors_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'feature/testing'
+
+feature 'error pages' do
+  context 'page does not exist' do
+    around do |example|
+      Rails.application.config.consider_all_requests_local = false
+      Rails.application.config.action_dispatch.show_exceptions = true
+      load 'application_controller.rb'
+      example.run
+      Rails.application.config.consider_all_requests_local = true
+      Rails.application.config.action_dispatch.show_exceptions = false
+      load 'application_controller.rb'
+    end
+
+    scenario 'renders 404 page' do
+      visit page_path('this_page_does_not_exist')
+      expect(page).to have_text('Sorry, this is not the page you want')
+      expect(page).to have_text(
+        "It may have been deleted or doesn't exist. Please check the address or"
+      )
+      expect(page).to have_link('return to your dashboard')
+    end
+  end
+end
