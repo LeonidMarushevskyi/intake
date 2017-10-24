5fe353303ed5bc0b734012b5cba74c8e08faaec2
Sara Bocciardi and Tyler Clemens <pair+Sara.Bocciardi+Tyler.Clemens@OSI.ca.gov>
2017-10-24 15:43:03 -0700

    Change mapStateToProps to use the referral selectors
    [#151030845]

--- app/javascript/history/ReferralContainer.jsx
+++ app/javascript/history/ReferralContainer.jsx
@@ -1,8 +1,25 @@
 import {connect} from 'react-redux'
 import ReferralView from 'history/ReferralView'
+import {
+  getStartDateSelector,
+  getEndDateSelector,
+  getReferralIdSelector,
+  getStatusSelector,
+  getNotificationSelector,
+  getCountySelector,
+  getPeopleAndRolesSelector,
+} from 'selectors/historyOfInvolvementReferralSelectors'
 
-const mapStateToProps = (state, ownProps) => (
-  {}
+const mapStateToProps = (state, {index}) => (
+  {
+    startDate: getStartDateSelector(state, index),
+    endDate: getEndDateSelector(state, index),
+    referralId: getReferralIdSelector(state, index),
+    status: getStatusSelector(state, index),
+    notification: getNotificationSelector(state, index),
+    county: getCountySelector(state, index),
+    peopleAndRoles: getPeopleAndRolesSelector(state, index).toJS(),
+  }
 )
 
 export default connect(mapStateToProps)(ReferralView)
