884a68921fd5e613106f1b393b527ad54fcb1091
Sara Bocciardi and Tyler Clemens <pair+Sara.Bocciardi+Tyler.Clemens@OSI.ca.gov>
2017-10-24 16:11:41 -0700

    Create stateless view for screenings in HOI
    - Add react stateless component
    - Add container to map data
    - Add selectors to get data from the store
    
    [#151030845]

--- app/javascript/history/ScreeningView.jsx
+++ app/javascript/history/ScreeningView.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import {dateRangeFormatter} from 'utils/dateFormatter'
 
-const ScreeningView = () => (
-  <div />
+const ScreeningView = ({startDate, endDate, status, county, people, reporter, worker}) => (
+  <tr>
+    <td>{dateRangeFormatter({start_date: startDate, end_date: endDate})}</td>
+    <td>
+      <div className='row'>Screening</div>
+      <div className='row'>({status})</div>
+    </td>
+    <td>{county}</td>
+    <td>
+      <div className='row'>
+        <span className='col-md-12 people'>{people}</span>
+      </div>
+      <div className='row'>
+        <span className='col-md-6 reporter'>Reporter: {reporter}</span>
+        <span className='col-md-6 worker'>Worker: {worker}</span>
+      </div>
+    </td>
+  </tr>
 )
+
+ScreeningView.propTypes = {
+  county: PropTypes.string,
+  endDate: PropTypes.string,
+  people: PropTypes.string,
+  reporter: PropTypes.string,
+  startDate: PropTypes.string,
+  status: PropTypes.string,
+  worker: PropTypes.string,
+}
 
 export default ScreeningView
