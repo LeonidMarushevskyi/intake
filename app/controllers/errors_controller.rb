3c0db8c69933dba53fbc2a046a0495c7ac8920b0
Aman Alshurafa and Thomas Ramirez <pair+Aman.Alshurafa+Thomas.Ramirez@OSI.ca.gov>
2017-10-24 15:24:40 -0700

    Let React handle 404 errors instead of Rails
    [#151965191]

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,7 +2,6 @@
 
 # Errors Controller to handle errors
 class ErrorsController < ApplicationController
-  layout false
   def not_found
     @dashboard_url = Rails.application.config.intake[:dashboard_url]
     render status: 404
