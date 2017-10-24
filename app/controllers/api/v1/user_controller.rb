c3b998bdd9278e1ce0d4a666be012ab60c638a0b
Bruno Fagundez <bruno.fagundez@osi.ca.gov>
2017-10-24 10:22:33 -0700

    Added a user controller to return user info with an api endpoint
    [#151606576]

---
+++ app/controllers/api/v1/user_controller.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# User controller will provide additional info from the current user
+module Api
+  module V1
+    class UserController < ApiController # :nodoc:
+      def user_info
+        user_payload = { first_name: 'Not', last_name: 'Available' }
+
+        user_payload = session[:user_details] if session[:user_details]
+
+        render json: user_payload, status: 200
+      end
+    end
+  end
+end
---
+++ spec/controllers/api/v1/user_controller_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Api::V1::UserController do
+  let(:security_token) { 'security_token' }
+  let(:staff) { FactoryGirl.build(:staff, staff_id: '123') }
+  let(:session) do
+    {
+      'user_details' => staff
+    }
+  end
+
+  describe 'when the information from the user is on the session' do
+    it 'returns the data on get' do
+      process :user_info, method: :get, session: session
+      expect(response.status).to eq(200)
+      expect(response.body).to eq(staff.to_json)
+    end
+  end
+
+  describe 'when the information from the user is not on the session, return not available' do
+    it 'returns nil' do
+      process :user_info, method: :get, session: nil
+      expect(response.status).to eq(200)
+      expect(response.body).to eq({ first_name: 'Not', last_name: 'Available' }.to_json)
+    end
+  end
+end
