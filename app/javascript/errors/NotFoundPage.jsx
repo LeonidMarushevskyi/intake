51022089cabc49c23c9a26ee0d79aacfdec47d00
Aman Alshurafa and Thomas Ramirez <pair+Aman.Alshurafa+Thomas.Ramirez@OSI.ca.gov>
2017-10-25 16:47:27 -0700

    Simplify not found page
    - does not need to be a class
    
    [#151965191]

--- app/javascript/errors/NotFoundPage.jsx
+++ app/javascript/errors/NotFoundPage.jsx
@@ -1,21 +1,19 @@
 import React from 'react'
 import * as IntakeConfig from 'common/config'
 
-export default class NotFoundPage extends React.Component {
-  render() {
-    return (
-      <div className='error-panel centered'>
-        <h4>
-          <i className='fa fa-warning c-red gap-right'/>
-          Sorry, this is not the page you want.
-        </h4>
-        <div className='gap-top'>
-          <span>It may have been deleted or doesn&apos;t exist. Please check the address or</span>
-          <br/>
-          <a href={IntakeConfig.dashboardUrl()}>return to your dashboard</a>
-          <span>.</span>
-        </div>
-      </div>
-    )
-  }
-}
+const NotFoundPage = () => (
+  <div className='error-panel centered'>
+    <h4>
+      <i className='fa fa-warning c-red gap-right'/>
+      Sorry, this is not the page you want.
+    </h4>
+    <div className='gap-top'>
+      <span>It may have been deleted or doesn&apos;t exist. Please check the address or</span>
+      <br/>
+      <a href={IntakeConfig.dashboardUrl()}>return to your dashboard</a>
+      <span>.</span>
+    </div>
+  </div>
+)
+
+export default NotFoundPage
