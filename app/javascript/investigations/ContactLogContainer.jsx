90073a2db9d32d704257d8513c9f07c760e7cf1f
Nate Jones <Nate.Jones@OSI.ca.gov>
2017-10-26 11:47:45 -0700

    Add contact log selector
    Contact log selector transforms contact data to align with
    the contain log component property values. It provides `people`
    as a list of formatted names, `startedAt` as a formatted time,
    communication `method` and `status` as their respective system code
    display values.
    
    [#151030885]

--- app/javascript/investigations/ContactLogContainer.jsx
+++ app/javascript/investigations/ContactLogContainer.jsx
@@ -1,10 +1,12 @@
 import ContactLog from 'investigations/ContactLog'
+import {getContactLogsSelector} from 'selectors/contactLogSelectors'
 import {connect} from 'react-redux'
 
 const mapStateToProps = (state, ownProps) => {
+  const contactLogs = getContactLogsSelector(state).toJS()
   return {
     investigationId: ownProps.id,
-    contactLogs: state.getIn(['investigation', 'contacts']).toJS(),
+    contactLogs,
   }
 }
 
