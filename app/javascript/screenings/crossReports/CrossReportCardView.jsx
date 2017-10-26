2043788850d0a270a3dd97a7f3d24e2706f6e377
John Gedeon <John.Gedeon@OSI.ca.gov>
2017-10-26 16:09:23 -0700

    Remove unused stuff from CrossReportCardView
    - All this is now replaced with the refactored containerized component
    
    [#150924573]

--- app/javascript/screenings/crossReports/CrossReportCardView.jsx
+++ app/javascript/screenings/crossReports/CrossReportCardView.jsx
@@ -1,146 +1,17 @@
-import * as Validator from 'utils/validator'
 import CrossReportFormContainer from 'screenings/crossReports/CrossReportFormContainer'
 import CrossReportShowContainer from 'screenings/crossReports/CrossReportShowContainer'
-import Immutable from 'immutable'
 import PropTypes from 'prop-types'
 import React from 'react'
-import {
-  AGENCY_TYPES,
-  ALLEGATIONS_REQUIRE_CROSS_REPORTS_MESSAGE,
-  CROSS_REPORTS_REQUIRED_FOR_ALLEGATIONS,
-  LAW_ENFORCEMENT,
-  DISTRICT_ATTORNEY,
-  DEPARTMENT_OF_JUSTICE,
-  COUNTY_LICENSING,
-  COMMUNITY_CARE_LICENSING,
-} from 'enums/CrossReport'
 import ScreeningCardHeader from 'screenings/ScreeningCardHeader'
 
 export default class CrossReportCardView extends React.Component {
   constructor(props) {
     super(props)
     this.toggleShow = this.toggleShow.bind(this)
-    this.isAgencyChecked = this.isAgencyChecked.bind(this)
-    this.isAgencyRequired = this.isAgencyRequired.bind(this)
-    this.onEvent = this.onEvent.bind(this)
-    this.onCancel = this.onCancel.bind(this)
-    this.onChange = this.onChange.bind(this)
     this.onEdit = this.onEdit.bind(this)
-    this.onSave = this.onSave.bind(this)
-    this.validateField = this.validateField.bind(this)
-    this.fieldValidations = Immutable.fromJS({
-      [LAW_ENFORCEMENT]: {
-        agency_type: [{
-          rule: 'isRequiredIf',
-          message: 'Please indicate cross-reporting to law enforcement.',
-          condition: () => this.isAgencyRequired(LAW_ENFORCEMENT),
-        }],
-        agency_code: [{
-          rule: 'isRequiredIf',
-          message: 'Please enter an agency name.',
-          condition: () => this.isAgencyChecked(LAW_ENFORCEMENT),
-        }],
-        communication_method: [{
-          rule: 'isRequiredIf',
-          message: 'Please select cross-report communication method.',
-          condition: () => this.isAgencyChecked(LAW_ENFORCEMENT),
-        }],
-        reported_on: [{
-          rule: 'isRequiredIf',
-          message: 'Please enter a cross-report date.',
-          condition: () => this.isAgencyChecked(LAW_ENFORCEMENT),
-        }],
-      },
-      [DISTRICT_ATTORNEY]: {
-        agency_type: [{
-          rule: 'isRequiredIf',
-          message: 'Please indicate cross-reporting to district attorney.',
-          condition: () => this.isAgencyRequired(DISTRICT_ATTORNEY),
-        }],
-        agency_code: [{
-          rule: 'isRequiredIf',
-          message: 'Please enter an agency name.',
-          condition: () => this.isAgencyChecked(DISTRICT_ATTORNEY),
-        }],
-        communication_method: [{
-          rule: 'isRequiredIf',
-          message: 'Please select cross-report communication method.',
-          condition: () => this.isAgencyChecked(DISTRICT_ATTORNEY),
-        }],
-        reported_on: [{
-          rule: 'isRequiredIf',
-          message: 'Please enter a cross-report date.',
-          condition: () => this.isAgencyChecked(DISTRICT_ATTORNEY),
-        }],
-      },
-      [DEPARTMENT_OF_JUSTICE]: {
-        agency_code: [{
-          rule: 'isRequiredIf',
-          message: 'Please enter an agency name.',
-          condition: () => this.isAgencyChecked(DEPARTMENT_OF_JUSTICE),
-        }],
-        communication_method: [{
-          rule: 'isRequiredIf',
-          message: 'Please select cross-report communication method.',
-          condition: () => this.isAgencyChecked(DEPARTMENT_OF_JUSTICE),
-        }],
-        reported_on: [{
-          rule: 'isRequiredIf',
-          message: 'Please enter a cross-report date.',
-          condition: () => this.isAgencyChecked(DEPARTMENT_OF_JUSTICE),
-        }],
-      },
-      [COUNTY_LICENSING]: {
-        agency_code: [{
-          rule: 'isRequiredIf',
-          message: 'Please enter an agency name.',
-          condition: () => this.isAgencyChecked(COUNTY_LICENSING),
-        }],
-        communication_method: [{
-          rule: 'isRequiredIf',
-          message: 'Please select cross-report communication method.',
-          condition: () => this.isAgencyChecked(COUNTY_LICENSING),
-        }],
-        reported_on: [{
-          rule: 'isRequiredIf',
-          message: 'Please enter a cross-report date.',
-          condition: () => this.isAgencyChecked(COUNTY_LICENSING),
-        }],
-      },
-      [COMMUNITY_CARE_LICENSING]: {
-        agency_code: [{
-          rule: 'isRequiredIf',
-          message: 'Please enter an agency name.',
-          condition: () => this.isAgencyChecked(COMMUNITY_CARE_LICENSING),
-        }],
-        communication_method: [{
-          rule: 'isRequiredIf',
-          message: 'Please select cross-report communication method.',
-          condition: () => this.isAgencyChecked(COMMUNITY_CARE_LICENSING),
-        }],
-        reported_on: [{
-          rule: 'isRequiredIf',
-          message: 'Please enter a cross-report date.',
-          condition: () => this.isAgencyChecked(COMMUNITY_CARE_LICENSING),
-        }],
-      },
-    })
 
-    let errors
-    if (this.props.mode === 'show') {
-      errors = this.validateAllCrossReports()
-    } else {
-      errors = Immutable.Map()
-    }
     this.state = {
       mode: this.props.mode,
-      errors: errors,
-    }
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.mode === 'show') {
-      this.setState({errors: this.validateAllCrossReports()})
     }
   }
 
@@ -148,119 +19,8 @@ export default class CrossReportCardView extends React.Component {
     this.setState({mode: 'show'})
   }
 
-  isAgencyChecked(agencyType) {
-    const result = this.props.crossReports.find((report) => report.get('agency_type') === agencyType)
-    return Boolean(result)
-  }
-
-  isAgencyRequired(agencyType) {
-    return CROSS_REPORTS_REQUIRED_FOR_ALLEGATIONS.includes(agencyType) && this.props.areCrossReportsRequired
-  }
-
   onEdit() {
     this.setState({mode: 'edit'})
-  }
-
-  onEvent(reports, changedItemSeq) {
-    const fieldName = changedItemSeq.shift()
-    const agencyType = changedItemSeq.shift()
-    let updatedReport
-    let updatedValue
-    let errors = Immutable.Map()
-
-    switch (fieldName) {
-      case 'agency_type':
-        updatedReport = reports.find((report) =>
-          report.get('agency_type') === agencyType
-        )
-        updatedValue = (updatedReport !== undefined)
-        errors = this.validateField(agencyType, fieldName, updatedValue)
-        break
-      case 'agency_code':
-        updatedReport = reports.find((report) =>
-          report.get('agency_type') === agencyType
-        ) || Immutable.Map()
-        updatedValue = updatedReport.get('agency_code')
-        errors = this.validateField(agencyType, fieldName, updatedValue)
-        break
-      case 'communication_method':
-      case 'reported_on':
-        Object.keys(AGENCY_TYPES).map((agency) => {
-          updatedReport = reports.find((report) =>
-            report.get('agency_type') === agency
-          ) || Immutable.Map()
-          updatedValue = updatedReport.get(fieldName)
-          const agency_errors = this.validateField(agency, fieldName, updatedValue).get(agency)
-          errors = errors.set(agency, agency_errors)
-        })
-    }
-    this.setState({errors: errors})
-  }
-
-  onCancel() {
-    this.setState({mode: 'show', errors: this.validateAllCrossReports()})
-    this.props.onCancel(Immutable.fromJS(['cross_reports']))
-  }
-
-  onChange(reports, changedItemSeq) {
-    let fieldErrors
-    const [fieldName, agencyType] = changedItemSeq
-    if (agencyType) {
-      fieldErrors = this.state.errors.getIn([agencyType, fieldName]) || Immutable.fromJS([])
-    } else {
-      fieldErrors = this.state.errors.toSet().flatMap((item) => item.get(fieldName))
-    }
-    if (!fieldErrors.isEmpty()) {
-      this.onEvent(reports, changedItemSeq)
-    }
-    this.props.onChange(['cross_reports'], reports)
-  }
-
-  onSave() {
-    return this.props.onSave(Immutable.fromJS(['cross_reports'])).then(() => {
-      this.setState({mode: 'show', errors: this.validateAllCrossReports()})
-    })
-  }
-
-  validateField(agencyType, fieldName, value) {
-    const rules = this.fieldValidations.getIn([agencyType, fieldName])
-    const errors = this.state.errors.setIn([agencyType, fieldName], Validator.validateField({rules, value}))
-    return errors
-  }
-
-  validateAllCrossReports() {
-    const errors = {}
-    this.fieldValidations.map((rules, agency) => {
-      const crossReport = this.props.crossReports.find((report) =>
-        report.get('agency_type') === agency
-      ) || Immutable.Map()
-      errors[agency] = Validator.validateAllFields({
-        screening: crossReport,
-        fieldValidations: rules,
-      })
-    })
-    return Immutable.fromJS(errors)
-  }
-
-  crossReportsInclude(agencyType) {
-    const present = this.props.crossReports.some((crossReport) =>
-      crossReport.get('agency_type') === agencyType
-    )
-    return present
-  }
-
-  alertInfoMessage() {
-    if (this.props.areCrossReportsRequired) {
-      if (Immutable.List(CROSS_REPORTS_REQUIRED_FOR_ALLEGATIONS).every((agencyType) =>
-        this.crossReportsInclude(agencyType)
-      )) {
-        return null
-      } else {
-        return ALLEGATIONS_REQUIRE_CROSS_REPORTS_MESSAGE
-      }
-    } else {
-      return null
-    }
   }
 
   render() {
@@ -279,17 +39,7 @@ export default class CrossReportCardView extends React.Component {
   }
 }
 
-CrossReportCardView.defaultProps = {
-  allegations: Immutable.fromJS([]),
-}
-
 CrossReportCardView.propTypes = {
-  actions: PropTypes.object.isRequired,
-  areCrossReportsRequired: PropTypes.bool.isRequired,
-  crossReports: PropTypes.object,
   editable: PropTypes.bool.isRequired,
   mode: PropTypes.oneOf(['edit', 'show']),
-  onCancel: PropTypes.func.isRequired,
-  onChange: PropTypes.func.isRequired,
-  onSave: PropTypes.func.isRequired,
 }
--- spec/javascripts/components/screenings/crossReports/CrossReportCardViewSpec.jsx
+++ spec/javascripts/components/screenings/crossReports/CrossReportCardViewSpec.jsx
@@ -1,176 +1,53 @@
 import CrossReportCardView from 'screenings/crossReports/CrossReportCardView'
-import Immutable from 'immutable'
 import React from 'react'
 import {shallow} from 'enzyme'
 
 describe('CrossReportCardView', () => {
-  let component
-  let promiseObj
-
-  const fetchCountyAgencies = jasmine.createSpy('fetchCountyAgencies')
-  const props = {
-    areCrossReportsRequired: true,
-    actions: {fetchCountyAgencies},
-    crossReports: Immutable.fromJS([
-      {county: '123', agency_type: 'DISTRICT_ATTORNEY', agency_code: '1234'},
-      {county: '123', agency_type: 'DEPARTMENT_OF_JUSTICE'},
-    ]),
-    editable: true,
+  function renderCrossReportCardView({
+    editable = true,
+    mode = 'edit',
+  }) {
+    const props = {
+      editable,
+      mode,
+    }
+    return shallow(<CrossReportCardView {...props} />)
   }
-  beforeEach(() => {
-    props.onEdit = jasmine.createSpy()
-    promiseObj = jasmine.createSpyObj('promiseObj', ['then'])
-    props.onCancel = jasmine.createSpy()
-    props.onChange = jasmine.createSpy()
-    props.onSave = jasmine.createSpy()
-  })
 
-  it('renders the card header', () => {
-    const component = shallow(<CrossReportCardView {...props} mode='edit' />)
-    const header = component.find('ScreeningCardHeader')
-    expect(header.length).toEqual(1)
-    expect(header.props().onEdit).toEqual(component.instance().onEdit)
-    expect(header.props().showEdit).toEqual(false)
-    expect(header.props().title).toEqual('Cross Report')
-  })
-
-  describe('crossReportsInclude', () => {
-    it('returns true if cross reports includes the agency type passed', () => {
-      const crossReports = Immutable.fromJS([{agency_type: 'District attorney'}])
-      const component = shallow(<CrossReportCardView {...props} mode='show' crossReports={crossReports} />)
-      expect(component.instance().crossReportsInclude('District attorney')).toEqual(true)
+  describe('when mode is set to edit', () => {
+    it('renders the card header', () => {
+      const component = renderCrossReportCardView({})
+      const header = component.find('ScreeningCardHeader')
+      expect(header.exists()).toEqual(true)
+      expect(header.props().onEdit).toEqual(component.instance().onEdit)
+      expect(header.props().showEdit).toEqual(false)
+      expect(header.props().title).toEqual('Cross Report')
     })
-
-    it('returns false if cross reports do not include the agency type passed', () => {
-      const crossReports = Immutable.fromJS([{agency_type: 'District attorney'}])
-      const component = shallow(<CrossReportCardView {...props} mode='show' crossReports={crossReports} />)
-      expect(component.instance().crossReportsInclude('Law enforcement')).toEqual(false)
+    it('renders the edit view', () => {
+      const component = renderCrossReportCardView({})
+      expect(component.find('Connect(CrossReportForm)').exists()).toEqual(true)
+      expect(component.find('Connect(CrossReportForm)').props().toggleShow).toEqual(component.instance().toggleShow)
     })
   })
 
-  describe('alertInfoMessage', () => {
-    it('returns null when cross reports are not required', () => {
-      const component = shallow(
-        <CrossReportCardView
-          {...props}
-          mode='show'
-          areCrossReportsRequired={false}
-        />
-      )
-      expect(component.instance().alertInfoMessage()).toEqual(null)
-    })
-
-    it('returns a message when cross reports are required and none have been selected', () => {
-      const crossReports = Immutable.List()
-      const component = shallow(
-        <CrossReportCardView
-          {...props}
-          mode='show'
-          crossReports={crossReports}
-          areCrossReportsRequired={true}
-        />
-      )
-      expect(component.instance().alertInfoMessage()).toContain('Any report that includes allegations (except General Neglect, Caretaker Absence, or "At risk, sibling abused")')
-    })
-
-    it('returns a message when cross reports are required but district attorney has not been selected', () => {
-      const crossReports = Immutable.fromJS([{agency_type: 'LAW_ENFORCEMENT'}])
-      const component = shallow(
-        <CrossReportCardView
-          {...props}
-          mode='show'
-          crossReports={crossReports}
-          areCrossReportsRequired={true}
-        />
-      )
-      expect(component.instance().alertInfoMessage()).toContain('Any report that includes allegations (except General Neglect, Caretaker Absence, or "At risk, sibling abused")')
-    })
-
-    it('returns a message when cross reports are required but law enforcement has not been selected', () => {
-      const crossReports = Immutable.fromJS([{agency_type: 'DISTRICT_ATTORNEY'}])
-      const component = shallow(
-        <CrossReportCardView
-          {...props}
-          mode='show'
-          crossReports={crossReports}
-          areCrossReportsRequired={true}
-        />
-      )
-      expect(component.instance().alertInfoMessage()).toContain('Any report that includes allegations (except General Neglect, Caretaker Absence, or "At risk, sibling abused")')
-    })
-
-    it('returns a message when cross reports are required, and law enforcement and D.A. have both been selected', () => {
-      const crossReports = Immutable.fromJS([{agency_type: 'DISTRICT_ATTORNEY'}, {agency_type: 'LAW_ENFORCEMENT'}])
-      const component = shallow(
-        <CrossReportCardView
-          {...props}
-          mode='show'
-          crossReports={crossReports}
-          areCrossReportsRequired={true}
-        />
-      )
-      expect(component.instance().alertInfoMessage()).toEqual(null)
-    })
-  })
-
-  describe('render', () => {
-    describe('when mode is set to edit', () => {
-      beforeEach(() => {
-        promiseObj.then.and.callFake((thenFunction) => thenFunction())
-        props.onSave.and.returnValue(promiseObj)
-        component = shallow(<CrossReportCardView {...props} mode='edit'/>)
-      })
-
-      it('renders the edit view', () => {
-        expect(component.find('Connect(CrossReportForm)').exists()).toEqual(true)
-        expect(component.find('Connect(CrossReportForm)').props().toggleShow).toEqual(component.instance().toggleShow)
-        // TODO: expect(component.find('CrossReportEditView').props().isAgencyRequired).toEqual(jasmine.any(Function))
-      })
-
-      describe('isAgencyRequired', () => {
-        it('returns true if the agency is required and cross reporting is required', () => {
-          expect(component.instance().isAgencyRequired('DISTRICT_ATTORNEY')).toEqual(true)
-          expect(component.instance().isAgencyRequired('LAW_ENFORCEMENT')).toEqual(true)
-        })
-
-        it('returns false if an agency is not required even if cross reporting is required', () => {
-          expect(component.instance().isAgencyRequired('COUNTY_LICENSING')).toEqual(false)
-        })
-
-        it('returns false if cross reporting is not required', () => {
-          component = shallow(<CrossReportCardView {...props} mode='edit' areCrossReportsRequired={false}/>)
-          expect(component.instance().isAgencyRequired('DISTRICT_ATTORNEY')).toEqual(false)
-        })
-      })
-
-      describe('isAgencyChecked', () => {
-        beforeEach(() => {
-          component = shallow(
-            <CrossReportCardView {...props}
-              crossReports={Immutable.fromJS([
-                {agency_type: 'Law enforcement'},
-              ])}
-              mode='edit'
-            />
-          )
-        })
-        it('returns true if agency is checked', () => {
-          expect(component.instance().isAgencyChecked('Law enforcement')).toEqual(true)
-        })
-        it('returns false if agency is NOT checked', () => {
-          expect(component.instance().isAgencyChecked('District attorney')).toEqual(false)
-        })
+  describe('when mode is set to show', () => {
+    describe('when editable is false', () => {
+      it('disables the showEdit', () => {
+        const component = renderCrossReportCardView({editable: false, mode: 'show'})
+        const header = component.find('ScreeningCardHeader')
+        expect(header.props().showEdit).toEqual(false)
       })
     })
-
-    describe('when mode is set to show', () => {
-      beforeEach(() => {
-        component = shallow(<CrossReportCardView {...props} mode='show'/>)
+    describe('when editable is true', () => {
+      it('enables the showEdit', () => {
+        const component = renderCrossReportCardView({editable: true, mode: 'show'})
+        const header = component.find('ScreeningCardHeader')
+        expect(header.props().showEdit).toEqual(true)
       })
-
-      it('renders the cross report show card', () => {
-        expect(component.find('Connect(CrossReportShow)').exists()).toEqual(true)
-      })
+    })
+    it('renders the cross report show card', () => {
+      const component = renderCrossReportCardView({mode: 'show'})
+      expect(component.find('Connect(CrossReportShow)').exists()).toEqual(true)
     })
   })
 })
