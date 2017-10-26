94b777b94aa8377703f1243f58b55e7d35f74afe
Aman Alshurafa <Aman.Alshurafa@OSI.ca.gov>
2017-10-26 16:11:34 -0700

    Feature spec for the new error banner behavior
    [#151954889]

--- spec/features/errors_spec.rb
+++ spec/features/errors_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 require 'feature/testing'
 
-feature 'error pages', skip: true do
+feature 'error pages' do
   around do |example|
     Rails.application.config.consider_all_requests_local = false
     Rails.application.config.action_dispatch.show_exceptions = true
@@ -11,13 +11,14 @@ feature 'error pages', skip: true do
     Rails.application.config.consider_all_requests_local = true
     Rails.application.config.action_dispatch.show_exceptions = false
   end
+
   context 'page does not exist' do
     let(:dashboard_url) { 'http://fake_dashboard.ca.gov' }
 
     scenario 'renders 404 page' do
-      pending(
-        'implemented but untestable in parallel. (selenium thread w/out config changes)'
-      )
+      if ENV.key?('TEST_ENV_NUMBER')
+        skip 'Pending as this test fails during parallel runs'
+      end
       allow(Rails.configuration).to receive(:intake).and_return(dashboard_url: dashboard_url)
       visit '/this_page_does_not_exist'
       expect(page).to have_text('Sorry, this is not the page you want.')
@@ -30,9 +31,7 @@ feature 'error pages', skip: true do
 
   context 'server has error' do
     scenario 'renders 500 page' do
-      pending(
-        'implemented but fails because react recovers'
-      )
+      pending 'implemented but fails because react recovers'
       stub_request(:get, '/screenings/2').and_return(json_body('I failed', status: 500))
       visit '/screenings/2'
 
@@ -40,6 +39,43 @@ feature 'error pages', skip: true do
       expect(page).to have_text(
         "It's nothing you did. Due to an entirely internal error, "\
         'your request could not be completred. Please try refreshing the page.'
+      )
+    end
+  end
+end
+
+feature 'error banner' do
+  let(:screening) { FactoryGirl.create(:screening) }
+  context 'error occurred after page was loaded' do
+    around do |example|
+      stub_request(
+        :get, intake_api_url(ExternalRoutes.intake_api_screening_path(screening.id))
+      ).and_return(json_body(screening.to_json, status: 200))
+      Feature.run_with_activated(:referral_submit) do
+        example.run
+      end
+    end
+
+    scenario 'hide the error banner after a successful action' do
+      stub_request(
+        :post,
+        intake_api_url(ExternalRoutes.intake_api_screening_submit_path(screening.id))
+      ).and_return(json_body([].to_json, status: 500))
+      visit edit_screening_path(id: screening.id)
+      expect(page).to_not have_text(
+        'Something went wrong, sorry! Please try your last action again.'
+      )
+      click_button 'Submit'
+      expect(page).to have_text(
+        'Something went wrong, sorry! Please try your last action again.'
+      )
+      stub_request(
+        :post,
+        intake_api_url(ExternalRoutes.intake_api_screening_submit_path(screening.id))
+      ).and_return(json_body([].to_json, status: 200))
+      click_button 'Submit'
+      expect(page).to_not have_text(
+        'Something went wrong, sorry! Please try your last action again.'
       )
     end
   end
