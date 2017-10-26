14f1bcd1ed6af5673dd86fcc460cdb71cb44079b
Aman Alshurafa and Thomas Ramirez <pair+Aman.Alshurafa+Thomas.Ramirez@OSI.ca.gov>
2017-10-26 14:14:18 -0700

    Route to error page
    [#151965260]

--- spec/controllers/errors_controller_spec.rb
+++ spec/controllers/errors_controller_spec.rb
@@ -9,4 +9,11 @@ RSpec.describe ErrorsController, type: :controller do
       expect(response).to have_http_status(404)
     end
   end
+
+  describe 'GET #server_error' do
+    it 'returns http 500' do
+      get :server_error
+      expect(response).to have_http_status(500)
+    end
+  end
 end
