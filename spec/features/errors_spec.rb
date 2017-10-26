5c40132264afdd4359f3b17beb9cb715fe66a516
Aman Alshurafa and Thomas Ramirez <pair+Aman.Alshurafa+Thomas.Ramirez@OSI.ca.gov>
2017-10-26 11:46:23 -0700

    Add feature test
    [#151965260]

--- spec/features/errors_spec.rb
+++ spec/features/errors_spec.rb
@@ -4,15 +4,15 @@ require 'rails_helper'
 require 'feature/testing'
 
 feature 'error pages' do
+  around do |example|
+    Rails.application.config.consider_all_requests_local = false
+    Rails.application.config.action_dispatch.show_exceptions = true
+    example.run
+    Rails.application.config.consider_all_requests_local = true
+    Rails.application.config.action_dispatch.show_exceptions = false
+  end
   context 'page does not exist' do
     let(:dashboard_url) { 'http://fake_dashboard.ca.gov' }
-    around do |example|
-      Rails.application.config.consider_all_requests_local = false
-      Rails.application.config.action_dispatch.show_exceptions = true
-      example.run
-      Rails.application.config.consider_all_requests_local = true
-      Rails.application.config.action_dispatch.show_exceptions = false
-    end
 
     scenario 'renders 404 page' do
       pending(
@@ -25,6 +25,22 @@ feature 'error pages' do
         "It may have been deleted or doesn't exist. Please check the address or"
       )
       expect(page).to have_link('return to your dashboard', href: dashboard_url)
+    end
+  end
+
+  context 'server has error' do
+    scenario 'renders 500 page' do
+      pending(
+        'implemented but fails because react recovers'
+      )
+      stub_request(:get, '/screenings/2').and_return(json_body('I failed', status: 500))
+      visit '/screenings/2'
+
+      expect(page).to have_text('Sorry, something went wrong.')
+      expect(page).to have_text(
+        "It's nothing you did. Due to an entirely internal error, "\
+        'your request could not be completred. Please try refreshing the page.'
+      )
     end
   end
 end
