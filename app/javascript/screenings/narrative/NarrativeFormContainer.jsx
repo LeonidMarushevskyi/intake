4b6ebba1758441215775bf87914fd7982d54ba2d
Sara Bocciardi <Sara.Bocciardi@OSI.ca.gov>
2017-10-25 20:00:22 -0700

    Use mergeProps to define callbacks
    - mapDispatchToProps doesn't have access to the store
    - mergeProps can combine state and dispatch for callbacks
    - Limit what we have to pass to the view

--- app/javascript/screenings/narrative/NarrativeFormContainer.jsx
+++ app/javascript/screenings/narrative/NarrativeFormContainer.jsx
@@ -10,10 +10,9 @@ import {saveScreening} from 'actions/screeningActions'
 import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
 
-const mapStateToProps = (state, ownProps) => (
+const mapStateToProps = (state, _ownProps) => (
   {
-    onCancel: ownProps.onCancel,
-    errors: getVisibleErrorsSelector(state).get('report_narrative').toJS(),
+    errors: getVisibleErrorsSelector(state).toJS(),
     reportNarrative: getReportNarrativeValueSelector(state),
     screening: getScreeningSelector(state).toJS(),
     screeningWithEdits: getScreeningWithEditsSelector(state).toJS(),
@@ -22,7 +21,33 @@ const mapStateToProps = (state, ownProps) => (
 
 const mapDispatchToProps = (dispatch, _ownProps) => {
   const actions = {...narrativeFormActions, saveScreening}
-  return {actions: bindActionCreators(actions, dispatch)}
+  return {...bindActionCreators(actions, dispatch)}
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(NarrativeForm)
+const mergeProps = (stateProps, dispatchProps, ownProps) => {
+  const {setField, resetFieldValues, touchField, touchAllFields, saveScreening} = dispatchProps
+  const {errors, reportNarrative, screening, screeningWithEdits} = stateProps
+  const {toggleShow} = ownProps
+
+  const onCancel = () => {
+    resetFieldValues(screening)
+    touchAllFields()
+    toggleShow()
+  }
+
+  const onSave = () => {
+    saveScreening(screeningWithEdits)
+    touchAllFields()
+    toggleShow()
+  }
+
+  return {
+    onBlur: (fieldName) => dispatch(touchField(fieldName)),
+    onCancel,
+    onChange: (fieldName, value) => dispatch(setField(fieldName, value)),
+    onSave,
+    reportNarrative,
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps, mergeProps)(NarrativeForm)
