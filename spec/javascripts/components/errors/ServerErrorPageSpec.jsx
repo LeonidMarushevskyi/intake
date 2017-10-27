bdfc4d662b16aefbc4439271885a258c50cb0c63
Thomas Ramirez <Thomas.Ramirez@OSI.ca.gov>
2017-10-27 14:17:55 -0700

    fix server error page spec
    [#151965260]

--- spec/javascripts/components/errors/ServerErrorPageSpec.jsx
+++ spec/javascripts/components/errors/ServerErrorPageSpec.jsx
@@ -9,7 +9,7 @@ describe('ServerErrorPage', () => {
     component = shallow(<ServerErrorPage/>)
   })
 
-  fit('renders the error text', () => {
+  it('renders the error text', () => {
     expect(component.text()).toContain('Sorry, something went wrong.')
     expect(component.text()).toContain(
       "It's nothing you did. Due to an entirely internal error, your request could not be completred. Please try refreshing the page."
