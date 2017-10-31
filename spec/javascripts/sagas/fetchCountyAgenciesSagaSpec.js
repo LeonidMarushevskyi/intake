66b0a85d5296893306d3af534837d46d08c6ad53
Nate Jones <Nate.Jones@OSI.ca.gov>
2017-10-31 15:20:28 -0700

    County agencies actions include payload
    [#151774703]

--- spec/javascripts/sagas/fetchCountyAgenciesSagaSpec.js
+++ spec/javascripts/sagas/fetchCountyAgenciesSagaSpec.js
@@ -2,7 +2,7 @@ import 'babel-polyfill'
 import {takeEvery, put, call} from 'redux-saga/effects'
 import {get} from 'utils/http'
 import {fetchCountyAgenciesSaga, fetchCountyAgencies} from 'sagas/fetchCountyAgenciesSaga'
-import {FETCH_COUNTY_AGENCIES, fetchSuccess, fetchFailure} from 'actions/countyAgenciesActions'
+import {FETCH_COUNTY_AGENCIES, fetch, fetchSuccess, fetchFailure} from 'actions/countyAgenciesActions'
 
 describe('fetchCountyAgenciesSaga', () => {
   it('fetches county agencies on FETCH_COUNTY_AGENCIES', () => {
@@ -12,20 +12,21 @@ describe('fetchCountyAgenciesSaga', () => {
 })
 
 describe('fetchCountyAgencies', () => {
+  const countyId = '123'
+  const action = fetch(countyId)
+
   it('fetches and puts countyAgencies', () => {
-    const countyId = '123'
-    const json = [{stuff: 123}]
-    const gen = fetchCountyAgencies({countyId})
+    const gen = fetchCountyAgencies(action)
     expect(gen.next().value).toEqual(call(get, '/api/v1/cross_report_agency/123'))
+    const json = [{stuff: 123}]
     expect(gen.next(json).value).toEqual(
       put(fetchSuccess(json))
     )
   })
 
   it('puts errors when errors are thrown', () => {
-    const countyId = '123'
     const error = 'some error'
-    const gen = fetchCountyAgencies({countyId})
+    const gen = fetchCountyAgencies(action)
     expect(gen.next().value).toEqual(call(get, '/api/v1/cross_report_agency/123'))
     expect(gen.throw(error).value).toEqual(
       put(fetchFailure('some error'))
