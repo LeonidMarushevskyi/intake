10d218c1331b450d73a5510c7567ea23e10f1b51
Tyler Clemens <Tyler.Clemens@OSI.ca.gov>
2017-10-31 14:00:24 -0700

    fix selector import RelationshipsSelectors -> relationshipsSelectors

--- spec/javascripts/selectors/relationshipsSelectorsSpec.js
+++ spec/javascripts/selectors/relationshipsSelectorsSpec.js
@@ -1,7 +1,7 @@
 import {fromJS} from 'immutable'
 import {
   getPeopleSelector,
-} from 'selectors/RelationshipsSelectors'
+} from 'selectors/relationshipsSelectors'
 import * as matchers from 'jasmine-immutable-matchers'
 
 describe('relationshipsViewSelectors', () => {
