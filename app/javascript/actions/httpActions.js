274ee647081e2e70da6cbc988fda62ffd91370c0
Nate Jones <Nate.Jones@OSI.ca.gov>
2017-11-01 16:03:43 -0700

    Http actions now include error
    [#151774703]

--- app/javascript/actions/httpActions.js
+++ app/javascript/actions/httpActions.js
@@ -1,9 +1,7 @@
 export const HTTP_COMPLETE = 'HTTP_COMPLETE'
-
 export function httpError(url, response) {
   return {type: HTTP_COMPLETE, payload: {url, response}, error: true}
 }
-
 export function httpSuccess(url, response) {
   return {type: HTTP_COMPLETE, payload: {url, response}}
 }
