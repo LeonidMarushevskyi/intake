a5575bacb3ffedfa6aac62c31e7a8bbbb12d9798
Sara Bocciardi <Sara.Bocciardi@OSI.ca.gov>
2017-11-02 14:13:09 -0700

    Move allegation show view per new guidelines
    - Try and contain our views in a views directory
    
    [#152464351]

--- app/javascript/screenings/AllegationsShowContainer.jsx
+++ app/javascript/screenings/AllegationsShowContainer.jsx
@@ -1,5 +1,5 @@
 import {connect} from 'react-redux'
-import AllegationShow from 'allegations/AllegationShow'
+import AllegationShow from 'views/allegations/AllegationShow'
 import {
   getFormattedAllegationsSelector,
   getAllegationsRequiredValueSelector,
