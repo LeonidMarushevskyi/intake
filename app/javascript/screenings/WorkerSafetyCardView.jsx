7a505bcee772ddab0c0812cc70ce77e36aa2884c
Thomas Ramirez <Thomas.Ramirez@OSI.ca.gov>
2017-11-03 14:09:08 -0700

    Replace Show View with show
    - breaks edit view (to be replaced with form)
    
    [#152464345]

--- app/javascript/screenings/WorkerSafetyCardView.jsx
+++ app/javascript/screenings/WorkerSafetyCardView.jsx
@@ -1,9 +1,7 @@
-import Immutable from 'immutable'
 import PropTypes from 'prop-types'
 import React from 'react'
 import ScreeningCardHeader from 'screenings/ScreeningCardHeader'
-import WorkerSafetyShowView from 'screenings/WorkerSafetyShowView'
-import WorkerSafetyEditView from 'screenings/WorkerSafetyEditView'
+import WorkerSafetyShowContainer from 'screenings/workerSafety/WorkerSafetyShowContainer'
 
 export default class WorkerSafetyCardView extends React.Component {
   constructor(props) {
@@ -12,43 +10,21 @@ export default class WorkerSafetyCardView extends React.Component {
       mode: this.props.mode,
     }
     this.onEdit = this.onEdit.bind(this)
-    this.onCancel = this.onCancel.bind(this)
-    this.onSave = this.onSave.bind(this)
-    this.fields = Immutable.fromJS(['safety_alerts', 'safety_information'])
+    this.toggleMode = this.toggleMode.bind(this)
+  }
+
+  toggleMode() {
+    const currentMode = this.state.mode
+    const newMode = currentMode === 'show' ? 'edit' : 'show'
+    this.setState({mode: newMode})
   }
 
   onEdit() {
     this.setState({mode: 'edit'})
   }
-  onCancel() {
-    this.setState({mode: 'show'})
-    this.props.onCancel(this.fields)
-  }
-
-  onSave() {
-    return this.props.onSave(this.fields).then(() => {
-      this.setState({mode: 'show'})
-    })
-  }
 
   render() {
     const {mode} = this.state
-    const allprops = {
-      show: {
-        onEdit: this.onEdit,
-      },
-      edit: {
-        onCancel: this.onCancel,
-        onChange: this.props.onChange,
-        onSave: this.onSave,
-      },
-    }
-    const WorkerSafetyView = (mode === 'edit') ? WorkerSafetyEditView : WorkerSafetyShowView
-    const props = {
-      ...allprops[mode],
-      safetyAlerts: this.props.screening.get('safety_alerts'),
-      safetyInformation: this.props.screening.get('safety_information'),
-    }
     return (
       <div className={`card ${mode} double-gap-top`} id='worker-safety-card'>
         <ScreeningCardHeader
@@ -56,7 +32,7 @@ export default class WorkerSafetyCardView extends React.Component {
           title='Worker Safety'
           showEdit={this.props.editable && mode === 'show'}
         />
-        <WorkerSafetyView {...props} />
+        {mode === 'show' && <WorkerSafetyShowContainer />}
       </div>
     )
   }
@@ -65,8 +41,4 @@ export default class WorkerSafetyCardView extends React.Component {
 WorkerSafetyCardView.propTypes = {
   editable: PropTypes.bool.isRequired,
   mode: PropTypes.oneOf(['show', 'edit']),
-  onCancel: PropTypes.func.isRequired,
-  onChange: PropTypes.func.isRequired,
-  onSave: PropTypes.func.isRequired,
-  screening: PropTypes.object.isRequired,
 }
