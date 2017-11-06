955eddee7606bbabe02b2b9c225176ee42564085
Nate Jones <Nate.Jones@OSI.ca.gov>
2017-11-06 13:44:18 -0800

    Add save screening action to screening info form container
    [#152464353]

--- app/javascript/screenings/screeningInformation/ScreeningInformationFormContainer.js
+++ app/javascript/screenings/screeningInformation/ScreeningInformationFormContainer.js
@@ -1,10 +1,13 @@
 import {connect} from 'react-redux'
 import ScreeningInformationForm from 'screenings/screeningInformation/ScreeningInformationForm'
 import COMMUNICATION_METHOD from 'enums/CommunicationMethod'
+import {setField, resetFieldValues, touchField, touchAllFields} from 'actions/screeningInformationFormActions'
 import {
   getScreeningInformationFormSelector,
+  getScreeningWithEditsSelector,
   getVisibleErrorsSelector,
 } from 'selectors/screeningInformationFormSelectors'
+import {saveScreening} from 'actions/screeningActions'
 import {getScreeningSelector} from 'selectors/screeningSelectors'
 
 const mapStateToProps = (state, _ownProps) => {
@@ -13,6 +16,8 @@ const mapStateToProps = (state, _ownProps) => {
   const communicationMethods = Object.keys(COMMUNICATION_METHOD)
     .map((value) => ({value, label: COMMUNICATION_METHOD[value]}))
   return {
+    screening: screening.toJS(),
+    screeningWithEdits: getScreeningWithEditsSelector(state).toJS(),
     assignee: screeningInformationForm.getIn(['assignee', 'value']),
     assigneeDisabled: Boolean(screening.get('assignee_staff_id')),
     communicationMethod: screeningInformationForm.getIn(['communication_method', 'value']),
@@ -23,4 +28,44 @@ const mapStateToProps = (state, _ownProps) => {
     startedAt: screeningInformationForm.getIn(['started_at', 'value']),
   }
 }
-export default connect(mapStateToProps, {})(ScreeningInformationForm)
+const mergeProps = (stateProps, {dispatch}, {toggleShow}) => {
+  const {
+    screening,
+    screeningWithEdits,
+    assignee,
+    assigneeDisabled,
+    communicationMethod,
+    communicationMethods,
+    endedAt,
+    errors,
+    name,
+    startedAt,
+  } = stateProps
+  const onCancel = () => {
+    dispatch(resetFieldValues(screening))
+    dispatch(touchAllFields())
+    toggleShow()
+  }
+  const onSave = () => {
+    dispatch(saveScreening(screeningWithEdits))
+    dispatch(touchAllFields())
+    toggleShow()
+  }
+  const onBlur = (fieldName) => dispatch(touchField(fieldName))
+  const onChange = (fieldName, value) => dispatch(setField(fieldName, value))
+  return {
+    onBlur,
+    onCancel,
+    onChange,
+    onSave,
+    assignee,
+    assigneeDisabled,
+    communicationMethod,
+    communicationMethods,
+    endedAt,
+    errors,
+    name,
+    startedAt,
+  }
+}
+export default connect(mapStateToProps, null, mergeProps)(ScreeningInformationForm)
