33e7e1e5cbfc14f85e0132cf4e7bbf97eb1f85d5
Tyler Clemens and Vinh Pham <pair+Tyler.Clemens+Vinh.Pham@OSI.ca.gov>
2017-11-07 15:25:00 -0800

    Add the incident information form container
    [#152464352]

---
+++ app/javascript/containers/screenings/IncidentInformationFormContainer.jsx
@@ -0,0 +1,66 @@
+import {connect} from 'react-redux'
+import {
+  getIncidentDateSelector,
+  getIncidentCountySelector,
+  getAddressSelector,
+  getLocationTypeSelector,
+  getScreeningWithEditsSelector,
+  getCounties,
+  getStates,
+  getLocationTypes,
+  getVisibleErrorsSelector,
+} from 'selectors/screening/incidentInformationFormSelector'
+import {getScreeningSelector} from 'selectors/screeningSelectors'
+import IncidentInformationForm from 'views/IncidentInformationForm'
+import {
+  setField,
+  touchAllFields,
+  touchField,
+  resetFieldValues,
+} from 'actions/incidentInformationFormActions'
+import {saveScreening} from 'actions/screeningActions'
+
+const mapStateToProps = (state, _ownProps) => ({
+  errors: getVisibleErrorsSelector(state).toJS(),
+  incidentDate: getIncidentDateSelector(state),
+  screening: getScreeningSelector(state).toJS(),
+  screeningWithEdits: getScreeningWithEditsSelector(state).toJS(),
+  selectedCounty: getIncidentCountySelector(state),
+  address: getAddressSelector(state).toJS(),
+  selectedLocationType: getLocationTypeSelector(state),
+  counties: getCounties(),
+  usStates: getStates(),
+  locationTypes: getLocationTypes(),
+})
+
+const mergeProps = (stateProps, dispatchProps, ownProps) => {
+  const {dispatch} = dispatchProps
+  const {
+    screening,
+    screeningWithEdits,
+    ...usedProps
+  } = stateProps
+  const {toggleShow} = ownProps
+
+  const onSave = () => {
+    dispatch(saveScreening(screeningWithEdits))
+    dispatch(touchAllFields())
+    toggleShow()
+  }
+  const onCancel = () => {
+    dispatch(resetFieldValues(screening))
+    dispatch(touchAllFields())
+    toggleShow()
+  }
+  const onChange = (fieldName, value) => dispatch(setField(fieldName, value))
+  const onBlur = (fieldName) => dispatch(touchField(fieldName))
+  return {
+    ...usedProps,
+    onSave,
+    onCancel,
+    onChange,
+    onBlur,
+  }
+}
+
+export default connect(mapStateToProps, null, mergeProps)(IncidentInformationForm)
