e10d6396df92327474d3b8c4ccaf3b1184b1b383
Aman Alshurafa <Aman.Alshurafa@OSI.ca.gov>
2017-11-07 23:03:52 -0800

    People show selectors to format & select people data
    [#152463649]

---
+++ app/javascript/selectors/screening/peopleShowSelectors.js
@@ -0,0 +1,31 @@
+import GENDERS from 'enums/Genders'
+import legacySourceFormatter from 'utils/legacySourceFormatter'
+import nameFormatter from 'utils/nameFormatter'
+import ssnFormatter from 'utils/ssnFormatter'
+import {fromJS} from 'immutable'
+import {dateFormatter} from 'utils/dateFormatter'
+import {flagPrimaryLanguage} from 'common/LanguageInfo'
+
+export const getFormattedPersonInformationSelector = (state, personId) => {
+  const person = state.get('participants').find((person) => person.get('id') === personId)
+  const legacyDescriptor = person.get('legacy_descriptor')
+  const races = person.get('races') && person.get('races').map((raceInformation) => {
+    const race = raceInformation.get('race')
+    const raceDetail = raceInformation.get('race_detail')
+    const raceDetailText = (raceDetail && ` - ${raceDetail}`) || ''
+    return `${race}${raceDetailText}`
+  }).join(', ')
+  const {hispanic_latino_origin, ethnicity_detail} = person.toJS().ethnicity || {}
+  return fromJS({
+    legacy_source: legacyDescriptor ? legacySourceFormatter(legacyDescriptor.toJS()) : '',
+    display_name: nameFormatter(person.toJS()),
+    gender: GENDERS[person.get('gender')],
+    roles: person.get('roles'),
+    languages: flagPrimaryLanguage((person.toJS().languages) || []).join(', '),
+    date_of_birth: dateFormatter(person.get('date_of_birth')),
+    approximate_age: [person.get('approximate_age'), person.get('approximate_age_units')].join(' '),
+    ssn: ssnFormatter(person.get('ssn')),
+    races: races,
+    ethnicity: hispanic_latino_origin && `${hispanic_latino_origin}${(ethnicity_detail && ` - ${ethnicity_detail}`) || ''}`,
+  })
+}
