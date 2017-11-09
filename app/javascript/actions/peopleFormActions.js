2649d5aa787ffc5ed36fe95caef6856b3e2c98c6
Sara Bocciardi <Sara.Bocciardi@OSI.ca.gov>
2017-11-08 22:23:33 -0800

    Add actions for adding/removing a person phone number
    [#152463644]

--- app/javascript/actions/peopleFormActions.js
+++ app/javascript/actions/peopleFormActions.js
@@ -1,5 +1,9 @@
 export const SET_PEOPLE_FORM_FIELD = 'SET_PEOPLE_FORM_FIELD'
+export const ADD_PEOPLE_FORM_PHONE_NUMBER = 'ADD_PEOPLE_FORM_PHONE_NUMBER'
+export const DELETE_PEOPLE_FORM_PHONE_NUMBER = 'DELETE_PEOPLE_FORM_PHONE_NUMBER'
 export const setField = (personId, fieldSet, value) => ({
   type: SET_PEOPLE_FORM_FIELD,
   payload: {personId, fieldSet, value},
 })
+export const addPhone = (personId) => ({type: ADD_PEOPLE_FORM_PHONE_NUMBER, payload: {personId}})
+export const deletePhone = (personId, phoneIndex) => ({type: DELETE_PEOPLE_FORM_PHONE_NUMBER, payload: {personId, phoneIndex}})
--- spec/javascripts/reducers/peopleFormReducerSpec.js
+++ spec/javascripts/reducers/peopleFormReducerSpec.js
@@ -1,6 +1,6 @@
 import * as matchers from 'jasmine-immutable-matchers'
 import {fetchScreeningSuccess, fetchScreeningFailure} from 'actions/screeningActions'
-import {setField} from 'actions/peopleFormActions'
+import {setField, addPhone, deletePhone} from 'actions/peopleFormActions'
 import peopleFormReducer from 'reducers/peopleFormReducer'
 import {Map, fromJS} from 'immutable'
 
@@ -18,6 +18,7 @@ describe('peopleFormReducer', () => {
           middle_name: 'middle name one',
           last_name: 'last name one',
           name_suffix: 'name suffix one',
+          phone_numbers: [],
           ssn: 'ssn one',
         }, {
           id: 'participant_two',
@@ -27,6 +28,7 @@ describe('peopleFormReducer', () => {
           middle_name: 'middle name two',
           last_name: 'last name two',
           name_suffix: 'name suffix two',
+          phone_numbers: [{number: '1234567890', type: 'Home'}],
           ssn: 'ssn two',
         }],
       })
@@ -39,6 +41,7 @@ describe('peopleFormReducer', () => {
             middle_name: {value: 'middle name one'},
             last_name: {value: 'last name one'},
             name_suffix: {value: 'name suffix one'},
+            phone_numbers: [],
             ssn: {value: 'ssn one'},
           },
           participant_two: {
@@ -48,6 +51,10 @@ describe('peopleFormReducer', () => {
             middle_name: {value: 'middle name two'},
             last_name: {value: 'last name two'},
             name_suffix: {value: 'name suffix two'},
+            phone_numbers: [{
+              number: {value: '1234567890'},
+              type: {value: 'Home'},
+            }],
             ssn: {value: 'ssn two'},
           },
         })
@@ -100,6 +107,38 @@ describe('peopleFormReducer', () => {
           participant_two: {
             roles: {value: ['c']},
           },
+        })
+      )
+    })
+  })
+
+  describe('on addPhone', () => {
+    it('adds a new empty phone item for the given person id', () => {
+      const lastState = fromJS({
+        person_one: {phone_numbers: []},
+        person_two: {phone_numbers: []},
+      })
+      const action = addPhone('person_one')
+      expect(peopleFormReducer(lastState, action)).toEqualImmutable(
+        fromJS({
+          person_one: {phone_numbers: [{number: {value: null}, type: {value: null}}]},
+          person_two: {phone_numbers: []},
+        })
+      )
+    })
+  })
+
+  describe('on deletePhone', () => {
+    it('deletes the phone item for the given person id and index', () => {
+      const lastState = fromJS({
+        person_one: {phone_numbers: [{number: {value: '1234567890'}, type: {value: 'Home'}}]},
+        person_two: {phone_numbers: [{number: {value: '0987654321'}, type: {value: 'Cell'}}]},
+      })
+      const action = deletePhone('person_one', 0)
+      expect(peopleFormReducer(lastState, action)).toEqualImmutable(
+        fromJS({
+          person_one: {phone_numbers: []},
+          person_two: {phone_numbers: [{number: {value: '0987654321'}, type: {value: 'Cell'}}]},
         })
       )
     })
