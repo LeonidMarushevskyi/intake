ad95c021417aa24774934ff68d60f6f848d15ca7
John Gedeon and Nate Jones <pair+John.Gedeon+Nate.Jones@OSI.ca.gov>
2017-11-13 14:26:03 -0800

    Add RACE_DETAILS to races enumeration
    [#152463643]

--- app/javascript/enums/Races.js
+++ app/javascript/enums/Races.js
@@ -1,4 +1,45 @@
 /*eslint quote-props: ["error", "consistent-as-needed"]*/
+export const RACE_DETAILS = {
+  'White': [
+    'Armenian',
+    'Central American',
+    'European',
+    'Middle Eastern',
+    'Romanian',
+  ],
+  'Black or African American': [
+    'Black',
+    'Ethiopian',
+    'Caribbean',
+  ],
+  'Asian': [
+    'Asian Indian',
+    'Cambodian',
+    'Chinese',
+    'Filipino',
+    'Hmong',
+    'Japanese',
+    'Korean',
+    'Laotian',
+    'Other Asian',
+    'Vietnamese',
+  ],
+  'American Indian or Alaska Native': [
+    'American Indian',
+    'Alaska Native',
+  ],
+  'Native Hawaiian or Other Pacific Islander': [
+    'Guamanian',
+    'Hawaiian',
+    'Other Asian/Pacific Islander',
+    'Other Pacific Islander',
+    'Polynesian',
+    'Samoan',
+  ],
+  'Unknown': [],
+  'Abandoned': [],
+  'Declined to answer': [],
+}
 const RACES = Object.freeze({
   'White': {
     exclusive: false,
