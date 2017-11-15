9a33f9c4e5fb785b38e6d0a72f5bb8e8f12128fd
Sara Bocciardi <Sara.Bocciardi@OSI.ca.gov>
2017-11-14 22:00:23 -0800

    Update allegations on update/delete person
    - Don't allow bad data to persist in the form if we have remove a person
    from a screening
    - Don't allow ghost data to persist in the form if we remove a victim
    and/or perpetrator role from a person
    
    [#152463643]

--- spec/javascripts/reducers/allegationsFormReducerSpec.js
+++ spec/javascripts/reducers/allegationsFormReducerSpec.js
@@ -3,6 +3,12 @@ import {
   resetAllegations,
   setAllegationTypes,
 } from 'actions/allegationsFormActions'
+import {
+  deletePersonSuccess,
+  deletePersonFailure,
+  updatePersonSuccess,
+  updatePersonFailure,
+} from 'actions/personCardActions'
 import {fetchScreeningSuccess, fetchScreeningFailure} from 'actions/screeningActions'
 import * as matchers from 'jasmine-immutable-matchers'
 import allegationsFormReducer from 'reducers/allegationsFormReducer'
@@ -11,7 +17,7 @@ describe('narrativeFormReducer', () => {
   beforeEach(() => jasmine.addMatchers(matchers))
 
   describe('on FETCH_SCREENING_COMPLETE', () => {
-    it('returns the narrative form', () => {
+    it('returns the allegations form', () => {
       const allegations = [{id: '123', victim_id: '1', perpetrator_id: '2', allegation_types: ['General neglect']}]
       const action = fetchScreeningSuccess({allegations})
       expect(allegationsFormReducer(List(), action)).toEqualImmutable(
@@ -58,8 +64,65 @@ describe('narrativeFormReducer', () => {
     it('resets the store to the allegations passed to reset', () => {
       const action = resetAllegations({allegations: []})
       const allegations = [{victim_id: '1', perpetrator_id: '2', allegation_types: ['General neglect']}]
-      const state = fromJS({allegationsForm: allegations})
+      const state = fromJS(allegations)
       expect(allegationsFormReducer(state, action)).toEqualImmutable(List())
+    })
+  })
+
+  describe('on DELETE_PERSON_COMPLETE', () => {
+    it('removes any allegations for the person removed', () => {
+      const action = deletePersonSuccess('3')
+      const allegations = [
+        {victimId: '3', perpetratorId: '2'},
+        {victimId: '1', perpetratorId: '3'},
+        {victimId: '1', perpetratorId: '2'},
+      ]
+      const state = fromJS(allegations)
+      expect(allegationsFormReducer(state, action)).toEqualImmutable(fromJS([
+        {victimId: '1', perpetratorId: '2'},
+      ]))
+    })
+
+    it('returns the last state on failure', () => {
+      const action = deletePersonFailure()
+      expect(allegationsFormReducer(List(), action))
+        .toEqualImmutable(List())
+    })
+  })
+
+  describe('on UPDATE_PERSON_COMPLETE', () => {
+    it('removes any allegations for the person updated if no longer a victim or perp', () => {
+      const action = updatePersonSuccess({id: '3', roles: []})
+      const allegations = [
+        {victimId: '3', perpetratorId: '2'},
+        {victimId: '1', perpetratorId: '3'},
+        {victimId: '1', perpetratorId: '2'},
+      ]
+      const state = fromJS(allegations)
+      expect(allegationsFormReducer(state, action)).toEqualImmutable(fromJS([
+        {victimId: '1', perpetratorId: '2'},
+      ]))
+    })
+
+    it('does not remove allegations for the person updated if roles are unchanged', () => {
+      const action = updatePersonSuccess({id: '3', roles: ['Victim', 'Perpetrator']})
+      const allegations = [
+        {victimId: '3', perpetratorId: '2'},
+        {victimId: '1', perpetratorId: '3'},
+        {victimId: '1', perpetratorId: '2'},
+      ]
+      const state = fromJS(allegations)
+      expect(allegationsFormReducer(state, action)).toEqualImmutable(fromJS([
+        {victimId: '3', perpetratorId: '2'},
+        {victimId: '1', perpetratorId: '3'},
+        {victimId: '1', perpetratorId: '2'},
+      ]))
+    })
+
+    it('returns the last state on failure', () => {
+      const action = updatePersonFailure()
+      expect(allegationsFormReducer(List(), action))
+        .toEqualImmutable(List())
     })
   })
 })
