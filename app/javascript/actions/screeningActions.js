59630399e3f270828b645dd9f452ff287bfd68a6
Nate Jones <Nate.Jones@OSI.ca.gov>
2017-11-17 14:21:24 -0800

    Formatting

--- app/javascript/actions/screeningActions.js
+++ app/javascript/actions/screeningActions.js
@@ -16,23 +16,18 @@ export const SAVE_SCREENING_COMPLETE = 'SAVE_SCREENING_COMPLETE'
 export function createScreeningSuccess(screening) {
   return {type: CREATE_SCREENING_COMPLETE, payload: {screening}}
 }
-
 export function createScreeningFailure(error) {
   return {type: CREATE_SCREENING_COMPLETE, payload: {error}, error: true}
 }
-
 export function createScreening() {
   return {type: CREATE_SCREENING}
 }
-
 export function fetchScreeningSuccess(screening) {
   return {type: FETCH_SCREENING_COMPLETE, payload: {screening}}
 }
-
 export function fetchScreeningFailure(error) {
   return {type: FETCH_SCREENING_COMPLETE, payload: {error}, error: true}
 }
-
 export function fetchScreening(id) {
   return {type: FETCH_SCREENING, payload: {id}}
 }
@@ -45,39 +40,30 @@ export function saveFailure(error) {
 export function save(screening) {
   return {type: SAVE_SCREENING, payload: {screening}}
 }
-
 export function fetchHistoryOfInvolvementsSuccess(history_of_involvements) {
   return {type: FETCH_HISTORY_OF_INVOLVEMENTS_COMPLETE, payload: {history_of_involvements}}
 }
-
 export function fetchHistoryOfInvolvementsFailure(error) {
   return {type: FETCH_HISTORY_OF_INVOLVEMENTS_COMPLETE, payload: {error}, error: true}
 }
-
 export function fetchHistoryOfInvolvements(id) {
   return {type: FETCH_HISTORY_OF_INVOLVEMENTS, payload: {id}}
 }
-
 export function submitScreeningSuccess(screening) {
   return {type: SUBMIT_SCREENING_COMPLETE, payload: {screening}}
 }
-
 export function submitScreeningFailure(error) {
   return {type: SUBMIT_SCREENING_COMPLETE, payload: {error}, error: true}
 }
-
 export function submitScreening(id) {
   return {type: SUBMIT_SCREENING, payload: {id}}
 }
-
 export function fetchRelationshipsSuccess(relationships) {
   return {type: FETCH_RELATIONSHIPS_COMPLETE, payload: {relationships}}
 }
-
 export function fetchRelationshipsFailure(error) {
   return {type: FETCH_RELATIONSHIPS_COMPLETE, payload: {error}, error: true}
 }
-
 export function fetchRelationships(id) {
   return {type: FETCH_RELATIONSHIPS, payload: {id}}
 }
