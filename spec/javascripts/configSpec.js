e0bbfe89ce50670ff1c89b73569ebe183cada8ff
Aman Alshurafa <Aman.Alshurafa@OSI.ca.gov>
2017-11-21 21:04:46 -0800

    Removing JS basePath, no longer used

--- spec/javascripts/configSpec.js
+++ spec/javascripts/configSpec.js
@@ -1,4 +1,4 @@
-import {isFeatureActive, isFeatureInactive, config, jsClipboardSupported, basePath, sdmPath} from 'common/config'
+import {isFeatureActive, isFeatureInactive, config, jsClipboardSupported, sdmPath} from 'common/config'
 
 describe('intake config', () => {
   let windowOrg
@@ -67,18 +67,6 @@ describe('intake config', () => {
       it('returns true', () => {
         expect(isFeatureInactive('enabled_feature')).toEqual(true)
       })
-    })
-  })
-
-  describe('.basePath', () => {
-    const basePathString = '/base-path-test'
-
-    beforeEach(() => {
-      window.org = Object.freeze({intake: {config: {base_path: basePathString}}})
-    })
-
-    it('returns the proper base path', () => {
-      expect(basePath()).toEqual(basePathString)
     })
   })
 
