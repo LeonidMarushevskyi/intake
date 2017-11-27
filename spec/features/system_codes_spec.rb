94dc2e691966354a68b15d0693e4c2fd88852fc8
John Gedeon <John.Gedeon@OSI.ca.gov>
2017-11-27 10:56:29 -0800

    Fix stubbing in system code feature test
    [INT-35]

--- spec/features/system_codes_spec.rb
+++ spec/features/system_codes_spec.rb
@@ -4,7 +4,10 @@ require 'rails_helper'
 
 feature 'System codes' do
   scenario 'system codes are fetch once per page load' do
-    stub_request(:get, ferb_api_url(ExternalRoutes.ferb_api_lov_path)).and_return(status: 200)
+    stub_request(:get, intake_api_url(ExternalRoutes.intake_api_screenings_path))
+      .and_return(json_body([].to_json, status: 200))
+    stub_request(:get, ferb_api_url(ExternalRoutes.ferb_api_lov_path))
+      .and_return(json_body([].to_json, status: 200))
     visit root_path
     click_link 'Start Screening'
     expect(a_request(:get, ferb_api_url(ExternalRoutes.ferb_api_lov_path))).to have_been_made.once
