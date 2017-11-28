eb5f693b294f7a04e917c47051fef8a512b656e0
Nate Jones <Nate.Jones@OSI.ca.gov>
2017-11-28 11:02:23 -0500

    Person card is in edit mode after a person is added
    [#INT-9]

--- app/javascript/reducers/screeningPageReducer.js
+++ app/javascript/reducers/screeningPageReducer.js
@@ -1,8 +1,16 @@
 import {SET_SCREENING_PAGE_MODE, SET_PERSON_CARD_MODE} from 'actions/screeningPageActions'
+import {CREATE_PERSON_COMPLETE} from 'actions/personCardActions'
 import {Map} from 'immutable'
 import {createReducer} from 'utils/createReducer'
 
 export default createReducer(Map(), {
   [SET_SCREENING_PAGE_MODE]: (state, {payload: {mode}}) => state.set('mode', mode),
   [SET_PERSON_CARD_MODE]: (state, {payload: {personId, mode}}) => state.setIn(['peopleCards', personId], mode),
+  [CREATE_PERSON_COMPLETE]: (state, {payload: {person}, error}) => {
+    if (error) {
+      return state
+    } else {
+      return state.setIn(['peopleCards', person.id], 'edit')
+    }
+  },
 })
--- spec/javascripts/reducers/screeningPageReducerSpec.js
+++ spec/javascripts/reducers/screeningPageReducerSpec.js
@@ -1,30 +1,58 @@
 import * as matchers from 'jasmine-immutable-matchers'
 import {setPageMode, setPersonCardMode} from 'actions/screeningPageActions'
 import screeningPageReducer from 'reducers/screeningPageReducer'
+import {createPersonSuccess, createPersonFailure} from 'actions/personCardActions'
 import {Map, fromJS} from 'immutable'
 
 describe('screeningPageReducer', () => {
   beforeEach(() => jasmine.addMatchers(matchers))
 
   describe('on SET_SCREENING_PAGE_MODE', () => {
+    const initialState = Map()
+
     it('returns the screening page with the updated mode', () => {
       const action = setPageMode('edit')
-      expect(screeningPageReducer(Map(), action)).toEqualImmutable(
+      expect(screeningPageReducer(initialState, action)).toEqualImmutable(
         fromJS({mode: 'edit'})
       )
     })
   })
   describe('on SET_PERSON_CARD_MODE', () => {
+    const initialState = fromJS({mode: 'edit'})
+
     it('returns the screening page with the updated person card mode', () => {
       const personId = 'some-arbitrary-id'
       const action = setPersonCardMode(personId, 'show')
-      const initialState = fromJS({mode: 'edit'})
       expect(screeningPageReducer(initialState, action)).toEqualImmutable(
         fromJS({
           mode: 'edit',
           peopleCards: {'some-arbitrary-id': 'show'},
         })
       )
+    })
+  })
+  describe('on CREATE_PERSON_COMPLETE', () => {
+    const initialState = fromJS({mode: 'show'})
+    const newPerson = {id: 'some-arbitrary-id'}
+
+    describe('when there is no error', () => {
+      it('returns the screening page with the created person card in edit mode', () => {
+        const action = createPersonSuccess(newPerson)
+        expect(screeningPageReducer(initialState, action)).toEqualImmutable(
+          fromJS({
+            mode: 'show',
+            peopleCards: {'some-arbitrary-id': 'edit'},
+          })
+        )
+      })
+    })
+    describe('when there is an error', () => {
+      it('returns the screening page untouched', () => {
+        const action = createPersonFailure()
+        expect(screeningPageReducer(initialState, action)).toEqualImmutable(
+          initialState
+        )
+      })
     })
   })
 })
