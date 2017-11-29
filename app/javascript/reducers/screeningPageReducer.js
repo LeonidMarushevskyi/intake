557174135279bbf281e9928ee63d5e78bf32977b
Nate Jones <Nate.Jones@OSI.ca.gov>
2017-11-29 11:30:20 -0500

    Refactor screening page reducer
    Extract functions to make code more readable
    
    [#INT-9]

--- app/javascript/reducers/screeningPageReducer.js
+++ app/javascript/reducers/screeningPageReducer.js
@@ -5,6 +5,20 @@ import {Map} from 'immutable'
 import {SET_SCREENING_PAGE_MODE, SET_PERSON_CARD_MODE} from 'actions/screeningPageActions'
 import {createReducer} from 'utils/createReducer'
 
+function peopleCards({participants}, mode) {
+  return Map(
+    participants.reduce((peopleCards, {id}) => ({...peopleCards, [id]: mode}), {})
+  )
+}
+
+function modeOnCreatePerson() {
+  if (IntakeConfig.isFeatureActive('release_two')) {
+    return 'show'
+  } else {
+    return 'edit'
+  }
+}
+
 export default createReducer(Map(), {
   [SET_SCREENING_PAGE_MODE]: (state, {payload: {mode}}) => state.set('mode', mode),
   [SET_PERSON_CARD_MODE]: (state, {payload: {personId, mode}}) => state.setIn(['peopleCards', personId], mode),
@@ -13,20 +27,14 @@ export default createReducer(Map(), {
       return state
     } else {
       const mode = state.get('mode')
-      const peopleCards = screening.participants.reduce(
-        (peopleCards, {id}) => ({...peopleCards, [id]: mode}),
-        {}
-      )
-      return state.set('peopleCards', Map(peopleCards))
+      return state.set('peopleCards', peopleCards(screening, mode))
     }
   },
   [CREATE_PERSON_COMPLETE]: (state, {payload: {person}, error}) => {
     if (error) {
       return state
-    } else if (IntakeConfig.isFeatureActive('release_two')) {
-      return state.setIn(['peopleCards', person.id], 'show')
     } else {
-      return state.setIn(['peopleCards', person.id], 'edit')
+      return state.setIn(['peopleCards', person.id], modeOnCreatePerson())
     }
   },
 })
