266ef55cb5335a23eecd32648a573b9cf86531aa
Sara Bocciardi <Sara.Bocciardi@OSI.ca.gov>
2017-12-07 11:55:05 -0800

    Add disable prop for the screening submit button
    [#554]

--- app/javascript/screenings/ScreeningSubmitButton.jsx
+++ app/javascript/screenings/ScreeningSubmitButton.jsx
@@ -1,12 +1,13 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-const ScreeningSubmitButton = ({actions, params}) => (
+const ScreeningSubmitButton = ({actions, disabled, params}) => (
   <div className='row'>
     <div className='centered double-gap-top double-gap-bottom'>
       <button
         className='btn btn-primary'
         onClick={() => actions.submitScreening(params.id)}
+        disabled={disabled}
       >
         Submit
       </button>
@@ -16,6 +17,7 @@ const ScreeningSubmitButton = ({actions, params}) => (
 
 ScreeningSubmitButton.propTypes = {
   actions: PropTypes.object.isRequired,
+  disabled: PropTypes.bool,
   params: PropTypes.object.isRequired,
 }
 export default ScreeningSubmitButton
--- spec/javascripts/components/screenings/ScreeningSubmitButtonSpec.jsx
+++ spec/javascripts/components/screenings/ScreeningSubmitButtonSpec.jsx
@@ -3,17 +3,30 @@ import ScreeningSubmitButton from 'screenings/ScreeningSubmitButton'
 import {shallow} from 'enzyme'
 
 describe('ScreeningSubmitButton', () => {
-  let component
-  let submitScreening
-
-  beforeEach(() => {
-    submitScreening = jasmine.createSpy('submitScreening')
-    const props = {params: {id: '99'}, actions: {submitScreening}}
-    component = shallow(<ScreeningSubmitButton {...props} />)
-  })
+  const renderScreeningSubmitButton = ({params = {id: '1'}, actions = {}, disabled = false}) => {
+    const props = {
+      params,
+      actions,
+      disabled,
+    }
+    return shallow(<ScreeningSubmitButton {...props} />)
+  }
 
   it('clicking the submit button submits the screening', () => {
-    component.find('button[children="Submit"]').simulate('click')
+    const submitScreening = jasmine.createSpy('submitScreening')
+    const params = {id: '99'}
+    const actions = {submitScreening}
+    const disabled = false
+    const component = renderScreeningSubmitButton({params, actions, disabled})
+    const submitButton = component.find('button[children="Submit"]')
+    expect(submitButton.props().disabled).toEqual(false)
+    submitButton.simulate('click')
     expect(submitScreening).toHaveBeenCalledWith('99')
+  })
+
+  it('disables the submit button if disabled is true', () => {
+    const component = renderScreeningSubmitButton({disabled: true})
+    const submitButton = component.find('button[children="Submit"]')
+    expect(submitButton.props().disabled).toEqual(true)
   })
 })
