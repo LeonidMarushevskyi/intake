34a31aa37fcaa18ca53f1d01c349b82ddecbc110
anuroop <anuroop.buggaveeti15@gmail.com>
2017-12-08 15:35:42 -0800

    Container test for the cases peice of the new HOI payload
    [#550]

--- spec/javascripts/containers/screenings/HistoryTableContainerSpec.jsx
+++ spec/javascripts/containers/screenings/HistoryTableContainerSpec.jsx
@@ -8,6 +8,98 @@ import * as IntakeConfig from 'common/config'
 describe('HistoryTableContainer', () => {
   const state = fromJS({
     involvements: {
+      cases: [
+        {
+          id: 'ccccccccc',
+          start_date: '2017-12-02',
+          end_date: '2017-12-02',
+          county: {
+            id: '1101',
+            description: 'Sacramento',
+          },
+          service_component: {
+            id: '1695',
+            description: 'Permanent Placement',
+          },
+          focus_child: {
+            id: 'iiiiiii',
+            first_name: 'Victim First Name',
+            last_name: 'Victim Last Name',
+            legacy_descriptor: {
+              legacy_id: 'iiiiiii',
+              legacy_ui_id: 'iiiiiii-hohj-jkj',
+              legacy_last_updated: '2017-12-03T11:59:42.715-0800',
+              legacy_table_name: 'CLIENT_T',
+              legacy_table_description: 'Client',
+            },
+            limited_access_code: 'SEALED',
+          },
+          assigned_social_worker: {
+            id: 'jhgguhgjh',
+            first_name: 'Worker First Name',
+            last_name: 'Worker Last Name',
+            legacy_descriptor: {
+              legacy_id: 'jhgguhgjh',
+              legacy_ui_id: 'jhgguhgjh-hohj-jkj',
+              legacy_last_updated: '2017-12-03T11:59:42.631-0800',
+              legacy_table_name: 'STFPERST',
+              legacy_table_description: 'Staff',
+            },
+          },
+          access_limitation: {
+            limited_access_code: 'SEALED',
+            limited_access_date: '2017-12-02',
+            limited_access_description: 'bla bla blah',
+            limited_access_government_entity: {
+              id: '1101',
+              description: 'Sacramento',
+            },
+          },
+          parents: [
+            {
+              id: 'fafafafafa',
+              first_name: 'Father First Name',
+              last_name: 'Father Last Name',
+              legacy_descriptor: {
+                legacy_id: 'fafafafafa',
+                legacy_ui_id: 'fafafafafa-hohj-jkj',
+                legacy_last_updated: '2017-12-03T11:59:42.716-0800',
+                legacy_table_name: 'CLIENT_T',
+                legacy_table_description: 'Client',
+              },
+              relationship: {
+                id: '214',
+                description: 'Father/Son (Step)',
+              },
+              limited_access_code: 'NONE',
+            },
+            {
+              id: 'mmmmmmmmmm',
+              first_name: 'Mother First Name',
+              last_name: 'Mother Last Name',
+              legacy_descriptor: {
+                legacy_id: 'mmmmmmmmmm',
+                legacy_ui_id: 'mmmmmmmmmm-hohj-jkj',
+                legacy_last_updated: '2017-12-03T11:59:42.716-0800',
+                legacy_table_name: 'CLIENT_T',
+                legacy_table_description: 'Client',
+              },
+              relationship: {
+                id: '250',
+                description: 'Mother/Son (Adoptive)',
+              },
+              limited_access_code: 'NONE',
+            },
+          ],
+          legacy_descriptor: {
+            legacy_id: 'ccccccccc',
+            legacy_ui_id: 'ccccccccc-hohj-jkj',
+            legacy_last_updated: '2017-12-03T11:59:42.715-0800',
+            legacy_table_name: 'CASE_T',
+            legacy_table_description: 'Case',
+          },
+        },
+      ],
       referrals: [
         {
           id: 'jhvuify0X5',
@@ -123,5 +215,17 @@ describe('HistoryTableContainer', () => {
       worker: 'Howling Murdock',
       reporter: 'Templeton Pec',
     }])
+  })
+  it('passes formatted cases HistoryTable view', () => {
+    expect(component.find('HistoryTable').props().cases).toEqual([{
+      caseId: 'ccccccccc-hohj-jkj',
+      county: 'Sacramento',
+      dateRange: '12/02/2017 - 12/02/2017',
+      focusChild: 'Victim First Name Victim Last Name',
+      parents: 'Father First Name Father Last Name, Mother First Name Mother Last Name',
+      restrictedAccessStatus: 'Sealed',
+      status: 'Closed - Permanent Placement',
+      worker: 'Worker First Name Worker Last Name'},
+    ])
   })
 })
