83ceab9bcfa6b581209b11bf32e1cfa2c95dfbd1
Tyler Clemens <Tyler.Clemens@OSI.ca.gov>
2017-12-12 08:48:44 -0800

    only pass what we need to, to rails
    [#17]

--- app/javascript/sagas/createParticipantSaga.js
+++ app/javascript/sagas/createParticipantSaga.js
@@ -13,7 +13,17 @@ import {getScreeningIdValueSelector} from 'selectors/screeningSelectors'
 
 export function* createParticipant({payload: {person}}) {
   try {
-    const response = yield call(post, '/api/v1/participants', person)
+    const {screening_id, legacy_descriptor} = person
+    const {legacy_id, legacy_table_name} = legacy_descriptor || {}
+    const response = yield call(post, '/api/v1/participants', {
+      participant: {
+        screening_id,
+        legacy_descriptor: {
+          legacy_id,
+          legacy_table_name,
+        },
+      },
+    })
     yield put(createPersonSuccess(response))
     const screeningId = yield select(getScreeningIdValueSelector)
     yield put(fetchRelationships(screeningId))
