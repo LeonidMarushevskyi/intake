864cfe1d6d854b8af001bd82e40fc070bdc82da9
Nate Jones <Nate.Jones@OSI.ca.gov>
2017-12-15 09:45:29 -0800

    Cleanup participant search feature spec
    [#INT-7]

--- spec/features/screening/participant/search_participant_spec.rb
+++ spec/features/screening/participant/search_participant_spec.rb
@@ -4,111 +4,8 @@ require 'rails_helper'
 require 'spec_helper'
 require 'feature/testing'
 feature 'searching a participant in autocompleter' do
-  def build_response_from_person(person)
-    sealed_and_sensitive = 'N'
-    sealed_and_sensitive = 'S' if person.sensitive
-    sealed_and_sensitive = 'R' if person.sealed
-
-    {
-      hits: {
-        total: 1,
-        hits: [{
-          _source: {
-            id: person.id,
-            legacy_source_table: 'CLIENT_T',
-            first_name: person.first_name,
-            gender: person.gender,
-            last_name: person.last_name,
-            middle_name: person.middle_name,
-            name_suffix: person.name_suffix,
-            ssn: person.ssn,
-            phone_numbers: [{ 'number' => phone_number.number, 'type' => phone_number.type }],
-            languages: [
-              { name: 'French', primary: true },
-              { name: 'Italian' }
-            ],
-            addresses: [{
-              'legacy_id' => person.addresses.first.legacy_id,
-              'legacy_source_table' => person.addresses.first.legacy_source_table,
-              'street_number' => 123,
-              'street_name' => 'Fake St',
-              'state_code' => 'NY',
-              'city' => 'Springfield',
-              'zip' => '12345',
-              'type' => ''
-            }],
-            date_of_birth: date_of_birth.to_s(:db),
-            legacy_descriptor: {
-              legacy_last_updated: person.legacy_descriptor.legacy_last_updated,
-              legacy_id: person.legacy_descriptor.legacy_id,
-              legacy_ui_id: person.legacy_descriptor.legacy_ui_id,
-              legacy_table_name: person.legacy_descriptor.legacy_table_name,
-              legacy_table_description: person.legacy_descriptor.legacy_table_description
-            },
-            race_ethnicity: {
-              hispanic_origin_code: 'Y',
-              race_codes: [
-                { description: 'White - European*' },
-                { description: 'Alaskan Native*' }
-              ],
-              hispanic_codes: [
-                { description: 'Central American' }
-              ],
-              hispanic_unable_to_determine_code: ''
-            },
-            sensitivity_indicator: sealed_and_sensitive
-          }
-        }]
-      }
-    }
-  end
-
   let(:existing_screening) { FactoryGirl.create(:screening) }
   let(:date_of_birth) { 15.years.ago.to_date }
-  let(:address) do
-    FactoryGirl.create(
-      :address,
-      street_address: '123 Fake St',
-      city: 'Springfield',
-      state: 'NY',
-      zip: '12345',
-      type: 'Work'
-    )
-  end
-  let(:phone_number) do
-    FactoryGirl.create(
-      :phone_number,
-      number: '971-287-6774',
-      type: 'Home'
-    )
-  end
-  let(:person) do
-    FactoryGirl.create(
-      :person_search,
-      date_of_birth: date_of_birth.to_s(:db),
-      first_name: 'Marge',
-      middle_name: 'Jacqueline',
-      name_suffix: 'md',
-      gender: 'female',
-      last_name: 'Simpson',
-      ssn: '123231234',
-      languages: %w[French Italian],
-      legacy_descriptor: FactoryGirl.create(:legacy_descriptor),
-      addresses: [address],
-      phone_numbers: [phone_number],
-      races: [
-        { race: 'White', race_detail: 'European' },
-        { race: 'American Indian or Alaska Native' }
-      ],
-      ethnicity: { hispanic_latino_origin: 'Yes', ethnicity_detail: ['Central American'] },
-      sensitive: true,
-      sealed: false
-    )
-  end
-
-  let(:marge_response) do
-    build_response_from_person(person)
-  end
   before do
     stub_request(
       :get, intake_api_url(ExternalRoutes.intake_api_screening_path(existing_screening.id))
@@ -120,8 +17,54 @@ feature 'searching a participant in autocompleter' do
 
   context 'search for a person' do
     scenario 'search result contains person information' do
-      built_response = build_response_from_person(person)
-      stub_person_search('Ma', built_response)
+      search_response = {
+        hits: {
+          total: 1,
+          hits: [{
+          _source: {
+            legacy_source_table: '',
+            first_name: 'Marge',
+            gender: 'female',
+            last_name: 'Simpson',
+            middle_name: 'Jacqueline',
+            name_suffix: 'md',
+            ssn: '123231234',
+            phone_numbers: [
+              { 'number' => '971-287-6774', 'type' => 'Home' }
+            ],
+            languages: [
+              { name: 'French', primary: true },
+              { name: 'Italian' }
+            ],
+            race_ethnicity: {
+              hispanic_origin_code: 'Y',
+              race_codes: [
+                { description: 'White - European*' },
+                { description: 'Alaskan Native*' }
+              ],
+              hispanic_codes: [
+                { description: 'Central American' }
+              ],
+              hispanic_unable_to_determine_code: ''
+            },
+            addresses: [{
+              'legacy_id' => '',
+              'legacy_source_table' => '',
+              'street_number' => 123,
+              'street_name' => 'Fake St',
+              'state_code' => 'NY',
+              'city' => 'Springfield',
+              'zip' => '11222',
+              'type' => ''
+            }],
+            date_of_birth: date_of_birth.to_s(:db),
+            legacy_descriptor: {},
+            sensitivity_indicator: 'S'
+          }
+        }]
+        }
+      }
+      stub_person_search('Ma', search_response)
 
       within '#search-card', text: 'Search' do
         fill_in_autocompleter 'Search for any person', with: 'Ma'
@@ -137,43 +80,71 @@ feature 'searching a participant in autocompleter' do
         expect(page).to have_content 'Home'
         expect(page).to have_content '971-287-6774'
         expect(page).to have_content 'SSN'
-        expect(page).to have_content '1234'
         expect(page).to have_content '123-23-1234'
-        expect(page).to have_content '123 Fake St, Springfield, NY 12345'
+        expect(page).to have_content '123 Fake St, Springfield, NY 11222'
         expect(page).to have_content 'Sensitive'
         expect(page).to_not have_content 'Sealed'
       end
     end
 
     scenario 'search results format the SSN' do
-      marge = FactoryGirl.create(
-        :person_search,
-        first_name: 'Marge',
-        addresses: [address],
-        legacy_descriptor: FactoryGirl.create(:legacy_descriptor),
-        ssn: '123456789'
-      )
-      built_response = build_response_from_person(marge)
-      stub_person_search('Ma', built_response)
-
+      search_response = {
+        hits: {
+          total: 1,
+          hits: [{
+          _source: {
+            legacy_source_table: 'CLIENT_T',
+            first_name: 'Marge',
+            gender: '',
+            last_name: '',
+            middle_name: '',
+            name_suffix: '',
+            ssn: '123456789',
+            phone_numbers: [],
+            languages: [],
+            addresses: [],
+            date_of_birth: '',
+            legacy_descriptor: {},
+            race_ethnicity: {},
+            sensitivity_indicator: ''
+          }
+        }]
+        }
+      }
+      stub_person_search('Ma', search_response)
       within '#search-card', text: 'Search' do
         fill_in_autocompleter 'Search for any person', with: 'Ma'
       end
-
       within '.react-autosuggest__suggestions-list' do
         expect(page).to have_content '123-45-6789'
       end
     end
 
     scenario 'results include information about the legacy source information for a person' do
-      marge = FactoryGirl.create(
-        :person_search,
-        first_name: 'Marge',
-        addresses: [address],
-        legacy_descriptor: { legacy_ui_id: '123-456-789', legacy_table_description: 'Client' }
-      )
-      built_response = build_response_from_person(marge)
-      stub_person_search('Ma', built_response)
+      search_response = {
+        hits: {
+          total: 1,
+          hits: [{
+          _source: {
+            legacy_source_table: '',
+            first_name: 'Marge',
+            gender: '',
+            last_name: '',
+            middle_name: '',
+            name_suffix: '',
+            ssn: '',
+            phone_numbers: [],
+            languages: [],
+            addresses: [],
+            date_of_birth: '',
+            legacy_descriptor: { legacy_ui_id: '123-456-789', legacy_table_description: 'Client' },
+            race_ethnicity: {},
+            sensitivity_indicator: ''
+          }
+        }]
+        }
+      }
+      stub_person_search('Ma', search_response)
 
       within '#search-card', text: 'Search' do
         fill_in_autocompleter 'Search for any person', with: 'Ma'
@@ -185,7 +156,30 @@ feature 'searching a participant in autocompleter' do
     end
 
     scenario 'person without phone_numbers' do
-      stub_person_search('Ma', marge_response)
+      search_response = {
+        hits: {
+          total: 1,
+          hits: [{
+          _source: {
+            legacy_source_table: '',
+            first_name: 'Marge',
+            gender: '',
+            last_name: 'Simpson',
+            middle_name: 'Jacqueline',
+            name_suffix: 'md',
+            ssn: '',
+            phone_numbers: [],
+            languages: [],
+            addresses: [],
+            date_of_birth: '',
+            legacy_descriptor: {},
+            race_ethnicity: {},
+            sensitivity_indicator: ''
+          }
+        }]
+        }
+      }
+      stub_person_search('Ma', search_response)
 
       within '#search-card', text: 'Search' do
         fill_in_autocompleter 'Search for any person', with: 'Ma'
@@ -197,7 +191,30 @@ feature 'searching a participant in autocompleter' do
     end
 
     scenario 'person without addresses' do
-      stub_person_search('Ma', marge_response)
+      search_response = {
+        hits: {
+          total: 1,
+          hits: [{
+          _source: {
+            legacy_source_table: '',
+            first_name: 'Marge',
+            gender: '',
+            last_name: 'Simpson',
+            middle_name: 'Jacqueline',
+            name_suffix: 'md',
+            ssn: '',
+            phone_numbers: [],
+            languages: [],
+            addresses: [],
+            date_of_birth: '',
+            legacy_descriptor: {},
+            race_ethnicity: {},
+            sensitivity_indicator: ''
+          }
+        }]
+        }
+      }
+      stub_person_search('Ma', search_response)
 
       within '#search-card', text: 'Search' do
         fill_in_autocompleter 'Search for any person', with: 'Ma'
@@ -209,16 +226,30 @@ feature 'searching a participant in autocompleter' do
     end
 
     scenario 'person who is neither sensitive nor sealed' do
-      marge = FactoryGirl.create(
-        :person_search,
-        first_name: 'Marge',
-        addresses: [address],
-        legacy_descriptor: FactoryGirl.create(:legacy_descriptor),
-        sensitive: false,
-        sealed: false
-      )
-      built_response = build_response_from_person(marge)
-      stub_person_search('Ma', built_response)
+      search_response = {
+        hits: {
+          total: 1,
+          hits: [{
+          _source: {
+            legacy_source_table: '',
+            first_name: 'Marge',
+            gender: '',
+            last_name: '',
+            middle_name: '',
+            name_suffix: '',
+            ssn: '',
+            phone_numbers: [],
+            languages: [],
+            addresses: [],
+            date_of_birth: '',
+            legacy_descriptor: {},
+            race_ethnicity: {},
+            sensitivity_indicator: 'N'
+          }
+        }]
+        }
+      }
+      stub_person_search('Ma', search_response)
 
       within '#search-card', text: 'Search' do
         fill_in_autocompleter 'Search for any person', with: 'Ma'
@@ -231,16 +262,30 @@ feature 'searching a participant in autocompleter' do
     end
 
     scenario 'person who is sensitive' do
-      marge = FactoryGirl.create(
-        :person_search,
-        first_name: 'Marge',
-        addresses: [address],
-        legacy_descriptor: FactoryGirl.create(:legacy_descriptor),
-        sensitive: true,
-        sealed: false
-      )
-      built_response = build_response_from_person(marge)
-      stub_person_search('Ma', built_response)
+      search_response = {
+        hits: {
+          total: 1,
+          hits: [{
+          _source: {
+            legacy_source_table: '',
+            first_name: 'Marge',
+            gender: '',
+            last_name: '',
+            middle_name: '',
+            name_suffix: '',
+            ssn: '',
+            phone_numbers: [],
+            languages: [],
+            addresses: [],
+            date_of_birth: '',
+            legacy_descriptor: {},
+            race_ethnicity: {},
+            sensitivity_indicator: 'S'
+          }
+        }]
+        }
+      }
+      stub_person_search('Ma', search_response)
 
       within '#search-card', text: 'Search' do
         fill_in_autocompleter 'Search for any person', with: 'Ma'
@@ -253,16 +298,30 @@ feature 'searching a participant in autocompleter' do
     end
 
     scenario 'person who is sealed' do
-      marge = FactoryGirl.create(
-        :person_search,
-        first_name: 'Marge',
-        addresses: [address],
-        legacy_descriptor: FactoryGirl.create(:legacy_descriptor),
-        sensitive: false,
-        sealed: true
-      )
-      built_response = build_response_from_person(marge)
-      stub_person_search('Ma', built_response)
+      search_response = {
+        hits: {
+          total: 1,
+          hits: [{
+          _source: {
+            legacy_source_table: '',
+            first_name: 'Marge',
+            gender: '',
+            last_name: '',
+            middle_name: '',
+            name_suffix: '',
+            ssn: '',
+            phone_numbers: [],
+            languages: [],
+            addresses: [],
+            date_of_birth: '',
+            legacy_descriptor: {},
+            race_ethnicity: {},
+            sensitivity_indicator: 'R'
+          }
+        }]
+        }
+      }
+      stub_person_search('Ma', search_response)
 
       within '#search-card', text: 'Search' do
         fill_in_autocompleter 'Search for any person', with: 'Ma'
@@ -311,7 +370,7 @@ feature 'searching a participant in autocompleter' do
               sealed: false,
               races: [],
               ethnicity: {},
-              legacy_id: 'Ca10L2205I'
+              legacy_id: ''
             }
           }]
         }
