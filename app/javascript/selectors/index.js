532305269eab0b669439540466dd9df52c69746a
Tyler Clemens <Tyler.Clemens@OSI.ca.gov>
2017-12-20 13:14:35 -0800

    add a method to find an LOV by its code
    [#537]

--- app/javascript/selectors/index.js
+++ app/javascript/selectors/index.js
@@ -2,3 +2,7 @@ import {List} from 'immutable'
 export const findByCategory = (statusCodes = List(), selectedCategory) => (
   statusCodes.filter(({category}) => category === selectedCategory)
 )
+
+export const findByCode = (statusCodes, selectedCode) => (
+  statusCodes.find(({code}) => code === selectedCode) || {}
+)
