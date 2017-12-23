bc2c817aa10fe7daf33a87274726f06b15757b90
Aman Alshurafa <Aman.Alshurafa@OSI.ca.gov>
2017-12-23 13:25:35 -0800

    Refactor and clean up getPageHeaderDetailSelector to reduce complexity
    [#19]

--- app/javascript/selectors/pageLayoutSelectors.js
+++ app/javascript/selectors/pageLayoutSelectors.js
@@ -3,28 +3,48 @@ import {getScreeningTitleSelector, getScreeningIsReadOnlySelector} from 'selecto
 import {getAllCardsAreSavedValueSelector, getScreeningHasErrorsSelector} from 'selectors/screening/screeningPageSelectors'
 import {isFeatureActive} from 'common/config'
 
+const getDashboardDetailSelector = () => ({
+  pageHeaderButtonDisabled: false,
+  pageHeaderButtonText: 'Start Screening',
+  pageHeaderHasButton: true,
+  pageHeaderLocation: 'dashboard',
+  pageHeaderTitle: 'Dashboard',
+})
+
+const getScreeningDetailSelector = (state) => {
+  const buttonDisabled = (!getAllCardsAreSavedValueSelector(state) || getScreeningHasErrorsSelector(state))
+  const hasButton = !((getScreeningIsReadOnlySelector(state) || isFeatureActive('release_two')))
+  return {
+    pageHeaderButtonDisabled: buttonDisabled,
+    pageHeaderButtonText: 'Submit',
+    pageHeaderHasButton: hasButton,
+    pageHeaderLocation: 'screening',
+    pageHeaderTitle: getScreeningTitleSelector(state),
+  }
+}
+
+const getContactDetailSelector = (state) => ({
+  pageHeaderHasButton: false,
+  pageHeaderLocation: 'contact',
+  pageHeaderTitle: `Contact for ${getInvestigationTitleSelector(state)}`,
+})
+
+const getInvestigationDetailSelector = (state) => ({
+  pageHeaderHasButton: false,
+  pageHeaderLocation: 'investigation',
+  pageHeaderTitle: getInvestigationTitleSelector(state),
+})
+
 export const getPageHeaderDetailSelector = (path, state) => {
-  let pageHeaderDetails = {pageHeaderTitle: '', pageHeaderButton: null}
+  let pageHeaderDetails = {pageHeaderTitle: '', pageHeaderHasButton: false}
   if (path === '/') {
-    pageHeaderDetails = {
-      pageHeaderButtonDisabled: false, pageHeaderButtonText: 'Start Screening',
-      pageHeaderHasButton: true, pageHeaderLocation: 'dashboard', pageHeaderTitle: 'Dashboard',
-    }
+    pageHeaderDetails = getDashboardDetailSelector()
   } else if (path.includes('screenings')) {
-    const pageHeaderHasButton = !((getScreeningIsReadOnlySelector(state) || isFeatureActive('release_two')))
-    pageHeaderDetails = {
-      pageHeaderButtonDisabled: !getAllCardsAreSavedValueSelector(state) || getScreeningHasErrorsSelector(state),
-      pageHeaderButtonText: 'Submit', pageHeaderHasButton: pageHeaderHasButton,
-      pageHeaderLocation: 'screening', pageHeaderTitle: getScreeningTitleSelector(state),
-    }
+    pageHeaderDetails = getScreeningDetailSelector(state)
   } else if (path.includes('contacts')) {
-    pageHeaderDetails = {
-      pageHeaderHasButton: false, pageHeaderLocation: 'contact',
-      pageHeaderTitle: `Contact for ${getInvestigationTitleSelector(state)}`,
-    }
+    pageHeaderDetails = getContactDetailSelector(state)
   } else if (path.includes('investigations')) {
-    pageHeaderDetails = {pageHeaderHasButton: false, pageHeaderLocation: 'investigation',
-      pageHeaderTitle: getInvestigationTitleSelector(state)}
+    pageHeaderDetails = getInvestigationDetailSelector(state)
   }
   return pageHeaderDetails
 }
