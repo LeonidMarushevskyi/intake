b80bdf728cc9be4f161dd9788d8f296b9e81b6da
Pierre Klein <pierre.klein@osi.ca.gov>
2018-01-18 15:29:01 -0800

    style empty state on history card

--- app/javascript/views/history/EmptyHistory.jsx
+++ app/javascript/views/history/EmptyHistory.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const EmptyHistory = () => (
   <div className='card-body no-pad-top'>
     <div className='row'>
-      <p>Search for people and add them to see their child welfare history.</p>
+      <div className='col-md-12 empty-relationships'>
+        <div className='double-gap-top  centered'>
+          <span className='c-dark-grey'>Search for people and add them to see their child welfare history.</span>
+        </div>
+      </div>
     </div>
   </div>
 )
