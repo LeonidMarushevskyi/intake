65761265388ce5cc326f0e7fad670498d9543a34
Pierre Klein <pierre.klein@osi.ca.gov>
2018-01-18 15:33:32 -0800

    remove hover on table

--- app/javascript/views/history/HistoryTable.jsx
+++ app/javascript/views/history/HistoryTable.jsx
@@ -10,11 +10,11 @@ export default class HistoryTable extends React.Component {
     const {cases, onCopy, onError, onSuccess, referrals, screenings} = this.props
     return (<div className='card-body no-pad-top'>
       <div className='table-responsive' ref={(history) => { this.historyTable = history }}>
-        <table className='table table-hover'>
+        <table className='table'>
           <colgroup>
-            <col className='col-md-2' />
             <col className='col-md-2'/>
-            <col className='col-md-2' />
+            <col className='col-md-2'/>
+            <col className='col-md-2'/>
             <col className='col-md-6'/>
           </colgroup>
           <thead>
