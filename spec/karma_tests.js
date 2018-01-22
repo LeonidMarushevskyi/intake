a6eb564315b30d61d9401566d65709f821b12a70
Nate Jones <Nate.Jones@OSI.ca.gov>
2018-01-22 14:18:40 -0800

    Use enzyme-adapter-react-16 for jasmine tests

--- spec/karma_tests.js
+++ spec/karma_tests.js
@@ -1,3 +1,7 @@
+var Enzyme = require('enzyme')
+var Adapter = require('enzyme-adapter-react-16')
+Enzyme.configure({adapter: new Adapter()})
+
 var componentContext = require.context('./../app/javascript', true, /\*\.(js|jsx)$/)
 componentContext.keys().forEach(componentContext)
 
