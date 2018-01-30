bb6a89b291db329f6d5a2b90c0781deae08379d2
Sara Bocciardi <Sara.Bocciardi@OSI.ca.gov>
2018-01-30 11:40:48 -0800

    Stub out layout of the snapshot page
    - Use the screening containers for now
    - Will replace them one by one
    
    [#587]

--- app/javascript/snapshots/SnapshotPage.jsx
+++ app/javascript/snapshots/SnapshotPage.jsx
@@ -1,10 +1,34 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import {connect} from 'react-redux'
+import PersonSearchFormContainer from 'containers/screenings/PersonSearchFormContainer'
+import PersonCardView from 'screenings/PersonCardView'
+import HistoryOfInvolvementContainer from 'containers/screenings/HistoryOfInvolvementContainer'
+import HistoryTableContainer from 'containers/screenings/HistoryTableContainer'
+import EmptyHistory from 'views/history/EmptyHistory'
 
-const SnapshotPage = ({id}) => (
-  <div>
-    <p>I'm a snapshot! My Id is {id}</p>
+const SnapshotPage = ({participants}) => (
+  <div className='row'>
+    <div className='card edit double-gap-bottom' id='snapshot-card'>
+      <div className='card-body'>
+        <div className='row'>
+          <div className='col-md-12'>
+            <div className='double-pad-top'>
+              The Child Welfare History Snapshot allows you to search CWS/CMS for people and their past history with CWS.
+              To start, search by any combination of name, date of birth, or social security number. Click on a person from
+              the results to add them to the Snapshot, and their basic information and history will automatically appear below.
+              You can add as many people as you like, and when ready, copy the summary of their history.
+              You will need to manually paste it into a document or a field in CWS/CMS.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <PersonSearchFormContainer />
+    {participants.map((participant) =>
+      <PersonCardView key={participant.get('id')} personId={participant.get('id')} />
+    )}
+    <HistoryOfInvolvementContainer empty={<EmptyHistory />} notEmpty={<HistoryTableContainer />} />
   </div>
 )
 
@@ -18,7 +42,7 @@ SnapshotPage.defaultProps = {
 function mapStateToProps(state) {
   const snapshot = state.get('snapshot')
   return {
-    id: snapshot.get('id')
+    participants: [],
   }
 }
 
