21fb053f257ec59dfa54202cbd0573b2bfe7584d
Sara Bocciardi <Sara.Bocciardi@OSI.ca.gov>
2018-01-30 14:33:39 -0800

    Saga to create people on a snapshot
    [#587]

---
+++ spec/javascripts/sagas/createSnapshotPersonSagaSpec.js
@@ -0,0 +1,50 @@
+import 'babel-polyfill'
+import {takeEvery, put, call} from 'redux-saga/effects'
+import {post} from 'utils/http'
+import {
+  createSnapshotPerson,
+  createSnapshotPersonSaga,
+} from 'sagas/createSnapshotPersonSaga'
+import {CREATE_SNAPSHOT_PERSON} from 'actions/personCardActions'
+import * as personCardActions from 'actions/personCardActions'
+
+describe('createSnapshotPersonSaga', () => {
+  it('creates participant on CREATE_SNAPSHOT_PERSON', () => {
+    const gen = createSnapshotPersonSaga()
+    expect(gen.next().value).toEqual(takeEvery(CREATE_SNAPSHOT_PERSON, createSnapshotPerson))
+  })
+})
+
+describe('createSnapshotPerson', () => {
+  const legacy_descriptor = {legacy_id: '1', legacy_table_name: 'table'}
+  const params = {screening_id: '1', legacy_descriptor}
+  const participant = {first_name: 'Michael', ...params}
+  const action = personCardActions.createSnapshotPerson({snapshotId: '1', legacy_descriptor})
+
+  it('creates and puts participant and fetches relationships and history', () => {
+    const gen = createSnapshotPerson(action)
+    expect(gen.next().value).toEqual(call(post, '/api/v1/participants', {participant: params}))
+    expect(gen.next(participant).value).toEqual(
+      put(personCardActions.createPersonSuccess(participant))
+    )
+  })
+
+  it('puts errors when non-403 errors are thrown', () => {
+    const gen = createSnapshotPerson(action)
+    expect(gen.next().value).toEqual(call(post, '/api/v1/participants', {participant: params}))
+    const error = {responseJSON: 'some error'}
+    expect(gen.throw(error).value).toEqual(
+      put(personCardActions.createPersonFailure('some error'))
+    )
+  })
+
+  it('calls an alert when the error status is 403', () => {
+    const gen = createSnapshotPerson(action)
+    expect(gen.next().value).toEqual(call(post, '/api/v1/participants', {participant: params}))
+    const error = {responseJSON: 'some error', status: 403}
+    expect(gen.throw(error).value).toEqual(
+      call(alert, 'You are not authorized to add this person.')
+    )
+  })
+})
+
