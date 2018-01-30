d48df13ed4db7f398c2596c891ae6d8aac3f87fd
Sara Bocciardi <Sara.Bocciardi@OSI.ca.gov>
2018-01-29 18:39:23 -0800

    Add saga for creating a snapshot
    - For now, continue to use the screenings API
    - Under the hood, a snapshot will still be a screening
    
    [#587]

--- app/javascript/actions/snapshotActions.js
+++ app/javascript/actions/snapshotActions.js
@@ -1,7 +1,14 @@
 import {
   CREATE_SNAPSHOT,
+  CREATE_SCREENING_COMPLETE,
 } from 'actions/actionTypes'
 
+export function createSnapshotSuccess(screening) {
+  return {type: CREATE_SCREENING_COMPLETE, payload: {screening}}
+}
+export function createSnapshotFailure(error) {
+  return {type: CREATE_SCREENING_COMPLETE, payload: {error}, error: true}
+}
 export function createSnapshot() {
   return {type: CREATE_SNAPSHOT}
 }
