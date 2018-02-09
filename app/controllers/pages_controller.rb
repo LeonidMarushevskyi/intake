b4083f2a1af7e565d51633d54e13792110ca2e77
Tyler Clemens <Tyler.Clemens@OSI.ca.gov>
2018-02-09 13:58:31 -0800

    remove pages controller
    [#INT-1328]

--- app/controllers/pages_controller.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-# Pages Controller handles all static page presentation.
-class PagesController < ApplicationController
-  include HighVoltage::StaticPage
-  before_action :authenticate_user, if: :authentication_enabled?
-end
