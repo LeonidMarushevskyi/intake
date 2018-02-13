63c92bec05c72e35e4ac70ed28cf2a854ae38e5f
Nate Jones <Nate.Jones@OSI.ca.gov>
2018-02-12 16:34:05 -0800

    Fix brittle test

--- spec/features/screening/submit_screening_spec.rb
+++ spec/features/screening/submit_screening_spec.rb
@@ -45,9 +45,10 @@ feature 'Submit Screening' do
         visit edit_screening_path(existing_screening.id)
         expect(page).to have_button('Submit', disabled: true)
         within('.card', text: 'Screening Information') { click_button 'Save' }
-        within('.card', text: "#{participant.first_name} #{participant.last_name}") do
+        within edit_participant_card_selector(participant.id) do
           click_button 'Save'
         end
+        expect(page).to have_css show_participant_card_selector(participant.id)
         within('.card', text: 'Narrative') { click_button 'Save' }
         within('.card', text: 'Incident Information') { click_button 'Save' }
         within('.card', text: 'Allegations') { click_button 'Save' }
