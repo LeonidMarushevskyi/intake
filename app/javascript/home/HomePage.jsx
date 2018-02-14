8517728539f64093e711a07acf70f972a62d1f83
Sara Bocciardi <Sara.Bocciardi@OSI.ca.gov>
2018-02-14 15:42:17 -0800

    Add page header to home page
    - Render create screenings button when screenings are active
    - Render create snapshot button when snapshot is active
    
    [#766]

--- app/javascript/home/HomePage.jsx
+++ app/javascript/home/HomePage.jsx
@@ -1,11 +1,13 @@
 import * as screeningActions from 'actions/screeningActions'
-import {fetch as fetchScreenings} from 'actions/screeningsActions'
+import {fetch as fetchScreenings, createScreening} from 'actions/screeningsActions'
+import {createSnapshot} from 'actions/snapshotActions'
 import PropTypes from 'prop-types'
 import React from 'react'
 import ScreeningsTable from 'screenings/ScreeningsTable'
 import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
-import * as IntakeConfig from 'common/config'
+import PageHeader from 'common/PageHeader'
+import {isFeatureActive} from 'common/config'
 
 export class HomePage extends React.Component {
   constructor() {
@@ -13,17 +15,60 @@ export class HomePage extends React.Component {
   }
 
   componentDidMount() {
-    if (IntakeConfig.isFeatureInactive('release_two')) {
+    if (isFeatureActive('screenings')) {
       this.props.actions.fetchScreenings()
     }
   }
 
+  startScreeningButton() {
+    const {actions: {createScreening}} = this.props
+    return (
+      <button type='button'
+        className='btn primary-btn pull-right'
+        disabled={false}
+        onClick={createScreening}
+      >
+        Start Screening
+      </button>
+    )
+  }
+
+  startSnapshotButton() {
+    const {actions: {createSnapshot}} = this.props
+    return (
+      <button type='button'
+        className='btn primary-btn pull-right'
+        disabled={false}
+        onClick={createSnapshot}
+        style={{marginRight: '10px'}}
+      >
+        Start Snapshot
+      </button>
+    )
+  }
+
+  buttons() {
+    return (
+      <div>
+        {isFeatureActive('screenings') && this.startScreeningButton() || <div />}
+        {isFeatureActive('snapshot') && this.startSnapshotButton() || <div />}
+      </div>
+    )
+  }
+
   render() {
     return (
-      <div className='row gap-top'>
-        <div className='col-md-3' />
-        <div className='col-md-9'>
-          { IntakeConfig.isFeatureInactive('release_two') && <ScreeningsTable screenings={this.props.screenings} /> }
+      <div>
+        <div>
+          <PageHeader pageTitle='Dashboard' button={this.buttons()} />
+        </div>
+        <div className='container'>
+          <div className='row gap-top'>
+            <div className='col-md-3' />
+            <div className='col-md-9'>
+              { isFeatureActive('screenings') && <ScreeningsTable screenings={this.props.screenings} /> }
+            </div>
+          </div>
         </div>
       </div>
     )
@@ -42,7 +87,7 @@ function mapStateToProps(state, _ownProps) {
 }
 
 function mapDispatchToProps(dispatch, _ownProps) {
-  const actions = Object.assign({}, screeningActions, {fetchScreenings})
+  const actions = {fetchScreenings, createScreening, createSnapshot, ...screeningActions}
   return {
     actions: bindActionCreators(actions, dispatch),
   }
