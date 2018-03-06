400619471d333f66647bb9553729a14f16c239df
Naga Pradeep Gajula <naga.gajula@osi.ca.gov>
2018-03-05 22:49:25 -0800

    Add link to people added/created in sidebar for snapshot [#1329]

--- app/javascript/snapshots/SnapshotSideBar.jsx
+++ app/javascript/snapshots/SnapshotSideBar.jsx
@@ -1,16 +1,37 @@
 import React from 'react'
 import {SideBar, NavLinks, NavLink} from 'react-wood-duck'
+import PropTypes from 'prop-types'
+import nameFormatter from 'utils/nameFormatter'
 
-const SnapshotSideBar = () => (
+const SnapshotSideBar = (props) => (
   <div className='col-md-2 hide-mobile'>
     <SideBar>
       <NavLinks>
-        <NavLink text='People & Roles' href='#search-card-anchor' />
+        <NavLink key={1} text='People & Roles' href='#search-card-anchor' >
+          <NavLinks nested={true} >
+            {props.participants && props.participants.map(({id, first_name, last_name, middle_name, name_suffix}) =>
+              <NavLink
+                key={id}
+                text={nameFormatter({first_name, last_name, middle_name, name_suffix})}
+                href={`#participants-card-${id}`}
+                preIcon='fa fa-user'
+              />
+            )}
+          </NavLinks>
+        </NavLink>
         <NavLink text='Relationships' href='#relationships-card-anchor' />
         <NavLink text='History' href='#history-card-anchor' />
       </NavLinks>
     </SideBar>
   </div>
 )
+
+SnapshotSideBar.propTypes = {
+  participants: PropTypes.array.isRequired,
+}
+
+SnapshotSideBar.defaultProps = {
+  participants: [],
+}
 
 export default SnapshotSideBar
