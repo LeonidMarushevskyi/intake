2fb217788296c8a9c84a425335d7a0e1252bbbb4
vikas199 <mvikasraj123@gmail.com>
2018-03-15 10:37:37 -0700

    fixed the linting issue of avoiding magic numbers

--- app/javascript/utils/phoneNumberFormatter.js
+++ app/javascript/utils/phoneNumberFormatter.js
@@ -1,9 +1,10 @@
 export const phoneNumberFormatter = (phone) => {
   if (phone) {
     phone = phone.replace(/[^\d]/g, '')
-    if (phone.length === 10) {
+    const length = 10
+    if (phone.length === length) {
       return phone.replace(/(\d{3})(\d{3})(\d{4})/, '($1)$2-$3')
     }
   }
-return null
+  return null
 }
