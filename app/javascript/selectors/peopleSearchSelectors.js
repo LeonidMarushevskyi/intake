c585f9ba78d17ac3482ae12ca300f2b0b06c5c61
Will Mitchell <will.mitchell@osi.ca.gov>
2018-03-16 18:12:39 -0700

    Bring getPeopleResultsSelector to a nicer climate

--- app/javascript/selectors/peopleSearchSelectors.js
+++ app/javascript/selectors/peopleSearchSelectors.js
@@ -26,26 +26,30 @@ export const getLastResultsSortValueSelector = (state) => {
 
 const formatSSN = (ssn) => ssn && ssn.replace(/(\d{3})(\d{2})(\d{4})/, '$1-$2-$3')
 const formatDOB = (dob, highlight) => (highlight ? '<em>'.concat(dob, '</em>') : dob)
+const formatPhoneNumber = (phoneNumber) => phoneNumber && Map({
+  number: phoneNumberFormatter(phoneNumber.get('number')),
+  type: phoneNumber.get('type'),
+})
 
 // Try to find a match from a list of highlights by stripping out <em> tags
 const highlightNameField = (exactName, highlights) => (highlights.find(
   (highlight) => highlight.replace(/<(\/)?em>/g, '') === exactName
 ) || exactName)
 
+const maybeHighlightedField = (result, highlight, key) => highlight.getIn(
+  [key, 0],
+  highlightNameField(result.get(key), highlight.get('autocomplete_search_bar', List()))
+)
+
 export const getPeopleResultsSelector = (state) => getPeopleSearchSelector(state)
   .get('results')
   .map((fullResult) => {
-    const result = fullResult.get('_source')
-    const phoneNumber = result.getIn(['phone_numbers', 0], null)
-    const highlightDateOfBirth = fullResult.hasIn(['highlight', 'searchable_date_of_birth'])
-    const autocomplete_highlight = fullResult.getIn(['highlight', 'autocomplete_search_bar'], List())
-
+    const result = fullResult.get('_source', Map())
+    const highlight = fullResult.get('highlight', Map())
     return Map({
       legacy_id: result.get('id'),
-      firstName: fullResult.getIn(['highlight', 'first_name', 0],
-        highlightNameField(result.get('first_name'), autocomplete_highlight)),
-      lastName: fullResult.getIn(['highlight', 'last_name', 0],
-        highlightNameField(result.get('last_name'), autocomplete_highlight)),
+      firstName: maybeHighlightedField(result, highlight, 'first_name'),
+      lastName: maybeHighlightedField(result, highlight, 'last_name'),
       middleName: result.get('middle_name'),
       nameSuffix: result.get('name_suffix'),
       legacyDescriptor: result.get('legacy_descriptor'),
@@ -53,13 +57,10 @@ export const getPeopleResultsSelector = (state) => getPeopleSearchSelector(state
       languages: mapLanguages(state, result),
       races: mapRaces(state, result),
       ethnicity: mapEthnicities(state, result),
-      dateOfBirth: formatDOB(result.get('date_of_birth'), highlightDateOfBirth),
-      ssn: formatSSN(fullResult.getIn(['highlight', 'ssn', 0], result.get('ssn'))),
+      dateOfBirth: formatDOB(result.get('date_of_birth'), highlight.has('searchable_date_of_birth')),
+      ssn: formatSSN(highlight.getIn(['ssn', 0], result.get('ssn'))),
       address: mapAddress(state, result),
-      phoneNumber: phoneNumber && Map({
-        number: phoneNumberFormatter(phoneNumber.get('number')),
-        type: phoneNumber.get('type'),
-      }),
+      phoneNumber: formatPhoneNumber(result.getIn(['phone_numbers', 0], null)),
       isSensitive: mapIsSensitive(result),
       isSealed: mapIsSealed(result),
     })
