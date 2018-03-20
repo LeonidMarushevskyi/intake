e929d2953f61f07d5f973aa02d44cd1e6f69fa32
Naga Pradeep Gajula <naga.gajula@osi.ca.gov>
2018-03-20 00:05:12 -0700

    Add people to screening from relationships card

--- app/javascript/screenings/RelationshipsContainer.jsx
+++ app/javascript/screenings/RelationshipsContainer.jsx
@@ -1,11 +1,26 @@
 import {connect} from 'react-redux'
-import {Relationships} from 'investigations/Relationships'
-import {
-  getPeopleSelector,
-} from 'selectors/screening/relationshipsSelectors'
+import {Relationships} from 'screenings/Relationships'
+import {getPeopleSelector} from 'selectors/screening/relationshipsSelectors'
+import {createPerson} from 'actions/personCardActions'
+import {getScreeningIdValueSelector} from 'selectors/screeningSelectors'
 
 const mapStateToProps = (state, _ownProps) => ({
   people: getPeopleSelector(state).toJS(),
+  screeningId: getScreeningIdValueSelector(state),
 })
 
-export default connect(mapStateToProps)(Relationships)
+const mapDispatchToProps = (dispatch) => ({
+  onClick: (relationship, screeningId) => {
+    const relationshipsPerson = {
+      screening_id: screeningId,
+      legacy_descriptor: {
+        legacy_id: relationship.legacy_descriptor && relationship.legacy_descriptor.legacy_id,
+        legacy_source_table: relationship.legacy_descriptor && relationship.legacy_descriptor.legacy_table_name,
+      },
+    }
+    dispatch(createPerson(relationshipsPerson))
+  },
+  dispatch,
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Relationships)
--- app/javascript/selectors/screening/relationshipsSelectors.js
+++ app/javascript/selectors/screening/relationshipsSelectors.js
@@ -5,6 +5,20 @@ import {systemCodeDisplayValue, getRelationshipTypesSelector} from 'selectors/sy
 
 export const getScreeningRelationships = (state) => (state.get('relationships', List()))
 
+const isRelationshipCardExists = (people, relationship) => {
+  if (people && people.size > 0) {
+    const index = people.findIndex((person) => {
+      const personjs = person.toJS()
+      return personjs.legacy_id === relationship.legacy_descriptor.legacy_id
+    })
+    const none = -1
+    if (index !== none) {
+      return false
+    }
+  }
+  return true
+}
+
 export const getPeopleSelector = createSelector(
   getScreeningRelationships,
   getRelationshipTypesSelector,
@@ -18,7 +32,9 @@ export const getPeopleSelector = createSelector(
           middle_name: relationship.get('related_person_middle_name'),
           name_suffix: relationship.get('related_person_name_suffix'),
         }),
+        legacy_descriptor: relationship.get('legacy_descriptor'),
         type: systemCodeDisplayValue(relationship.get('indexed_person_relationship'), relationshipTypes),
+        person_card_exists: isRelationshipCardExists(people, relationship.toJS()),
       })
     )),
   }))
