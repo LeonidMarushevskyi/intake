736fa2558ab2337425c9cb247e04703effbcd089
Will Mitchell <will.mitchell@osi.ca.gov>
2018-03-23 16:32:59 -0700

    Reduce cognitive complexity?

--- app/javascript/reducers/errorsReducer.js
+++ app/javascript/reducers/errorsReducer.js
@@ -5,17 +5,16 @@ const initialState = fromJS({})
 
 export default function errorsReducer(state = initialState, action) {
   const {payload, error, type} = action
+
   if (error) {
     const {error: {responseJSON = {}} = {}} = payload
     const {api_response_body: {issue_details} = {}} = responseJSON
-    if (issue_details) {
-      return state.set(type, fromJS(issue_details))
-    } else {
-      return state.set(type, fromJS(payload))
-    }
-  } else if (type === FETCH_SCREENINGS) {
-    return initialState
-  } else {
-    return state.delete(type)
+    return state.set(type, fromJS(issue_details || payload))
   }
+
+  if (type === FETCH_SCREENINGS) {
+    return initialState
+  }
+
+  return state.delete(type)
 }
